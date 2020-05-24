a730db5cf2b423d9567a05910fabaa4775650c59
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-24 17:50:21 +0300

    checkpoint

--- src/common/store/configure.ts
+++ src/common/store/configure.ts
@@ -2,11 +2,27 @@ import {createStore, applyMiddleware, compose} from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer, {AppState} from './index';
 
+const enhancers = [];
+const middleware = [thunk];
+
+if (process.env.NODE_ENV === 'development' && typeof window !== 'undefined') {
+    const devToolsExtension = window['__REDUX_DEVTOOLS_EXTENSION__'];
+
+    if (typeof devToolsExtension === 'function') {
+        enhancers.push(devToolsExtension())
+    }
+}
+
+const composedEnhancers = compose(
+    applyMiddleware(...middleware),
+    ...enhancers
+);
+
 const configureStore = (preLoadedState: AppState) => {
     const store = createStore(
         rootReducer,
         preLoadedState,
-        applyMiddleware(thunk)
+        composedEnhancers
     );
 
     if (module.hot) {
