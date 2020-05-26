4674f12edd1ab065900cfed8435e4093149a5a33
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-26 14:50:04 +0300

    checkpoint

---
+++ src/common/store/trending-tags/index.test.ts
@@ -0,0 +1,31 @@
+import reducer, {initialState, fetchAct, fetchedAct, fetchErrorAct} from './index';
+
+import {State} from './types';
+
+let state: State = initialState;
+
+it('1- default state', () => {
+    expect(state).toMatchSnapshot();
+});
+
+
+it('2- fetch', () => {
+    state = reducer(state, fetchAct());
+    expect(state).toMatchSnapshot();
+});
+
+it('3- fetch error', () => {
+    state = reducer(state, fetchErrorAct());
+    expect(state).toMatchSnapshot();
+});
+
+it('4- fetch', () => {
+    state = reducer(state, fetchAct());
+    expect(state).toMatchSnapshot();
+});
+
+it('5- fetched', () => {
+    state = reducer(state, fetchedAct(['bitcoin', 'hive', 'photography']));
+    expect(state).toMatchSnapshot();
+});
+
