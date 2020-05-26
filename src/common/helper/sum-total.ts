d45f214cc7b34e41cd55c6b142284fcef2f5fcaa
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-26 21:13:17 +0300

    checkpoint

---
+++ src/common/helper/sum-total.ts
@@ -0,0 +1,12 @@
+import {Discussion} from '@esteemapp/dhive';
+
+import parseToken from './parse-token';
+import isEmptyDate from './is-empty-date';
+
+export default (entry: Discussion) => {
+    if (entry.pending_payout_value && isEmptyDate(entry.last_payout)) {
+        return entry.total_payout_value ? parseToken(entry.total_payout_value) + parseToken(entry.pending_payout_value) : 0;
+    }
+
+    return entry.total_payout_value ? parseToken(entry.total_payout_value) + parseToken(entry.curator_payout_value) : 0;
+};
