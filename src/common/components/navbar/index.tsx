a0486e969a587c4125437e3ac020cafe3dc9e436
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-28 15:57:37 +0300

    checkpoint

--- src/common/components/navbar/index.tsx
+++ src/common/components/navbar/index.tsx
@@ -25,43 +25,45 @@ export default class NavBar extends Component <Props> {
 
     render() {
         return <div className="nav-bar">
-            <Link to="/" className="brand">
-                <span className="brand-text">ecency</span>
-            </Link>
+            <div className="nav-bar-inner">
+                <Link to="/" className="brand">
+                    <span className="brand-text">ecency</span>
+                </Link>
 
-            <div className="main-menu">
-                <Link className="menu-item" to="/">Posts</Link>
-                <Link className="menu-item" to="/communities">Communities</Link>
-                <a className="menu-item downloads" href="#">
-                    <span className="label">Downloads</span>
-                    <span className="icons">
+                <div className="main-menu">
+                    <Link className="menu-item" to="/">Posts</Link>
+                    <Link className="menu-item" to="/communities">Communities</Link>
+                    <a className="menu-item downloads" href="#">
+                        <span className="label">Downloads</span>
+                        <span className="icons">
                         <span className="img-apple">{appleSvg}</span>
                         <span className="img-google">{googleSvg}</span>
                         <span className="img-desktop">{desktopSvg}</span>
                     </span>
-                </a>
-                <Link className="menu-item" to="/about">About</Link>
-            </div>
+                    </a>
+                    <Link className="menu-item" to="/about">About</Link>
+                </div>
 
-            <div className="search-bar">
+                <div className="search-bar">
 
                     <span className="prepend">
                             {magnifySvg}
                     </span>
 
-                <FormControl
-                    placeholder={_t('navbar.search-placeholder')}
-                />
+                    <FormControl
+                        placeholder={_t('navbar.search-placeholder')}
+                    />
 
-            </div>
+                </div>
 
-            <div className="alt-controls">
+                <div className="alt-controls">
             <span className="switch-theme"
                   onClick={this.changeTheme}>
                 <ToolTip content={_t('navbar.change-theme')} placement="left">
                 {brightnessSvg}
                 </ToolTip>
             </span>
+                </div>
             </div>
         </div>
     }
