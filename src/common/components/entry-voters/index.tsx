293e0f4054b96255769d4cbc7925b72b3f9cd5bf
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-30 17:21:47 +0300

    checkpoint

--- src/common/components/entry-voters/index.tsx
+++ src/common/components/entry-voters/index.tsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Modal, Table, Pagination, Button } from 'react-bootstrap';
+import { Modal, Table, Button, Spinner } from 'react-bootstrap';
 
 import { Entry } from '../../store/entries/types';
 
@@ -10,7 +10,7 @@ import parseAsset from '../../helper/parse-asset';
 
 import FormattedCurrency from '../formatted-currency';
 
-import {chevronLeftSvg, chevronRightSvg} from '../../../svg';
+import { chevronLeftSvg, chevronRightSvg } from '../../../svg';
 
 interface Vote {
     voter: string,
@@ -46,10 +46,33 @@ export const prepareVotes = (entry: Entry, votes: Vote[]): Vote[] => {
     });
 };
 
+interface DetailProps {
+    entry: Entry,
+    onHide: () => void
+}
 
-export class EntryVotersDetail extends Component<{ votes: Vote[] }, { page: number }>{
-    state = {
-        page: 0
+interface DetailState {
+    loading: boolean,
+    votes: Vote[],
+    page: number,
+}
+
+export class EntryVotersDetail extends Component<DetailProps, DetailState>{
+    state: DetailState = {
+        loading: false,
+        votes: [],
+        page: 0,
+    }
+
+    componentDidMount() {
+        const { entry } = this.props;
+
+        this.setState({ loading: true });
+        getPost(entry.author, entry.permlink).then(r => {
+            this.setState({ votes: prepareVotes(entry, r.active_votes) });
+        }).finally(() => {
+             this.setState({ loading: false });
+        });
     }
 
     prev = () => {
@@ -63,37 +86,57 @@ export class EntryVotersDetail extends Component<{ votes: Vote[] }, { page: numb
     }
 
     render() {
-        const { votes } = this.props;
-        const { page } = this.state;
-
-        const pageSize = 10;
-        const totalPages = Math.ceil(votes.length / pageSize);
-
-        const start = page * pageSize;
-
-        const hasPrev = page !== 0;
-        const hasNext = page + 1 !== totalPages;
-
-        const votes_ = votes.slice(start, start + pageSize)
+        const { onHide } = this.props;
+        const { votes } = this.state;
 
         return (
             <>
-                <Table borderless={true} striped={true}>
-                    <tbody>
-                        {votes_.map((v, i) => (<tr key={i}>
-                            <td>{v.voter}</td>
-                            <td><FormattedCurrency {...this.props} value={v.reward} /></td>
-                        </tr>))}
-                    </tbody>
-                </Table>
-                
-                <div className="voters-pagination">
-                        <Button size="sm" disabled={!hasPrev} onClick={this.prev}>{chevronLeftSvg}</Button>
-                    <div className="page-numbers">
-                        <span className="current-page"> {page + 1}</span> / {totalPages}
-                    </div>
-                    <Button size="sm" disabled={!hasNext} onClick={this.next}>{chevronRightSvg}</Button>
-                </div>
+                <Modal onHide={onHide} show={true} centered={true}>
+                    <Modal.Header closeButton={true}>
+                        <Modal.Title>Voters {votes.length}</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        {(() => {
+                            const { loading } = this.state;
+                            console.log(loading);
+                            if (loading) {
+                                return <div className="voters-list loading"> <Spinner animation="grow" variant="primary" /></div>;
+                            }
+
+                            const { page } = this.state;
+
+                            const pageSize = 6;
+                            const totalPages = Math.ceil(votes.length / pageSize);
+
+                            const start = page * pageSize;
+
+                            const hasPrev = page !== 0;
+                            const hasNext = page + 1 !== totalPages;
+
+                            const list = votes.slice(start, start + pageSize);
+
+                            return <>
+                                <div className="voters-list">
+                                    <Table borderless={true} striped={true}>
+                                        <tbody>
+                                            {list.map((v, i) => (<tr key={i}>
+                                                <td>{v.voter}</td>
+                                                <td><FormattedCurrency {...this.props} value={v.reward} /></td>
+                                            </tr>))}
+                                        </tbody>
+                                    </Table>
+                                </div>
+                                <div className="voters-pagination">
+                                    <Button size="sm" disabled={!hasPrev} onClick={this.prev}>{chevronLeftSvg}</Button>
+                                    <div className="page-numbers">
+                                        <span className="current-page"> {page + 1}</span> / {totalPages}
+                                    </div>
+                                    <Button size="sm" disabled={!hasNext} onClick={this.next}>{chevronRightSvg}</Button>
+                                </div>
+                            </>
+                        })()}
+                    </Modal.Body>
+                </Modal>
             </>
         )
     }
@@ -104,45 +147,31 @@ interface Props {
     children: JSX.Element
 }
 
+interface State {
+    visible: boolean
+}
 
-export default class EntryVoters extends Component<Props, { visible: boolean, votes: Vote[] }> {
-
-    state = {
-        visible: false,
-        votes: []
+export default class EntryVoters extends Component<Props, State> {
+    state: State = {
+        visible: false
     }
 
-    show = () => {
-        const { entry } = this.props;
-        getPost(entry.author, entry.permlink).then(r => {
-            this.setState({ visible: true, votes: prepareVotes(entry, r.active_votes) });
-        });
-    };
-
-    hide = () => {
-        this.setState({ visible: false, votes: [] });
+    toggle = () => {
+        const { visible } = this.state;
+        this.setState({ visible: !visible });
     };
 
     render() {
-        const { children } = this.props;
-        const { visible, votes } = this.state;
+        const { children, entry } = this.props;
+        const { visible } = this.state;
 
         const clonedChildren = React.cloneElement(children, {
-            onClick: this.show
+            onClick: this.toggle
         });
 
         return <>
             {clonedChildren}
-            {visible && (
-                <Modal onHide={this.hide} show={true} centered={true}>
-                    <Modal.Header closeButton={true}>
-                        <Modal.Title>Voters {votes.length}</Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <EntryVotersDetail {...this.props} votes={votes} />
-                    </Modal.Body>
-                </Modal>
-            )}
+            {visible && <EntryVotersDetail {...this.props} entry={entry} onHide={this.toggle} />}
         </>;
     }
 }
