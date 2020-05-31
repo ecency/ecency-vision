df8b0e7cf2d92fa750925798b52aa97d65c52e6a
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-05-31 21:00:48 +0300

    checkpoint

--- src/common/components/community-card/index.tsx
+++ src/common/components/community-card/index.tsx
@@ -16,6 +16,8 @@ import DownloadTrigger from "../download-trigger";
 
 import ln2list from "../../util/nl2list";
 
+import { _t } from "../../i18n";
+
 import { accountGroupSvg } from "../../img/svg";
 
 interface Props {
@@ -44,24 +46,30 @@ export default class CommunityCard extends Component<Props> {
         <div className="community-panel">
           <div className="section-about">{community.about}</div>
           <div className="section-stats">
-            <div className="stat">{subscribers} subscribers</div>
-            <div className="stat">{`$${rewards}`} pending rewards</div>
-            <div className="stat">{authors} active posters</div>
+            <div className="stat">
+              {_t("community.n-subscribers", { n: subscribers })}
+            </div>
+            <div className="stat">
+              {_t("community.n-rewards", { n: rewards })}
+            </div>
+            <div className="stat">
+              {_t("community.n-authors", { n: authors })}
+            </div>
           </div>
           <div className="section-controls">
             <DownloadTrigger>
               <Button variant="primary" block={true}>
-                Subscribe
+                {_t("community.subscribe")}
               </Button>
             </DownloadTrigger>
             <DownloadTrigger>
               <Button variant="primary" block={true}>
-                New Post
+                {_t("community.post")}
               </Button>
             </DownloadTrigger>
           </div>
           <div className="section-team">
-            <h4 className="section-header">Leadership</h4>
+            <h4 className="section-header">{_t("community.team")}</h4>
             {community.team.map((m, i) => {
               if (m[0].startsWith("hive-")) {
                 return null;
@@ -80,13 +88,13 @@ export default class CommunityCard extends Component<Props> {
         </div>
         <div className="community-panel">
           <div className="section-description">
-            <h4 className="section-header">Description</h4>
+            <h4 className="section-header">{_t("community.description")}</h4>
             {ln2list(community.description).map((x, i) => (
               <p key={i}>{x}</p>
             ))}
           </div>
           <div className="section-rules">
-            <h4 className="section-header">Rules</h4>
+            <h4 className="section-header">{_t("community.rules")}</h4>
             <ol>
               {ln2list(community.flag_text).map((x, i) => (
                 <li key={i}>{x}</li>
@@ -94,7 +102,7 @@ export default class CommunityCard extends Component<Props> {
             </ol>
           </div>
           <div className="section-lang">
-            <h4 className="section-header">Language</h4>
+            <h4 className="section-header">{_t("community.lang")}</h4>
             {community.lang}
           </div>
         </div>
