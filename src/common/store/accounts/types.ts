4dd12a0cd66e02c1fe34496e492ad21873193d8c
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-06-01 16:02:25 +0300

    checkpoint

--- src/common/store/accounts/types.ts
+++ src/common/store/accounts/types.ts
@@ -1,3 +1,30 @@
 import { ExtendedAccount } from "@esteemapp/dhive";
 
 export interface Account extends ExtendedAccount {}
+
+export interface State {
+  data: Account;
+  loading: boolean;
+  error: boolean;
+}
+
+export enum ActionTypes {
+  FETCH = "@account/FETCH",
+  FETCHED = "@account/FETCHED",
+  FETCH_ERROR = "@account/FETCH_ERROR"
+}
+
+export interface FetchAction {
+  type: ActionTypes.FETCH;
+}
+
+export interface FetchedAction {
+  type: ActionTypes.FETCHED;
+  data: Account;
+}
+
+export interface FetchErrorAction {
+  type: ActionTypes.FETCH_ERROR;
+}
+
+export type Actions = FetchAction | FetchedAction | FetchErrorAction;
