4bd9dee9f574735b3ebf1054a6e59abc62415fd3
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-06-12 15:00:15 +0300

    improvements on communities

---
+++ src/common/components/community-list-item/index.tsx
@@ -0,0 +1,78 @@
+import React, { Component } from "react";
+
+import { History, Location } from "history";
+import { Link } from "react-router-dom";
+
+import { Button } from "react-bootstrap";
+
+import isEqual from "react-fast-compare";
+
+import { State as GlobalState } from "../../store/global/types";
+import { Account } from "../../store/accounts/types";
+import { Community } from "../../store/community/types";
+
+import ProfileLink from "../../components/profile-link";
+import DownloadTrigger from "../../components/download-trigger";
+import { makePath } from "../../components/tag-link";
+
+import defaults from "../../constants/defaults.json";
+
+import { _t } from "../../i18n";
+
+import _c from "../../util/fix-class-names";
+
+import formattedNumber from "../../util/formatted-number";
+
+interface Props {
+  history: History;
+  location: Location;
+  global: GlobalState;
+  community: Community;
+  addAccount: (data: Account) => void;
+}
+
+export default class CommunityListItem extends Component<Props> {
+  shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
+    return !isEqual(this.props.community, nextProps.community);
+  }
+
+  render() {
+    const { community } = this.props;
+
+    const nOpts = { fractionDigits: 0 };
+    const subscribers = formattedNumber(community.subscribers, nOpts);
+    const authors = formattedNumber(community.num_authors, nOpts);
+    const posts = formattedNumber(community.num_pending, nOpts);
+
+    return (
+      <div className="list-item">
+        <div className="item-content">
+          <h2 className="item-title">
+            <Link to={makePath(defaults.filter, community.name)}>{community.title}</Link>
+          </h2>
+          <div className="item-about">{community.about}</div>
+          <div className="item-stats">
+            <div className="stat">{_t("communities.n-subscribers", { n: subscribers })}</div>
+            <div className="stat">{_t("communities.n-authors", { n: authors })}</div>
+            <div className="stat">{_t("communities.n-posts", { n: posts })}</div>
+          </div>
+          {community.admins && (
+            <div className="item-admins">
+              {_t("communities.admins")}
+              {community.admins.map((x, i) => (
+                <ProfileLink key={i} {...this.props} username={x}>
+                  <a className="admin">{x}</a>
+                </ProfileLink>
+              ))}
+            </div>
+          )}
+        </div>
+        <div className="item-controls">
+          <DownloadTrigger>
+            <Button>{_t("communities.subscribe")}</Button>
+          </DownloadTrigger>
+        </div>
+      </div>
+    );
+  }
+}
--- src/common/pages/communities.tsx
+++ src/common/pages/communities.tsx
@@ -3,7 +3,7 @@ import { AnyAction, bindActionCreators, Dispatch } from "redux";
 import { connect } from "react-redux";
 import { History, Location } from "history";
 
-import { Form, FormControl, Button } from "react-bootstrap";
+import { Form, FormControl } from "react-bootstrap";
 
 import { AppState } from "../store";
 import { State as GlobalState } from "../store/global/types";
@@ -16,21 +16,14 @@ import { addAccount } from "../store/accounts/index";
 import Meta from "../components/meta";
 import Theme from "../components/theme/index";
 import NavBar from "../components/navbar/index";
-import ProfileLink from "../components/profile-link";
-import DownloadTrigger from "../components/download-trigger";
 import LinearProgress from "../components/linear-progress";
-import { makePath } from "../components/tag-link";
-
-import defaults from "../constants/defaults.json";
+import CommunityListItem from "../components/community-list-item";
 
 import { _t } from "../i18n";
 
 import _c from "../util/fix-class-names";
 
-import formattedNumber from "../util/formatted-number";
-
 import { getCommunities } from "../api/bridge";
-import { threadId } from "worker_threads";
 
 interface Props {
   history: History;
@@ -139,43 +132,9 @@ class EntryIndexPage extends Component<Props> {
             </div>
             {loading && <LinearProgress />}
             <div className="list-items">
-              {list.map((x, i) => {
-                const nOpts = { fractionDigits: 0 };
-                const subscribers = formattedNumber(x.subscribers, nOpts);
-                const authors = formattedNumber(x.num_authors, nOpts);
-                const posts = formattedNumber(x.num_pending, nOpts);
-
-                return (
-                  <div key={i} className="list-item">
-                    <div className="item-content">
-                      <h2 className="item-title">
-                        <a href={makePath(defaults.filter, x.name)}>{x.title}</a>
-                      </h2>
-                      <div className="item-about">{x.about}</div>
-                      <div className="item-stats">
-                        <div className="stat">{_t("communities.n-subscribers", { n: subscribers })}</div>
-                        <div className="stat">{_t("communities.n-authors", { n: authors })}</div>
-                        <div className="stat">{_t("communities.n-posts", { n: posts })}</div>
-                      </div>
-                      {x.admins && (
-                        <div className="item-admins">
-                          {_t("communities.admins")}
-                          {x.admins.map((x, i) => (
-                            <ProfileLink key={i} {...this.props} username={x}>
-                              <a className="admin">{x}</a>
-                            </ProfileLink>
-                          ))}
-                        </div>
-                      )}
-                    </div>
-                    <div className="item-controls">
-                      <DownloadTrigger>
-                        <Button>{_t("communities.subscribe")}</Button>
-                      </DownloadTrigger>
-                    </div>
-                  </div>
-                );
-              })}
+              {list.map((x, i) => (
+                <CommunityListItem {...this.props} key={i} community={x} />
+              ))}
             </div>
           </div>
         </div>
