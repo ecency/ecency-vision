dde612b738f84d608efca13e5809e9f4965285b3
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-06-13 13:31:09 +0300

    dropdown menu improvements

--- src/common/components/dropdown/index.tsx
+++ src/common/components/dropdown/index.tsx
@@ -1,92 +1,95 @@
-import React, {Component} from 'react';
-import {History} from 'history';
+import React, { Component } from "react";
+import { History } from "history";
 
-import {menuDownSvg} from '../../img/svg';
+import { menuDownSvg } from "../../img/svg";
 
 interface MenuItem {
-    label: string,
-    href: string,
-    active?: boolean
+  label: string;
+  href: string;
+  active?: boolean;
 }
 
 interface Props {
-    history: History,
-    label: string,
-    items: MenuItem[]
+  history: History;
+  label: string;
+  items: MenuItem[];
 }
 
 interface State {
-    menu: boolean
+  menu: boolean;
 }
 
 export default class MyDropDown extends Component<Props> {
-    state: State = {
-        menu: false
-    };
+  state: State = {
+    menu: false,
+  };
 
-    timer: any = null;
+  _timer: any = null;
 
-    mouseEnter = () => {
-        if (this.timer) {
-            clearTimeout(this.timer);
-            this.timer = null;
-        }
+  mouseEnter = () => {
+    this._timer = setTimeout(() => {
+      this.showMenu();
+    }, 500);
+  };
 
-        this.showMenu();
-    };
-
-    mouseLeave = () => {
-        this.timer = setTimeout(() => {
-            this.hideMenu();
-        }, 500);
-    };
-
-
-    showMenu = () => {
-        this.setState({menu: true});
-    };
-
-    hideMenu = () => {
-        this.setState({menu: false});
-    };
-
-    itemClicked = (i: MenuItem) => {
-        this.hideMenu();
-
-        setTimeout(() => {
-            const {history} = this.props;
-            history.push(i.href);
-        }, 200)
-    };
-
-    render() {
-        const {label, items} = this.props;
-        const {menu} = this.state;
-
-        return (
-            <div className="custom-dropdown" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
-                <div className={`dropdown-btn ${menu ? 'hover' : ''}`}>
-                    <div className="label">{label}</div>
-                    <div className="menu-down">{menuDownSvg}</div>
-                </div>
-
-                {menu && (
-                    <div className="the-menu">
-                        <div className="menu-inner">
-                            <div className="menu-list">
-                                {items.map((i, k) => {
-                                    return <div key={k}
-                                                className={`menu-item ${i.active ? 'active' : ''}`}
-                                                onClick={() => {
-                                                    this.itemClicked(i);
-                                                }}
-                                    ><span className="item-inner">{i.label}</span></div>
-                                })}
-                            </div>
-                        </div>
-                    </div>
-                )}
-            </div>
-        )
+  mouseLeave = () => {
+    if (this._timer) {
+      clearTimeout(this._timer);
     }
+
+    this.hideMenu();
+  };
+
+  showMenu = () => {
+    this.setState({ menu: true });
+  };
+
+  hideMenu = () => {
+    this.setState({ menu: false });
+  };
+
+  itemClicked = (i: MenuItem) => {
+    this.hideMenu();
+
+    setTimeout(() => {
+      const { history } = this.props;
+      history.push(i.href);
+    }, 200);
+  };
+
+  render() {
+    const { label, items } = this.props;
+    const { menu } = this.state;
+
+    return (
+      <div className="custom-dropdown" onMouseEnter={this.mouseEnter} onMouseLeave={this.mouseLeave}>
+        <div className={`dropdown-btn ${menu ? "hover" : ""}`}>
+          <div className="label">{label}</div>
+          <div className="menu-down">{menuDownSvg}</div>
+        </div>
+
+        {menu && (
+          <div className="the-menu">
+            <div className="menu-inner">
+              <div className="menu-list">
+                {items.map((i, k) => {
+                  return (
+                    <div
+                      key={k}
+                      className={`menu-item ${i.active ? "active" : ""}`}
+                      onClick={() => {
+                        this.itemClicked(i);
+                      }}
+                    >
+                      <span className="item-inner">{i.label}</span>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
