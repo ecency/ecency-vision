e4fd8cb886105ebadbb8fd0c4cedc249b3009457
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-06-13 14:22:23 +0300

    post canonical link genetator

---
+++ src/common/helper/entry-canonical.ts
@@ -0,0 +1,26 @@
+import { Entry } from "../store/entries/types";
+
+import apps from "@hivechain/hivescript/apps.json";
+
+import defaults from "../../common/constants/defaults.json";
+
+export default (entry: Entry): string | null => {
+  if (entry.json_metadata?.canonical_url) {
+    return entry.json_metadata?.canonical_url;
+  }
+
+  let scheme = `${defaults.base}/{category}/@{username}/{permlink}`;
+
+  if (entry.json_metadata?.app) {
+    const identifier = entry.json_metadata.app.split("/")[0];
+
+    if (apps[identifier]) {
+      scheme = apps[identifier].url_scheme;
+    }
+  }
+
+  return scheme
+    .replace("{category}", entry.category)
+    .replace("{username}", entry.author)
+    .replace("{permlink}", entry.permlink);
+};
