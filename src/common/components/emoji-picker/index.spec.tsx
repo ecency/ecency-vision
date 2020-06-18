29b289c7c579490de3b0e5c87c63985bc15006d6
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-06-18 14:45:17 +0300

    checkpoint

---
+++ src/common/components/emoji-picker/index.spec.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import renderer from "react-test-renderer";
+
+import EmojiPicker from "./index";
+
+import emojiData from "../../../../public/emoji.json";
+
+jest.mock("../../api/misc", () => ({
+  getEmojiData: () =>
+    new Promise((resolve) => {
+      resolve(emojiData);
+    }),
+}));
+
+jest.mock("../../util/local-storage", () => ({
+  get: () => ["dog2", "wink"],
+}));
+
+const detailProps = {
+  onPick: () => {},
+};
+
+const component = renderer.create(<EmojiPicker {...detailProps} />);
+
+it("(1) Default full render", () => {
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+it("(2) Filter", () => {
+  const instance: any = component.getInstance();
+  instance.setState({ filter: "dog" });
+  expect(component.toJSON()).toMatchSnapshot();
+});
+
+it("(3) Filter - No result", () => {
+  const instance: any = component.getInstance();
+  instance.setState({ filter: "loremipsumdolorsit" });
+  expect(component.toJSON()).toMatchSnapshot();
+});
