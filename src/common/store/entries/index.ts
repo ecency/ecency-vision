33127c3d285f5a4bcb26e0590442083ed2474b66
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-07-10 14:00:33 +0300

    checkpoint

--- src/common/store/entries/index.ts
+++ src/common/store/entries/index.ts
@@ -1,231 +1,231 @@
 import update from "immutability-helper";
 
-import { Dispatch } from "redux";
+import {Dispatch} from "redux";
 
-import { AppState } from "../index";
+import {AppState} from "../index";
 
 import {
-  Actions,
-  ActionTypes,
-  FetchAction,
-  FetchErrorAction,
-  FetchedAction,
-  InvalidateAction,
-  UpdateAction,
-  Entries,
-  Entry,
+    Actions,
+    ActionTypes,
+    FetchAction,
+    FetchErrorAction,
+    FetchedAction,
+    InvalidateAction,
+    UpdateAction,
+    Entries,
+    Entry,
 } from "./types";
 
-import { CommonActionTypes } from "../common";
+import {CommonActionTypes} from "../common";
+
+import {clone} from '../helper';
 
 import filterTagExtract from "../../helper/filter-tag-extract";
 
-import { getPostsRanked, getAccountPosts } from "../../api/bridge";
+import {getPostsRanked, getAccountPosts} from "../../api/bridge";
 
 export const makeGroupKey = (what: string, tag: string = ""): string => {
-  if (tag) {
-    return `${what}-${tag}`;
-  }
-  return `${what}`;
+    if (tag) {
+        return `${what}-${tag}`;
+    }
+    return `${what}`;
 };
 
 export const initialState: Entries = {};
 
 export default (state: Entries = initialState, action: Actions): Entries => {
-  switch (action.type) {
-    case CommonActionTypes.INIT: {
-      if (state["__manual__"] === undefined) {
-        return update(state, {
-          ["__manual__"]: {
-            $set: { entries: [], error: null, loading: false, hasMore: false },
-          },
-        });
-      }
-
-      return state;
-    }
-    case CommonActionTypes.LOCATION_CHANGE: {
-      const { pathname } = action.payload.location;
-      const params = filterTagExtract(pathname);
-
-      if (!params) {
-        return state;
-      }
-
-      const { filter, tag } = params;
-      const groupKey = makeGroupKey(filter, tag);
-
-      if (state[`${groupKey}`] === undefined) {
-        return update(state, {
-          [`${groupKey}`]: {
-            $set: { entries: [], error: null, loading: false, hasMore: false },
-          },
-        });
-      }
-
-      return state;
-    }
-    case ActionTypes.FETCH: {
-      const { groupKey } = action;
-      return update(state, {
-        [`${groupKey}`]: { $merge: { loading: true, error: null } },
-      });
-    }
-    case ActionTypes.FETCH_ERROR: {
-      const { groupKey, error } = action;
-      return update(state, {
-        [`${groupKey}`]: { $merge: { loading: false, error } },
-      });
-    }
-    case ActionTypes.FETCHED: {
-      const { groupKey, entries, hasMore } = action;
-
-      const merged = update(state, {
-        [`${groupKey}`]: { $merge: { loading: false, error: null, hasMore } },
-      });
-
-      // Filter entries
-      const newEntries = entries.filter((x) => {
-        return state[groupKey].entries.find((y) => y.author == x.author && y.permlink == x.permlink) === undefined;
-      });
-
-      return update(merged, {
-        [`${groupKey}`]: { entries: { $push: newEntries } },
-      });
-    }
-    case ActionTypes.INVALIDATE: {
-      const { groupKey } = action;
-
-      return update(state, {
-        [`${groupKey}`]: {
-          $merge: { entries: [], loading: false, error: null, hasMore: false },
-        },
-      });
-    }
-    case ActionTypes.UPDATE: {
-      const { entry } = action;
-
-      // TODO: Find better approach for immutabilty than JSON
-      const st = JSON.parse(JSON.stringify(state));
-
-      const groupKeys = Object.keys(st);
-
-      // Iterate over all groups and update the entry
-      for (const k of groupKeys) {
-        st[k].entries = st[k].entries.map(
-          (e: Entry): Entry => {
-            if (e.author === entry.author && e.permlink === entry.permlink) {
-              return entry;
+    switch (action.type) {
+        case CommonActionTypes.INIT: {
+            if (state["__manual__"] === undefined) {
+                return update(state, {
+                    ["__manual__"]: {
+                        $set: {entries: [], error: null, loading: false, hasMore: false},
+                    },
+                });
             }
 
-            return e;
-          }
-        );
-      }
+            return state;
+        }
+        case CommonActionTypes.LOCATION_CHANGE: {
+            const {pathname} = action.payload.location;
+            const params = filterTagExtract(pathname);
 
-      return JSON.parse(JSON.stringify(st));
+            if (!params) {
+                return state;
+            }
+
+            const {filter, tag} = params;
+            const groupKey = makeGroupKey(filter, tag);
+
+            if (state[`${groupKey}`] === undefined) {
+                return update(state, {
+                    [`${groupKey}`]: {
+                        $set: {entries: [], error: null, loading: false, hasMore: false},
+                    },
+                });
+            }
+
+            return state;
+        }
+        case ActionTypes.FETCH: {
+            const {groupKey} = action;
+            return update(state, {
+                [`${groupKey}`]: {$merge: {loading: true, error: null}},
+            });
+        }
+        case ActionTypes.FETCH_ERROR: {
+            const {groupKey, error} = action;
+            return update(state, {
+                [`${groupKey}`]: {$merge: {loading: false, error}},
+            });
+        }
+        case ActionTypes.FETCHED: {
+            const {groupKey, entries, hasMore} = action;
+
+            const merged = update(state, {
+                [`${groupKey}`]: {$merge: {loading: false, error: null, hasMore}},
+            });
+
+            // Filter entries
+            const newEntries = entries.filter((x) => {
+                return state[groupKey].entries.find((y) => y.author == x.author && y.permlink == x.permlink) === undefined;
+            });
+
+            return update(merged, {
+                [`${groupKey}`]: {entries: {$push: newEntries}},
+            });
+        }
+        case ActionTypes.INVALIDATE: {
+            const {groupKey} = action;
+
+            return update(state, {
+                [`${groupKey}`]: {
+                    $merge: {entries: [], loading: false, error: null, hasMore: false},
+                },
+            });
+        }
+        case ActionTypes.UPDATE: {
+            const {entry} = action;
+
+            const st = clone(state);
+            const groupKeys = Object.keys(st);
+
+            // Iterate over all groups and update the entry
+            for (const k of groupKeys) {
+                st[k].entries = st[k].entries.map(
+                    (e: Entry): Entry => {
+                        if (e.author === entry.author && e.permlink === entry.permlink) {
+                            return entry;
+                        }
+
+                        return e;
+                    }
+                );
+            }
+
+            return clone(st);
+        }
+        default:
+            return state;
     }
-    default:
-      return state;
-  }
 };
 
 /* Actions */
 export const fetchEntries = (what: string = "", tag: string = "", more: boolean = false) => (
-  dispatch: Dispatch,
-  getState: () => AppState
+    dispatch: Dispatch,
+    getState: () => AppState
 ) => {
-  const { entries } = getState();
-  const pageSize = 20;
+    const {entries} = getState();
+    const pageSize = 20;
 
-  const groupKey = makeGroupKey(what, tag);
+    const groupKey = makeGroupKey(what, tag);
 
-  const theEntries = entries[groupKey].entries;
+    const theEntries = entries[groupKey].entries;
 
-  if (!more && theEntries.length > 0) {
-    return;
-  }
+    if (!more && theEntries.length > 0) {
+        return;
+    }
 
-  const lastEntry = theEntries[theEntries.length - 1];
+    const lastEntry = theEntries[theEntries.length - 1];
 
-  let start_author = "";
-  let start_permlink = "";
+    let start_author = "";
+    let start_permlink = "";
 
-  if (lastEntry) {
-    start_author = lastEntry.author;
-    start_permlink = lastEntry.permlink;
-  }
+    if (lastEntry) {
+        start_author = lastEntry.author;
+        start_permlink = lastEntry.permlink;
+    }
 
-  dispatch(fetchAct(groupKey));
+    dispatch(fetchAct(groupKey));
 
-  let prms: Promise<Entry[] | null>;
-  if (tag.startsWith("@")) {
-    // @username/blog|replies|comments|feed
-    const username = tag.replace("@", "");
+    let prms: Promise<Entry[] | null>;
+    if (tag.startsWith("@")) {
+        // @username/blog|replies|comments|feed
+        const username = tag.replace("@", "");
 
-    prms = getAccountPosts(what, username, start_author, start_permlink);
-  } else {
-    // trending/tag
-    prms = getPostsRanked(what, start_author, start_permlink, pageSize, tag);
-  }
+        prms = getAccountPosts(what, username, start_author, start_permlink);
+    } else {
+        // trending/tag
+        prms = getPostsRanked(what, start_author, start_permlink, pageSize, tag);
+    }
 
-  prms
-    .then((resp) => {
-      if (resp) {
-        dispatch(fetchedAct(groupKey, resp, resp.length >= pageSize));
-      } else {
-        dispatch(fetchErrorAct(groupKey, "server error"));
-      }
-    })
-    .catch((e) => {
-      dispatch(fetchErrorAct(groupKey, "network error"));
-    });
+    prms
+        .then((resp) => {
+            if (resp) {
+                dispatch(fetchedAct(groupKey, resp, resp.length >= pageSize));
+            } else {
+                dispatch(fetchErrorAct(groupKey, "server error"));
+            }
+        })
+        .catch((e) => {
+            dispatch(fetchErrorAct(groupKey, "network error"));
+        });
 };
 
 export const addEntry = (entry: Entry) => (dispatch: Dispatch) => {
-  dispatch(fetchedAct("__manual__", [entry], false));
+    dispatch(fetchedAct("__manual__", [entry], false));
 };
 
 export const updateEntry = (entry: Entry) => (dispatch: Dispatch) => {
-  dispatch(updateAct(entry));
+    dispatch(updateAct(entry));
 };
 
 /* Action Creators */
 
 export const fetchAct = (groupKey: string): FetchAction => {
-  return {
-    type: ActionTypes.FETCH,
-    groupKey,
-  };
+    return {
+        type: ActionTypes.FETCH,
+        groupKey,
+    };
 };
 
 export const fetchErrorAct = (groupKey: string, error: string): FetchErrorAction => {
-  return {
-    type: ActionTypes.FETCH_ERROR,
-    groupKey,
-    error,
-  };
+    return {
+        type: ActionTypes.FETCH_ERROR,
+        groupKey,
+        error,
+    };
 };
 
 export const fetchedAct = (groupKey: string, entries: Entry[], hasMore: boolean): FetchedAction => {
-  return {
-    type: ActionTypes.FETCHED,
-    groupKey,
-    entries,
-    hasMore,
-  };
+    return {
+        type: ActionTypes.FETCHED,
+        groupKey,
+        entries,
+        hasMore,
+    };
 };
 
 export const invalidateAct = (groupKey: string): InvalidateAction => {
-  return {
-    type: ActionTypes.INVALIDATE,
-    groupKey,
-  };
+    return {
+        type: ActionTypes.INVALIDATE,
+        groupKey,
+    };
 };
 
 export const updateAct = (entry: Entry): UpdateAction => {
-  return {
-    type: ActionTypes.UPDATE,
-    entry,
-  };
+    return {
+        type: ActionTypes.UPDATE,
+        entry,
+    };
 };
