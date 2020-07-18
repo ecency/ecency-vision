d48b535cf99abe6c6c233c92c0f1b55d9fd39c9d
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-07-18 14:36:05 +0300

    checkpoint

--- src/common/i18n/index.ts
+++ src/common/i18n/index.ts
@@ -2,14 +2,14 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 
 const resources = {
-    enUs: {
+    ['en-US']: {
         translation: require('./locales/en-US.json')
     }
 };
 
 i18n.use(LanguageDetector).init({
     resources,
-    fallbackLng: 'enUs',
+    fallbackLng: 'en-US',
     interpolation: {
         escapeValue: false
     },
