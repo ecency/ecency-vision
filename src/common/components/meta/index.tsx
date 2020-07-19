0f286dc3cde1c8152442c68149de9d7bf9b9fb20
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-07-19 14:33:19 +0300

    checkpoint

--- src/common/components/meta/index.tsx
+++ src/common/components/meta/index.tsx
@@ -31,6 +31,7 @@ export default class Meta extends Component<Props> {
     return (
       <>
         <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <meta property="og:site_name" content={defaults.name} />
           <meta property="og:type" content="website" />
           <meta name="twitter:card" content="summary_large_image" />
