7026bad30d0c785b761f46970c605dc4f6bd8ed7
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-07-20 10:28:04 +0300

    checkpoint

--- src/common/components/entry-reblog-btn/index.spec.tsx
+++ src/common/components/entry-reblog-btn/index.spec.tsx
@@ -1,54 +1,63 @@
 import React from "react";
 
 import EntryReblogBtn from "./index";
+
 import TestRenderer from "react-test-renderer";
 
-import { entryInstance1 } from "../../helper/test-helper";
+import {entryInstance1, UiInstance} from "../../helper/test-helper";
 
 const defProps = {
-  text: false,
-  entry: { ...entryInstance1 },
-  users: [],
-  activeUser: null,
-  reblogs: [],
-  setActiveUser: () => {},
-  updateActiveUser: () => {},
-  deleteUser: () => {},
-  addReblog: () => {},
+    text: false,
+    entry: {...entryInstance1},
+    users: [],
+    activeUser: null,
+    reblogs: [],
+    ui: UiInstance,
+    setActiveUser: () => {
+    },
+    updateActiveUser: () => {
+    },
+    deleteUser: () => {
+    },
+    addReblog: () => {
+    },
+    toggleUIProp: () => {
+
+    }
 };
 
 it("(1) No active user", () => {
-  const props = { ...defProps };
-  const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
-  expect(renderer.toJSON()).toMatchSnapshot();
+    const props = {...defProps};
+    const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
+    expect(renderer.toJSON()).toMatchSnapshot();
 });
 
 it("(2) With text", () => {
-  const props = { ...defProps, text: true };
-  const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
-  expect(renderer.toJSON()).toMatchSnapshot();
+    const props = {...defProps, text: true};
+    const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
+    expect(renderer.toJSON()).toMatchSnapshot();
 });
 
 it("(3) Active user. Not reblogged", () => {
-  const props = { ...defProps, activeUser: { username: "user1", data: { name: "user1" } } };
-  const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
-  expect(renderer.toJSON()).toMatchSnapshot();
+    const props = {...defProps, activeUser: {username: "user1", data: {name: "user1"}}};
+    const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
+    expect(renderer.toJSON()).toMatchSnapshot();
 });
 
 it("(4) Active user. Reblogged", () => {
-  const props = {
-    ...defProps,
-    activeUser: { username: "user1", data: { name: "user1" } },
-    reblogs: [{ account: "user1", author: entryInstance1.author, permlink: entryInstance1.permlink }],
-  };
-  const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
-  expect(renderer.toJSON()).toMatchSnapshot();
+    const props = {
+        ...defProps,
+        activeUser: {username: "user1", data: {name: "user1"}},
+        reblogs: [{account: "user1", author: entryInstance1.author, permlink: entryInstance1.permlink}],
+    };
+    const renderer = TestRenderer.create(<EntryReblogBtn {...props} />);
+    expect(renderer.toJSON()).toMatchSnapshot();
 });
 
 it("(5) Reblogging", () => {
-  const props = { ...defProps, activeUser: { username: "user1", data: { name: "user1" } } };
-  const component = TestRenderer.create(<EntryReblogBtn {...props} />);
-  const instance: any = component.getInstance();
-  instance.stateSet({ inProgress: true });
-  expect(component.toJSON()).toMatchSnapshot();
+    const props = {...defProps, activeUser: {username: "user1", data: {name: "user1"}}};
+    const component = TestRenderer.create(<EntryReblogBtn {...props} />);
+    const instance: any = component.getInstance();
+    instance.stateSet({inProgress: true});
+    expect(component.toJSON()).toMatchSnapshot();
 });
