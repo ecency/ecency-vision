646468c365d6dbc964b7362cb96440997cc28592
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-07-21 12:13:57 +0300

    checkpoint

---
+++ src/common/store/subscriptions/index.spec.ts
@@ -0,0 +1,18 @@
+import reducer, {initialState, updateAct} from "./index";
+import {logoutAct} from "../active-user";
+
+let state = initialState;
+
+it("1- default state", () => {
+    expect(state).toMatchSnapshot();
+});
+
+it("2- updateAct", () => {
+    state = reducer(state, updateAct([["hive-148441", "GEMS", "guest", ""]]));
+    expect(state).toMatchSnapshot();
+});
+
+it("3- logoutAct", () => {
+    state = reducer(state, logoutAct());
+    expect(state).toMatchSnapshot();
+});
