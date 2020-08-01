2b4e0d9c06eb85442978c123fc9461f6611183be
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-08-01 15:05:59 +0300

    checkpoint

---
+++ src/common/components/entry-link/index.tsx
@@ -0,0 +1,60 @@
+import React, {Component} from "react";
+
+import {History} from "history";
+
+import {Entry} from "../../store/entries/types";
+
+import {getPost} from "../../api/bridge";
+
+export const makePath = (category: string, author: string, permlink: string, toReplies: boolean = false) =>
+    `/${category}/@${author}/${permlink}${toReplies ? "#replies" : ""}`;
+
+interface PartialEntry {
+    category: string;
+    author: string;
+    permlink: string;
+}
+
+interface Props {
+    history: History;
+    children: JSX.Element;
+    entry: Entry | PartialEntry;
+    afterClick?: () => void;
+}
+
+export default class EntryLink extends Component<Props> {
+    clicked = async (e: React.MouseEvent<HTMLElement>) => {
+        e.preventDefault();
+
+        const {history, afterClick} = this.props;
+        let {entry: _entry} = this.props;
+
+        if (!("title" in _entry)) {
+            // Get full content if the "entry" passed is "PartialEntry"
+            try {
+                const resp = await getPost(_entry.author, _entry.permlink);
+                if (resp) {
+                    _entry = resp
+                }
+            } catch (e) {
+                return;
+            }
+        }
+
+        const {category, author, permlink} = _entry;
+
+        history.push(makePath(category, author, permlink));
+
+        if (afterClick) afterClick();
+    };
+
+    render() {
+        const {children, entry} = this.props;
+
+        const href = makePath(entry.category, entry.author, entry.permlink);
+
+        const props = Object.assign({}, children.props, {href, onClick: this.clicked});
+
+        return React.createElement("a", props);
+    }
+}
