07f6e10bc7bbfd740936c8453036985a058ad696
feruz <fmiboy@gmail.com>
2020-08-04 10:34:49 +0300

    revert avatars

--- src/common/components/user-avatar/index.tsx
+++ src/common/components/user-avatar/index.tsx
@@ -1,5 +1,4 @@
 import React, {Component} from 'react';
-import {Context} from "react-img-webp";
 
 import defaults from '../../constants/defaults.json';
 
@@ -8,6 +7,7 @@ interface Props {
     size: string,
     onClick: (e: React.MouseEvent<HTMLElement>) => void
 }
+
 export default class UserAvatar extends Component<Props> {
     public static defaultProps = {
         onClick: () => {
@@ -19,21 +19,16 @@ export default class UserAvatar extends Component<Props> {
         const imgSize = size === 'xLarge' ? 'large' : ((size === 'normal' || size === 'small') ? 'small' : 'medium');
         const cls = `user-avatar ${size}`;
         const imageSrc = `${defaults.imageServer}/u/${username}/avatar/${imgSize}`;
-        const imageSrcWebp = `${defaults.imageServer}/webp/u/${username}/avatar/${imgSize}`;
-        
+
         return (
-            <Context.WebP.Consumer>
-                {(value) =>
-                    <span onClick={onClick}
-                        role="none"
-                        key="user-avatar-image"
-                        className={cls}
-                        style={{
-                            backgroundImage: `url(${value.supportWebP ? imageSrcWebp : imageSrc})`
-                        }}
-                    />
-                }
-            </Context.WebP.Consumer>
+            <span onClick={onClick}
+                  role="none"
+                  key="user-avatar-image"
+                  className={cls}
+                  style={{
+                      backgroundImage: `url(${imageSrc})`
+                  }}
+            />
         );
     }
 }
