f9555487f4f73bd59114ca65f1d6d599335b536b
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-08-26 12:12:05 +0300

    message feedback component visual improvements && code correactions and formatting

--- src/common/components/feedback/index.tsx
+++ src/common/components/feedback/index.tsx
@@ -1,101 +1,112 @@
-import React, { Component } from "react";
+import React, {Component} from "react";
 
 import random from "../../util/rnd";
 
-import { checkSvg, alertCircleSvg } from "../../img/svg";
+import {checkSvg, alertCircleSvg} from "../../img/svg";
 
 export const error = (message: string) => {
-  const detail: FeedbackObject = {
-    id: random(),
-    type: "error",
-    message,
-  };
-  const ev = new CustomEvent("feedback", { detail });
-  window.dispatchEvent(ev);
+    const detail: FeedbackObject = {
+        id: random(),
+        type: "error",
+        message,
+    };
+    const ev = new CustomEvent("feedback", {detail});
+    window.dispatchEvent(ev);
 };
 
 export const success = (message: string) => {
-  const detail: FeedbackObject = {
-    id: random(),
-    type: "success",
-    message,
-  };
-  const ev = new CustomEvent("feedback", { detail });
-  window.dispatchEvent(ev);
+    const detail: FeedbackObject = {
+        id: random(),
+        type: "success",
+        message,
+    };
+    const ev = new CustomEvent("feedback", {detail});
+    window.dispatchEvent(ev);
 };
 
 type FeedbackType = "error" | "success";
 
 export interface FeedbackObject {
-  id: string;
-  type: FeedbackType;
-  message: string;
+    id: string;
+    type: FeedbackType;
+    message: string;
 }
 
-interface Props {}
+interface Props {
+}
 
 interface State {
-  list: FeedbackObject[];
+    list: FeedbackObject[];
 }
 
 export default class Feedback extends Component<Props, State> {
-  state: State = {
-    list: [],
-  };
+    state: State = {
+        list: [],
+    };
 
-  componentDidMount() {
-    window.addEventListener("feedback", this.onFeedback);
-  }
+    _mounted: boolean = true;
 
-  componentWillUnmount() {
-    window.removeEventListener("feedback", this.onFeedback);
-  }
-
-  onFeedback = (e: Event) => {
-    const detail: FeedbackObject = (e as CustomEvent).detail;
-
-    const { list } = this.state;
-    if (list.find((x) => x.message === detail.message) !== undefined) {
-      return;
+    componentDidMount() {
+        window.addEventListener("feedback", this.onFeedback);
     }
 
-    const newList = [...list, detail];
-    this.setState({ list: newList });
+    componentWillUnmount() {
+        window.removeEventListener("feedback", this.onFeedback);
 
-    setTimeout(() => {
-      const { list } = this.state;
-      const newList = list.filter((x) => x.id !== detail.id);
-      this.setState({ list: newList });
-    }, 5000);
-  };
-
-  render() {
-    const { list } = this.state;
-
-    if (list.length === 0) {
-      return null;
+        this._mounted = false;
     }
-    return (
-      <div className="feedback-container">
-        {list.map((x) => {
-          switch (x.type) {
-            case "success":
-                return (
-                    <div key={x.id} className="feedback-success">
-                        {checkSvg} {x.message}
-                    </div>
-                );
-            case "error":
-                return (
-                    <div key={x.id} className="feedback-error">
-                        {alertCircleSvg} {x.message}
-                    </div>
-                );
-            default:
-              return null;
-          }
-        })}
-      </div>
-    );
-  }
+
+    stateSet = (state: {}, cb?: () => void) => {
+        if (this._mounted) {
+            this.setState(state, cb);
+        }
+    };
+
+    onFeedback = (e: Event) => {
+        const detail: FeedbackObject = (e as CustomEvent).detail;
+
+        const {list} = this.state;
+        if (list.find((x) => x.message === detail.message) !== undefined) {
+            return;
+        }
+
+        const newList = [...list, detail];
+        this.stateSet({list: newList});
+
+        setTimeout(() => {
+            const {list} = this.state;
+            const newList = list.filter((x) => x.id !== detail.id);
+            this.stateSet({list: newList});
+        }, 5000);
+    };
+
+    render() {
+        const {list} = this.state;
+
+        if (list.length === 0) {
+            return null;
+        }
+        return (
+            <div className="feedback-container">
+                {list.map((x) => {
+                    switch (x.type) {
+                        case "success":
+                            return (
+                                <div key={x.id} className="feedback-success">
+                                    {checkSvg} {x.message}
+                                </div>
+                            );
+                        case "error":
+                            return (
+                                <div key={x.id} className="feedback-error">
+                                    {alertCircleSvg} {x.message}
+                                </div>
+                            );
+                        default:
+                            return null;
+                    }
+                })}
+            </div>
+        );
+    }
 }
