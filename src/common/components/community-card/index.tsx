67fde261a3d730426f2a8b0e1ea91ba67e90e340
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-09-12 14:38:44 +0300

    checkpoint

--- src/common/components/community-card/index.tsx
+++ src/common/components/community-card/index.tsx
@@ -1,5 +1,7 @@
 import React, {Component} from "react";
 
+import {Modal} from "react-bootstrap";
+
 import {History} from "history";
 
 import isEqual from "react-fast-compare";
@@ -20,9 +22,9 @@ import {_t} from "../../i18n";
 import {
     informationOutlineSvg,
     scriptTextOutlineSvg,
-    earthSvg,
     accountGroupSvg
 } from "../../img/svg";
+import {Activities} from "../community-activities";
 
 interface Props {
     history: History;
@@ -31,13 +33,52 @@ interface Props {
     addAccount: (data: Account) => void;
 }
 
-export class CommunityCard extends Component<Props> {
-    shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
-        return !isEqual(this.props.community, nextProps.community);
+interface State {
+    info: JSX.Element | null
+}
+
+export class CommunityCard extends Component<Props, State> {
+    state: State = {
+        info: null
+    }
+
+    shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>): boolean {
+        return !isEqual(this.props.community, nextProps.community)
+            || !isEqual(this.state, nextState);
+    }
+
+    toggleInfo = (info: JSX.Element | null) => {
+        this.setState({info});
     }
 
     render() {
+        const {info} = this.state;
         const {community} = this.props;
+
+        const description: JSX.Element | null = community.description.trim() !== "" ?
+            <>{ln2list(community.description).map((x, i) => (
+                <p key={i}>{x}</p>
+            ))}</> : null;
+
+        const rules: JSX.Element | null = community.flag_text.trim() !== "" ?
+            <>{ln2list(community.flag_text).map((x, i) => (
+                <p key={i}>{'- '}{x}</p>
+            ))}</> : null;
+
+        const team: JSX.Element = <>{
+            community.team.map((m, i) => {
+                if (m[0].startsWith("hive-")) {
+                    return null;
+                }
+
+                return (
+                    <div className="team-member" key={i}>
+                        {ProfileLink({...this.props, username: m[0], children: <a className="username">{`@${m[0]}`}</a>})}
+                        <span className="role">{m[1]}</span>
+                        {m[2] !== "" && <span className="extra">{m[2]}</span>}
+                    </div>
+                );
+            })}</>;
 
         return (
             <div className="community-card">
@@ -56,60 +97,62 @@ export class CommunityCard extends Component<Props> {
                         {community.about}
                     </div>
                 </div>
-                {community.description.trim() !== "" && (
-                    <div className="community-section">
-                        <div className="section-header">
-                            {informationOutlineSvg} {_t('community.description')}
+                <div className="community-sections">
+                    {description && (
+                        <div className="community-section">
+                            <div className="section-header" onClick={() => {
+                                this.toggleInfo(description);
+                            }}>
+                                {informationOutlineSvg} {_t('community.description')}
+                            </div>
+                            <div className="section-content">
+                                {description}
+                            </div>
+                        </div>
+                    )}
+                    {rules && (
+                        <div className="community-section">
+                            <div className="section-header" onClick={() => {
+                                this.toggleInfo(rules);
+                            }}>
+                                {scriptTextOutlineSvg} {_t('community.rules')}
+                            </div>
+                            <div className="section-content">
+                                {rules}
+                            </div>
+                        </div>
+                    )}
+                    <div className="community-section section-team">
+                        <div className="section-header" onClick={() => {
+                            this.toggleInfo(team);
+                        }}>
+                            {accountGroupSvg} {_t('community.team')}
                         </div>
                         <div className="section-content">
-                            {ln2list(community.description).map((x, i) => (
-                                <p key={i}>{x}</p>
-                            ))}
+                            {team}
                         </div>
                     </div>
-                )}
-                {community.flag_text.trim() !== "" && (
-                    <div className="community-section">
-                        <div className="section-header">
-                            {scriptTextOutlineSvg} {_t('community.rules')}
+                    {/*community.lang.trim() !== "" && (
+                        <div className="community-section">
+                            <div className="section-header">
+                                {earthSvg} {_t('community.lang')}
+                            </div>
+                            <div className="section-content">
+                                {community.lang.toUpperCase()}
+                            </div>
                         </div>
-                        <div className="section-content">
-                            {ln2list(community.flag_text).map((x, i) => (
-                                <p key={i}>{'- '}{x}</p>
-                            ))}
-                        </div>
-                    </div>
-                )}
-
-                <div className="community-section section-team">
-                    <div className="section-header">
-                        {accountGroupSvg} {_t('community.team')}
-                    </div>
-                    <div className="section-content">
-                        {community.team.map((m, i) => {
-                            if (m[0].startsWith("hive-")) {
-                                return null;
-                            }
-                            return (
-                                <div className="team-member" key={i}>
-                                    {ProfileLink({...this.props, username: m[0], children: <a className="username">{`@${m[0]}`}</a>})}
-                                    <span className="role">{m[1]}</span>
-                                    {m[2] !== "" && <span className="extra">{m[2]}</span>}
-                                </div>
-                            );
-                        })}
-                    </div>
+                    )*/}
                 </div>
 
-                {community.lang.trim() !== "" && (
-                    <div className="community-section">
-                        <div className="section-header">
-                            {earthSvg}  {_t('community.lang')}
-                        </div>
-                        <div className="section-content">
-                            {community.lang.toUpperCase()}
-                        </div>
-                    </div>
+                {info && (
+                    <Modal show={true} centered={true} onHide={() => {
+                        this.toggleInfo(null);
+                    }} animation={false} className="community-info-dialog modal-thin-header">
+                        <Modal.Header closeButton={true}/>
+                        <Modal.Body>
+                            {info}
+                        </Modal.Body>
+                    </Modal>
                 )}
             </div>
         );
