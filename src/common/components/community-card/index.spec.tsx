7c4466c42ee420588c593d5a33ca55a777c23b6b
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-09-18 10:47:40 +0300

    test update

--- src/common/components/community-card/index.spec.tsx
+++ src/common/components/community-card/index.spec.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-import {CommunityCard} from "./index";
 import renderer from "react-test-renderer";
 import {createBrowserHistory} from "history";
+import {StaticRouter} from "react-router-dom";
 
-import {communityInstance1, globalInstance} from "../../helper/test-helper";
+import {CommunityCard} from "./index";
+
+import {communityInstance1, globalInstance, activeUserMaker} from "../../helper/test-helper";
 
 it("(1) Default render", () => {
     const props = {
@@ -17,5 +19,22 @@ it("(1) Default render", () => {
     };
 
     const component = renderer.create(<CommunityCard {...props} />);
+    expect(component.toJSON()).toMatchSnapshot();
+});
+
+it("(2) Should show edit roles button", () => {
+    const props = {
+        history: createBrowserHistory(),
+        global: globalInstance,
+        community: {...communityInstance1},
+        activeUser: activeUserMaker("hive-148441"),
+        addAccount: () => {
+        }
+    };
+
+    const component = renderer.create(
+        <StaticRouter location="/hive-148441" context={{}}>
+            <CommunityCard {...props} />
+        </StaticRouter>);
     expect(component.toJSON()).toMatchSnapshot();
 });
