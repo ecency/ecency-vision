cc0350e6b6c5bdf9c2e56fff5ac0713e29cfd8e0
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-09-18 14:08:38 +0300

    navbar improvements for small screens

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -574,5 +574,10 @@ export const tagSvg = (<svg viewBox="0 0 24 24">
 </svg>);
 
 export const scriptTextOutlineSvg = (<svg viewBox="0 0 24 24">
-    <path fill="currentColor" d="M15,20A1,1 0 0,0 16,19V4H8A1,1 0 0,0 7,5V16H5V5A3,3 0 0,1 8,2H19A3,3 0 0,1 22,5V6H20V5A1,1 0 0,0 19,4A1,1 0 0,0 18,5V9L18,19A3,3 0 0,1 15,22H5A3,3 0 0,1 2,19V18H13A2,2 0 0,0 15,20M9,6H14V8H9V6M9,10H14V12H9V10M9,14H14V16H9V14Z" />
-</svg>)
+    <path fill="currentColor"
+          d="M15,20A1,1 0 0,0 16,19V4H8A1,1 0 0,0 7,5V16H5V5A3,3 0 0,1 8,2H19A3,3 0 0,1 22,5V6H20V5A1,1 0 0,0 19,4A1,1 0 0,0 18,5V9L18,19A3,3 0 0,1 15,22H5A3,3 0 0,1 2,19V18H13A2,2 0 0,0 15,20M9,6H14V8H9V6M9,10H14V12H9V10M9,14H14V16H9V14Z"/>
+</svg>);
+
+export const menuSvg = (<svg viewBox="0 0 24 24">
+    <path fill="currentColor" d="M3,6H21V8H3V6M3,11H21V13H3V11M3,16H21V18H3V16Z"/>
+</svg>);
