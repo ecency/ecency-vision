bb3a975e9bbe727a63dd6cf45c9b0c0c3d48a243
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-10-07 13:10:18 +0300

    checkpoint

--- src/desktop/app/index.tsx
+++ src/desktop/app/index.tsx
@@ -4,20 +4,55 @@ import {render} from 'react-dom';
 import {Provider} from "react-redux";
 import {ConnectedRouter} from "connected-react-router";
 
-import configureStore from "../../common/store/configure";
+import _c from 'js-cookie'
 
-import {history} from "../../common/store";
+import {AppState, history} from "../../common/store";
+import {ListStyle, Theme} from "../../common/store/global/types";
+import {Global} from "../../common/store/global/types";
+import configureStore from "../../common/store/configure";
+import initialState from "../../common/store/initial-state";
 
 import App from "../../common/app";
+
+import defaults from "../../common/constants/defaults.json";
+
+import * as ls from "../../common/util/local-storage";
 
 import "typeface-ibm-plex-sans";
 
 import "../../style/theme-day.scss";
 import "../../style/theme-night.scss";
 
-const store = configureStore(null);
 
-console.log(store.getState())
+// Create store
+
+const theme = _c.get("theme") || defaults.theme;
+const intro = !_c.get("hide-intro");
+const listStyle = _c.get("list-style") || defaults.listStyle;
+
+const globalState: Global = {
+    ...initialState.global,
+    theme: Theme[theme],
+    listStyle: ListStyle[listStyle],
+    intro
+};
+
+const activeUser = ls.get("active_user");
+
+const preloadedState: AppState = {
+    ...initialState,
+    global: globalState,
+    activeUser: activeUser ? {
+        username: activeUser,
+        data: {name: activeUser},
+        points: {
+            points: "0.000",
+            uPoints: "0.000"
+        }
+    } : initialState.activeUser,
+}
+
+const store = configureStore(preloadedState);
 
 document.addEventListener('DOMContentLoaded', () => {
     render(
