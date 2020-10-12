5f075b9eaaba9b507be095bda51930bb0c64bc7a
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-10-12 16:56:37 +0300

    close handler

--- src/desktop/app/main.dev.ts
+++ src/desktop/app/main.dev.ts
@@ -112,11 +112,7 @@ const createWindow = async () => {
  */
 
 app.on('window-all-closed', () => {
-    // Respect the OSX convention of having the application in memory even
-    // after all windows have been closed
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+    app.quit();
 });
 
 if (process.env.E2E_BUILD === 'true') {
