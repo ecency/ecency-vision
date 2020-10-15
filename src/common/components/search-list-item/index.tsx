cb6535be36ada504573da4beaad89c7a3912a1b4
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-10-15 21:59:29 +0300

    search page is done

---
+++ src/common/components/search-list-item/index.tsx
@@ -0,0 +1,154 @@
+import React, {Component} from "react";
+
+import {History} from "history";
+
+import moment from "moment";
+
+import {Global} from "../../store/global/types";
+import {Account} from "../../store/accounts/types";
+
+
+import EntryLink from "../entry-link";
+import ProfileLink from "../profile-link";
+import UserAvatar from "../user-avatar";
+import Tag from "../tag";
+import FormattedCurrency from "../formatted-currency";
+
+import defaults from "../../constants/defaults.json";
+
+import {
+    catchPostImage,
+    postBodySummary,
+    setProxyBase,
+    // @ts-ignore
+} from "@esteemapp/esteem-render-helpers";
+
+setProxyBase(defaults.imageServer);
+
+import {SearchResult} from "../../api/private";
+
+import {peopleSvg, commentSvg} from "../../img/svg";
+
+const fallbackImage = require("../../img/fallback.png");
+const noImage = require("../../img/noimage.png");
+
+
+interface Props {
+    history: History;
+    global: Global;
+    addAccount: (data: Account) => void;
+    res: SearchResult;
+}
+
+class SearchListItem extends Component<Props> {
+    shouldComponentUpdate(): boolean {
+        return false;
+    }
+
+    render() {
+        const {global, res} = this.props;
+
+        const entry = {
+            category: res.category,
+            author: res.author,
+            permlink: res.permlink
+        }
+
+        const summary: string = postBodySummary(res.body, 200);
+        const img: string = (global.canUseWebp ? catchPostImage(res.body, 600, 500, 'webp') : catchPostImage(res.body, 600, 500)) || noImage;
+
+        let thumb = (
+            <img src={img} alt={res.title} onError={(e: React.SyntheticEvent) => {
+                const target = e.target as HTMLImageElement;
+                target.src = fallbackImage;
+            }}/>
+        );
+
+        const date = moment(res.created_at);
+        const dateRelative = date.fromNow(true);
+        const dateFormatted = date.format("LLLL");
+
+        return <div className="search-list-item">
+            <div className="item-header">
+                <div className="author-part">
+                    {ProfileLink({
+                        ...this.props,
+                        username: res.author,
+                        children: <a className="author-avatar">{UserAvatar({...this.props, username: res.author, size: "small"})}</a>
+                    })}
+                    {ProfileLink({
+                        ...this.props,
+                        username: res.author,
+                        children: <div className="author">{res.author}
+                            <span className="author-reputation">{res.author_rep.toFixed(0)}</span>
+                        </div>
+                    })}
+                </div>
+                {Tag({
+                    ...this.props,
+                    tag: res.category,
+                    type: "link",
+                    children: <a className="category">{res.category}</a>
+                })}
+
+                <span className="date" title={dateFormatted}>{dateRelative}</span>
+            </div>
+            <div className="item-body">
+                <div className="item-image">
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <div>
+                            {thumb}
+                        </div>
+                    })}
+                </div>
+                <div className="item-summary">
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <div className="item-title">{res.title}</div>
+                    })}
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <div className="item-body">{summary}</div>
+                    })}
+                </div>
+                <div className="item-controls">
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <a className="result-payout">
+                            <FormattedCurrency {...this.props} value={res.payout}/>
+                        </a>
+                    })}
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <a className="result-votes"> {peopleSvg} {res.total_votes}</a>
+                    })}
+                    {EntryLink({
+                        ...this.props,
+                        entry,
+                        children: <a className="result-replies">{commentSvg} {res.children}</a>
+                    })}
+                    <div className="app">{res.app}</div>
+                </div>
+            </div>
+        </div>
+    }
+}
+
+
+export default (p: Props) => {
+
+    const props: Props = {
+        history: p.history,
+        global: p.global,
+        addAccount: p.addAccount,
+        res: p.res
+    }
+
+    return <SearchListItem {...props} />;
+}
--- src/common/pages/search.tsx
+++ src/common/pages/search.tsx
@@ -1,17 +1,10 @@
-import React, {Component} from "react";
+import React, {Component, Fragment} from "react";
 
 import {connect} from "react-redux";
 
 import queryString from "query-string";
 
-import {History} from "history";
-
 import {Form, FormControl} from "react-bootstrap";
-
-import moment from "moment";
-
-import {Global} from "../store/global/types";
-import {Account} from "../store/accounts/types";
 
 import Meta from "../components/meta";
 import Theme from "../components/theme/index";
@@ -19,141 +12,14 @@ import NavBar from "../components/navbar/index";
 import NavBarElectron from "../../desktop/app/components/navbar";
 import LinearProgress from "../components/linear-progress";
 import ScrollToTop from "../components/scroll-to-top";
-import EntryLink from "../components/entry-link";
-import ProfileLink from "../components/profile-link";
-import UserAvatar from "../components/user-avatar";
-import Tag from "../components/tag";
-import FormattedCurrency from "../components/formatted-currency";
-
-import defaults from "../constants/defaults.json";
-
-import {
-    catchPostImage,
-    postBodySummary,
-    setProxyBase,
-    // @ts-ignore
-} from "@esteemapp/esteem-render-helpers";
-
-setProxyBase(defaults.imageServer);
+import DetectBottom from "../components/detect-bottom";
+import SearchListItem from "../components/search-list-item";
 
 import {search, SearchResult} from "../api/private";
 
 import {_t} from "../i18n";
 
-import {commentSvg, peopleSvg} from "../img/svg";
-
-const fallbackImage = require("../../common/img/fallback.png");
-const noImage = require("../../common/img/noimage.png");
-
 import {PageProps, pageMapDispatchToProps, pageMapStateToProps} from "./common";
-
-interface ItemProps {
-    history: History;
-    global: Global;
-    addAccount: (data: Account) => void;
-    res: SearchResult;
-}
-
-class SearchListItem extends Component<ItemProps> {
-    render() {
-        const {global, res} = this.props;
-
-        const entry = {
-            category: res.category,
-            author: res.author,
-            permlink: res.permlink
-        }
-
-        const summary: string = postBodySummary(res.body, 200);
-        const img: string = (global.canUseWebp ? catchPostImage(res.body, 600, 500, 'webp') : catchPostImage(res.body, 600, 500)) || noImage;
-
-        let thumb = (
-            <img src={img} alt={res.title} onError={(e: React.SyntheticEvent) => {
-                const target = e.target as HTMLImageElement;
-                target.src = fallbackImage;
-            }}/>
-        );
-
-        const date = moment(res.created_at);
-        const dateRelative = date.fromNow(true);
-        const dateFormatted = date.format("LLLL");
-
-        return <div className="search-list-item">
-            <div className="item-header">
-                <div className="author-part">
-                    {ProfileLink({
-                        ...this.props,
-                        username: res.author,
-                        children: <a className="author-avatar">{UserAvatar({...this.props, username: res.author, size: "small"})}</a>
-                    })}
-                    {ProfileLink({
-                        ...this.props,
-                        username: res.author,
-                        children: <div className="author">{res.author}
-                            <span className="author-reputation">{res.author_rep.toFixed(0)}</span>
-                        </div>
-                    })}
-                </div>
-                {Tag({
-                    ...this.props,
-                    tag: res.category,
-                    type: "link",
-                    children: <a className="category">{res.category}</a>
-                })}
-
-                <span className="date" title={dateFormatted}>{dateRelative}</span>
-            </div>
-            <div className="item-body">
-                <div className="item-image">
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <div>
-                            {thumb}
-                        </div>
-                    })}
-                </div>
-                <div className="item-summary">
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <div className="item-title">{res.title}</div>
-                    })}
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <div className="item-body">{summary}</div>
-                    })}
-                </div>
-                <div className="item-controls">
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <div className="entry-payout">
-                            <FormattedCurrency {...this.props} value={res.payout}/>
-                        </div>
-                    })}
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <div className="entry-votes">
-                            <span className="inner-btn no-data"> {peopleSvg} {res.total_votes}</span>
-                        </div>
-                    })}
-                    {EntryLink({
-                        ...this.props,
-                        entry,
-                        children: <a className="replies">
-                            <span className="inner">{commentSvg} {res.children}</span>
-                        </a>
-                    })}
-                    <div className="app">{res.app}</div>
-                </div>
-            </div>
-        </div>
-    }
-}
-
 
 interface State {
     q: string;
@@ -212,9 +78,10 @@ class SearchPage extends Component<PageProps, State> {
 
         this.stateSet({inProgress: true});
         search(q, sort, (results.length > 0 && scroll_id ? scroll_id : undefined)).then(r => {
+            const newResults = [...results, ...r.results]
             this.stateSet({
                 hits: r.hits,
-                results: r.results,
+                results: newResults,
                 scroll_id: r.scroll_id
             })
         }).finally(() => {
@@ -241,6 +108,15 @@ class SearchPage extends Component<PageProps, State> {
             this.setState(state, cb);
         }
     };
+
+    bottomReached = () => {
+        const {inProgress, scroll_id} = this.state;
+        if (inProgress || !scroll_id) {
+            return;
+        }
+
+        this.doSearch();
+    }
 
     render() {
         //  Meta config
@@ -292,17 +168,14 @@ class SearchPage extends Component<PageProps, State> {
 
                     {results.length > 0 && (
                         <div className="search-list">
-                            {results.map(res => <SearchListItem
-                                key={res.permlink}
-                                history={this.props.history}
-                                global={this.props.global}
-                                addAccount={this.props.addAccount}
-                                res={res}
-                            />)}
+                            {results.map(res => <Fragment key={res.permlink}>
+                                {SearchListItem({...this.props, res: res})}
+                            </Fragment>)}
                         </div>
                     )}
 
                     {inProgress && <LinearProgress/>}
+                    <DetectBottom onBottom={this.bottomReached}/>
                 </div>
             </>
         );
