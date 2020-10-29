f5fb989585755509c86143e9d518e39a2003e7ae
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-10-29 17:25:39 +0300

    improvements

--- src/common/components/entry-body-extra/index.tsx
+++ src/common/components/entry-body-extra/index.tsx
@@ -1,20 +1,35 @@
 import React, {Component} from "react";
 
+import mediumZoom, {Zoom} from 'medium-zoom';
+
 import {Entry} from "../../store/entries/types";
+
+import {AppWindow} from "../../../client/window";
+
+declare var window: AppWindow;
 
 interface Props {
     entry: Entry
 }
 
 class EntryBodyExtra extends Component<Props> {
+    zoom: Zoom | null = null;
+
     componentDidMount() {
         const {entry} = this.props;
 
-        if (entry.body.indexOf('https://twitter.com/') > -1) {
-            const twttr = window['twttr'];
-            if (twttr && twttr.widgets) {
-                twttr.widgets.load();
-            }
+        // Tweet renderer
+        if (/(?:https?:\/\/(?:(?:twitter\.com\/(.*?)\/status\/(.*))))/gi.test(entry.body)) {
+            window.twttr?.widgets?.load();
+        }
+
+        // Medium style image zoom
+        const elements: HTMLElement[] = [...document.querySelectorAll<HTMLElement>(".entry-body img")]
+            .filter(x => x.parentNode?.nodeName !== "A");
+        this.zoom = mediumZoom(elements);
+
+        if (this.zoom) {
+            this.zoom.detach();
         }
     }
 
