6b095f7a1b1370a660b76fd3ce0da2742baa8c9e
feruz <fmiboy@gmail.com>
2020-11-12 13:12:41 +0200

    update download links

--- src/common/components/download-trigger/index.tsx
+++ src/common/components/download-trigger/index.tsx
@@ -52,24 +52,24 @@ export default class DownloadTrigger extends Component <Props, State> {
                   <div className="download-buttons">
                     {(os !== 'iOS' && os !== 'AndroidOS' && os === 'WinOS') &&
                       <a className="download-button btn-desktop" target="_blank"
-                        href="https://github.com/ecency/esteem-surfer/releases/download/2.2.7/Esteem-Setup-2.2.7.exe" rel="noopener noreferrer">Windows</a>
+                        href="https://github.com/ecency/ecency-vision/releases/download/3.0.9/Ecency-Setup-3.0.9.exe" rel="noopener noreferrer">Windows</a>
                     }
                     {(os !== 'iOS' && os !== 'AndroidOS' && os === 'MacOS') &&
                       <a className="download-button btn-desktop" target="_blank"
-                        href="https://github.com/ecency/esteem-surfer/releases/download/2.2.7/Esteem-2.2.7.dmg" rel="noopener noreferrer">Mac</a>
+                        href="https://github.com/ecency/ecency-vision/releases/download/3.0.9/Ecency-3.0.9.dmg" rel="noopener noreferrer">Mac</a>
                     }
                     {(os !== 'iOS' && os !== 'AndroidOS' && (os === 'UnixOS' || os === 'LinuxOS')) &&
                       <a className="download-button btn-desktop" target="_blank"
-                        href="https://github.com/ecency/esteem-surfer/releases/download/2.2.7/esteem-surfer-2.2.7.tar.gz" rel="noopener noreferrer">Linux</a>
+                        href="https://github.com/ecency/ecency-vision/releases/download/3.0.9/ecency-surfer-3.0.9.tar.gz" rel="noopener noreferrer">Linux</a>
                     }
                     {(os === 'AndroidOS' || os !== 'iOS') && 
                       <a className="download-button btn-android" target="_blank"
-                        href="https://play.google.com/store/apps/details?id=app.esteem.mobile.android"
+                        href="https://android.ecency.com"
                         rel="noopener noreferrer">Android</a>
                     }
                     {(os === 'iOS' || os !== 'AndroidOS') &&
                       <a className="download-button btn-ios" target="_blank"
-                        href="https://apps.apple.com/us/app/esteem-v2/id1451896376" rel="noopener noreferrer">iOS</a>
+                        href="https://ios.ecency.com" rel="noopener noreferrer">iOS</a>
                     }
                   </div>
                 </div>
