a6a50aa1c17f5dbc14f473849a584307f2e6fb11
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-11-28 23:49:33 +0300

    tooltip

--- src/common/components/market-data/index.tsx
+++ src/common/components/market-data/index.tsx
@@ -16,6 +16,11 @@ import {_t} from "../../i18n";
 
 import {Tsx} from "../../i18n/helper";
 
+interface Price {
+    time: number;
+    price: number;
+}
+
 interface Props {
     label: string;
     coin: string;
@@ -25,36 +30,107 @@ interface Props {
 }
 
 interface State {
-    series: number[]
+    prices: Price[]
 }
 
 export class Market extends Component<Props, State> {
     state: State = {
-        series: []
+        prices: []
     }
+
+    node = React.createRef<HTMLDivElement>();
 
     componentDidMount() {
         const {coin, vsCurrency, fromTs, toTs} = this.props;
 
         getMarketData(coin, vsCurrency, fromTs, toTs).then((r) => {
             if (r && r.prices) {
-                const series = r.prices.map((x: any) => x[1]);
-                this.setState({series});
+                const prices: Price[] = r.prices.map((x: any) => ({time: x[0], price: x[1]}));
+                this.setState({prices}, () => {
+                    this.attachEvents();
+                });
             }
         });
+    }
+
+    componentWillUnmount() {
+        this.detachEvents();
     }
 
     shouldComponentUpdate(nextProps: Readonly<Props>, nextState: Readonly<State>): boolean {
         return !isEqual(this.state, nextState);
     }
 
+    attachEvents = () => {
+        const node = this.node.current;
+        if (!node) return;
+
+        const graph = node.querySelector(".graph")!;
+
+        node.querySelectorAll('.ct-point').forEach(el => {
+            const left = el.getAttribute("x1");
+
+            const graphBar = document.createElement("span");
+            graphBar.setAttribute("class", "graph-bar");
+            graphBar.style.left = `${left}px`;
+
+            graphBar.addEventListener('mouseover', this.pointMouseMove);
+            graphBar.addEventListener('mouseout', this.pointMouseOut);
+
+            graph.appendChild(graphBar);
+        });
+    }
+
+    detachEvents = () => {
+        const node = this.node.current;
+        if (!node) return;
+
+        node.querySelectorAll('.graph-bar').forEach(el => {
+            el.removeEventListener('mouseover', this.pointMouseMove);
+            el.removeEventListener('mouseout', this.pointMouseOut);
+        });
+    }
+
+    pointMouseMove = (e: Event) => {
+        const node = this.node.current;
+        if (!node) return;
+
+        const circle = e.currentTarget;
+        const circles = node.querySelectorAll('.graph-bar');
+        const index = Array.prototype.indexOf.call(circles, circle);
+        const item = this.state.prices[index];
+
+        const strPrice = numeral(item.price).format(this.priceFormatter());
+        const strDate = moment(item.time).format("YYYY-MM-DD HH:mm:ss");
+        const html = `<strong>${strPrice}</strong> ${strDate}`;
+
+        const tooltip = node.querySelector(".tooltip") as HTMLElement;
+        tooltip.style.visibility = "visible";
+        tooltip!.innerHTML = html;
+    }
+
+    pointMouseOut = () => {
+        const node = this.node.current;
+        if (!node) return;
+
+        const tooltip = node.querySelector(".tooltip") as HTMLElement;
+        tooltip.style.visibility = "hidden";
+        tooltip!.innerHTML = "";
+    }
+
+    priceFormatter = () => {
+        const {coin,} = this.props;
+        return coin === "bitcoin" ? ",$" : "0.000$";
+    }
+
     render() {
-        const {series} = this.state;
+        const {label} = this.props;
+        const {prices} = this.state;
 
         const options: ILineChartOptions = {
             width: "100%",
             height: "90px",
-            showPoint: false,
+            showPoint: true,
             showArea: true,
             lineSmooth: false,
             fullWidth: true,
@@ -76,23 +152,24 @@ export class Market extends Component<Props, State> {
             }
         };
 
-        const data = {
-            series: [series]
-        };
-
-        const {coin, label} = this.props;
+        const data = {series: [prices.map(x => x.price)]};
 
         let strPrice = "...";
-        if (series.length) {
-            const price = series[series.length - 1];
-            const formatter = coin === "bitcoin" ? ",$" : "0.00$";
-
-            strPrice = numeral(price).format(formatter)
+        if (prices.length) {
+            const price = prices[prices.length - 1].price;
+            strPrice = numeral(price).format(this.priceFormatter());
         }
 
-        return <div className="market-graph">
-            <div className="graph"><Graph data={data} options={options} type="Line"/></div>
-            <div className="price"><span className="coin">{label}</span>{" "}<span className="value">{strPrice}</span></div>
+        return <div className="market-graph" ref={this.node}>
+            <div className="graph">
+                <Graph data={data} options={options} type="Line"/>
+            </div>
+            <div className="info">
+                <div className="price">
+                    <span className="coin">{label}</span>{" "}<span className="value">{strPrice}</span>
+                </div>
+                <div className="tooltip"/>
+            </div>
         </div>;
     }
 }
@@ -107,7 +184,9 @@ export default class MarketData extends Component {
             <Market label="HIVE" coin="hive" vsCurrency="usd" fromTs={fromTs} toTs={toTs}/>
             <Market label="HBD" coin="hive_dollar" vsCurrency="usd" fromTs={fromTs} toTs={toTs}/>
             <Market label="BTC" coin="bitcoin" vsCurrency="usd" fromTs={fromTs} toTs={toTs}/>
-            <Tsx k="market-data.credits" args={{}}><div className="credits"/></Tsx>
+            <Tsx k="market-data.credits" args={{}}>
+                <div className="credits"/>
+            </Tsx>
         </div>
     }
 }
