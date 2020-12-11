a7dc6c84a5a6d6d31de13654abea9df4fddf4b91
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2020-12-11 17:58:33 +0300

    test fix

--- src/common/components/pin-btn/index.spec.tsx
+++ src/common/components/pin-btn/index.spec.tsx
@@ -5,6 +5,27 @@ import TestRenderer from "react-test-renderer";
 
 import {entryInstance1, communityInstance1, activeUserMaker} from "../../helper/test-helper";
 
+let TEST_MODE = 0;
+
+jest.mock("../../api/bridge", () => ({
+    getPostsRanked: () =>
+        new Promise((resolve) => {
+            if (TEST_MODE === 0) {
+                resolve([]);
+            }
+
+            if (TEST_MODE === 1) {
+                const entry = {
+                    ...entryInstance1,
+                    stats: {...entryInstance1.stats, is_pinned: true}
+                }
+                resolve([entry]);
+            }
+        }),
+}));
+
+const allOver = () => new Promise((resolve) => setImmediate(resolve));
+
 const defProps = {
     entry: {...entryInstance1},
     community: {...communityInstance1},
@@ -13,18 +34,17 @@ const defProps = {
     }
 };
 
-it("(1) Should show 'pin' label", () => {
+it("(1) Should show 'pin' label", async () => {
     const props = {...defProps};
     const renderer = TestRenderer.create(<PinBtn {...props} />);
+    await allOver();
     expect(renderer.toJSON()).toMatchSnapshot();
 });
 
 
-it("(2) Should show 'unpin' label", () => {
-    const stats = {...entryInstance1.stats!, is_pinned: true};
-    const entry = {...entryInstance1, stats};
-
-    const props = {...defProps, entry: entry};
-    const renderer = TestRenderer.create(<PinBtn {...props} />);
+it("(2) Should show 'unpin' label", async() => {
+    TEST_MODE = 1;
+    const renderer = TestRenderer.create(<PinBtn {...defProps} />);
+    await allOver();
     expect(renderer.toJSON()).toMatchSnapshot();
 });
