99c98f979549bce93e08117afae8c00d040038c0
feruzm <fmiboy@gmail.com>
2020-12-21 12:53:34 +0200

    enable more languages

--- src/common/i18n/index.ts
+++ src/common/i18n/index.ts
@@ -8,13 +8,38 @@ export const langOptions = [
         name: "English"
     },
     {
+        code: "es-ES",
+        name: "Español"
+    },
+    {
+        code: "hi-IN",
+        name: "Hindī"
+    },
+    {
+        code: "id-ID",
+        name: "Bahasa Indonesia"
+    },
+    {
+        code: "pt-PT",
+        name: "Português"
+    },
+    {
+        code: "sr-CS",
+        name: "Srpski"
+    },
+    {
         code: "fi-FI",
-        name: "Finnish"
+        name: "Suomi"
     }
 ];
 
 const enUs = require("./locales/en-US.json");
 const fiFI = require("./locales/fi-FI.json");
+const esES = require("./locales/es-ES.json");
+const hiIN = require("./locales/hi-IN.json");
+const idID = require("./locales/id-ID.json");
+const ptPT = require("./locales/pt-PT.json");
+const srCS = require("./locales/sr-CS.json");
 
 const resources = {
     ["en-US"]: {
@@ -22,6 +47,21 @@ const resources = {
     },
     ["fi-FI"]: {
         translation: fiFI
+    },
+    ["es-ES"]: {
+        translation: esES
+    },
+    ["hi-IN"]: {
+        translation: hiIN
+    },
+    ["id-ID"]: {
+        translation: idID
+    },
+    ["pt-PT"]: {
+        translation: ptPT
+    },
+    ["sr-CS"]: {
+        translation: srCS
     }
 };
 
