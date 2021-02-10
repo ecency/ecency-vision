e1901dfad4cc48d04f76c24d3cd3753b2b04bd37
Talha Buğra Bulut <talhabugrabulut@gmail.com>
2021-02-10 17:19:29 +0300

    tests

---
+++ src/common/components/entry-tip-btn/index.spec.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+
+import EntryTipBtn, {TippingDialog} from "./index";
+
+import {globalInstance, dynamicPropsIntance1, UiInstance, entryInstance1, fullAccountInstance} from "../../helper/test-helper";
+
+import TestRenderer from "react-test-renderer";
+
+const defProps = {
+    global: globalInstance,
+    dynamicProps: dynamicPropsIntance1,
+    users: [],
+    ui: UiInstance,
+    activeUser: {
+        username: 'foo',
+        data: {
+            ...fullAccountInstance,
+        },
+        points: {
+            points: "200.000",
+            uPoints: "0.000"
+        }
+    },
+    entry: entryInstance1,
+    signingKey: "",
+    addAccount: () => {
+    },
+    setActiveUser: () => {
+    },
+    updateActiveUser: () => {
+    },
+    deleteUser: () => {
+    },
+    toggleUIProp: () => {
+    },
+    setSigningKey: () => {
+    }
+};
+
+it("(1) Default render", async () => {
+    const renderer = TestRenderer.create(<EntryTipBtn {...defProps} />);
+
+    expect(renderer.toJSON()).toMatchSnapshot();
+});
+
+it("(2) Dialog", async () => {
+    const props = {
+        ...defProps,
+        onHide: () => {
+        }
+    }
+
+    const renderer = TestRenderer.create(<TippingDialog {...props} />);
+    expect(renderer.toJSON()).toMatchSnapshot();
+});
