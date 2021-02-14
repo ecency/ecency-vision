b3712d111183ec9dd18a37a887f9488546efac2f
Bartłomiej Górnicki <bgor91@gmail.com>
2021-02-14 23:06:32 +0100

    feature: build and push docker images to the official hub

--- src/config.example.ts
+++
@@ -1,9 +0,0 @@
-/* !!! DO NOT IMPORT config.js TO FRONTEND CODE !!! */
-
-export default {
-    privateApiAddr: "https://domain.com/api",
-    privateApiAuth: {user: "pass"},
-    hsClientSecret: "hivesignerclientsecret",
-    searchApiAddr: "https://api.search.com",
-    searchApiToken: "searchApiSecret"
-};
---
+++ src/config.ts
@@ -0,0 +1,11 @@
+/* !!! DO NOT IMPORT config.js TO FRONTEND CODE !!! */
+
+export default {
+    privateApiAddr: process.env.PRIVATE_API_ADDR || "https://domain.com/api",
+    privateApiAuth: {
+        user: process.env.PRIVATE_API_PASSWORD || "pass"
+    },
+    hsClientSecret: process.env.HIVESIGNER_CLIENT_SECRET || "hivesignerclientsecret",
+    searchApiAddr: process.env.SEARCH_API_ADDR || "https://api.search.com",
+    searchApiToken: process.env.SEARCH_API_SECRET || "searchApiSecret"
+};
