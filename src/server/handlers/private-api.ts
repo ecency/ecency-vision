faab28938728815d0f01c43256d0b72066073d04
feruz <fmiboy@gmail.com>
2021-02-18 10:21:34 +0200

    fix

--- src/server/handlers/private-api.ts
+++ src/server/handlers/private-api.ts
@@ -8,8 +8,7 @@ import {getTokenUrl, decodeToken} from "../../common/helper/hive-signer";
 
 import {apiRequest, baseApiRequest, getPromotedEntries} from "../helper";
 
-import * as hs from 'hivesigner-sdk';
-//const hs = require("hivesigner-sdk");
+const hs = require("hivesigner");
 
 const validateCode = async (req: express.Request, res: express.Response): Promise<string | false> => {
     const {code} = req.body;
