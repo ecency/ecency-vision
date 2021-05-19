7bf01fc6bb60e93547acf0ffeb98c603998bc8ec
feruz <fmiboy@gmail.com>
2021-05-19 07:00:14 +0300

    webp

--- src/common/helper/posting.ts
+++ src/common/helper/posting.ts
@@ -25,7 +25,7 @@ export const createPermlink = (title: string, random: boolean = false): string =
         perm = `${perm}-${rnd}`;
     }
 
-    // STEEMIT_MAX_PERMLINK_LENGTH
+    // HIVE_MAX_PERMLINK_LENGTH
     if (perm.length > 255) {
         perm = perm.substring(perm.length - 255, perm.length);
     }
@@ -43,7 +43,7 @@ export const createPermlink = (title: string, random: boolean = false): string =
 export const extractMetaData = (body: string): MetaData => {
     const urlReg = /(\b(https?|ftp):\/\/[A-Z0-9+&@#/%?=~_|!:,.;-]*[-A-Z0-9+&@#/%=~_|])/gim;
     const userReg = /(^|\s)(@[a-z][-.a-z\d]+[a-z\d])/gim;
-    const imgReg = /(https?:\/\/.*\.(?:tiff?|jpe?g|gif|png|svg|ico|heic))/gim;
+    const imgReg = /(https?:\/\/.*\.(?:tiff?|jpe?g|gif|png|svg|ico|heic|webp))/gim;
 
     const out: MetaData = {};
 
