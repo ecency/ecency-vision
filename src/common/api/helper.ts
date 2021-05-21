a3cdd0be9bb79c5183d421ed856bb6757d78a98f
talhasch <talhabugrabulut@gmail.com>
2021-05-21 16:09:37 +0300

    secrets deleted

--- src/common/api/helper.ts
+++ src/common/api/helper.ts
@@ -1,17 +1,3 @@
-import isElectron from "../util/is-electron";
-
 import defaults from "../constants/defaults.json";
 
-export const apiBase = (endpoint: string): string => {
-    let base = '';
-
-    if (isElectron()) {
-        if (process.env.NODE_ENV === "development") {
-            base = "http://localhost:3000";
-        } else {
-            base = defaults.base;
-        }
-    }
-
-    return `${base}${endpoint}`;
-}
+export const apiBase = (endpoint: string): string => `${defaults.base}${endpoint}`;
--- src/server/helper.ts
+++ src/server/helper.ts
@@ -1,19 +1,13 @@
-import axios, {AxiosResponse, Method} from "axios";
+import axios from "axios";
 
 import {Entry} from "../common/store/entries/types";
 import {DynamicProps} from "../common/store/dynamic-props/types";
 
 import {initialState as dynamicPropsInitialState} from "../common/store/dynamic-props";
 
-import {getPost} from "../common/api/bridge";
-
 import * as hiveApi from "../common/api/hive";
 
 import {cache} from "./cache";
-
-import config from "../config";
-
-import {baseApiRequest} from "./util";
 
 export const optimizeEntries = (entries: Entry[]): Entry[] => {
     return entries;
@@ -25,59 +19,6 @@ export const optimizeEntries = (entries: Entry[]): Entry[] => {
         };
     }); */
 };
-
-const makeApiAuth = () => {
-    try {
-        const auth = new Buffer(config.privateApiAuth, "base64").toString("utf-8");
-        return JSON.parse(auth);
-    } catch (e) {
-        return null;
-    }
-}
-
-export const apiRequest = (endpoint: string, method: Method, extraHeaders: any = {}, payload: any = {}): Promise<AxiosResponse> | Promise<any> => {
-    const apiAuth = makeApiAuth();
-    if (!apiAuth) {
-        return new Promise((resolve, reject) => {
-            console.error("Api auth couldn't be create!");
-            reject("Api auth couldn't be create!");
-        })
-    }
-
-    const url = `${config.privateApiAddr}/${endpoint}`;
-
-    const headers = {
-        "Content-Type": "application/json",
-        ...makeApiAuth(),
-        ...extraHeaders
-    }
-
-    return baseApiRequest(url, method, headers, payload)
-}
-
-export const fetchPromotedEntries = async (): Promise<Entry[]> => {
-    // fetch list from api
-    const list: { author: string, permlink: string, post_data?: Entry }[] = (await apiRequest('promoted-posts?limit=200', 'GET')).data;
-
-    // random sort & random pick 18 (6*3)
-    const promoted = list.sort(() => Math.random() - 0.5).filter((x, i) => i < 18);
-
-    return promoted.map(x => x.post_data).filter(x => x) as Entry[];
-}
-
-export const getPromotedEntries = async (): Promise<Entry[]> => {
-    let promoted: Entry[] | undefined = cache.get('promoted-entries');
-    if (promoted === undefined) {
-        try {
-            promoted = await fetchPromotedEntries();
-            cache.set("promoted-entries", promoted, 60);
-        } catch (e) {
-            promoted = [];
-        }
-    }
-
-    return promoted.sort(() => Math.random() - 0.5);
-}
 
 export const getSearchIndexCount = async (): Promise<number> => {
     let indexCount: number | undefined = cache.get("index-count");
