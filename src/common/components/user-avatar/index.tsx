6f8860f9f3f5b53a5f412359037320e4b7ed9f80
Serkan Akman <akmanserkan65@gmail.com>
2021-06-06 17:56:06 +0300

    Author card moved to a seperate folder as a component

--- src/common/components/user-avatar/index.tsx
+++ src/common/components/user-avatar/index.tsx
@@ -16,7 +16,7 @@ export class UserAvatar extends Component<Props> {
         const {username, size, global, src} = this.props;
         const imgSize = size === 'xLarge' ? 'large' : ((size === 'normal' || size === 'small') ? 'small' : 'medium');
         const cls = `user-avatar ${size}`;
-        const imageSrc = src || `${defaults.imageServer}${global.canUseWebp ? "/webp" : ""}/u/${username}/avatar/${imgSize}`;
+        const imageSrc = src || `${defaults.imageServer}${global?.canUseWebp ? "/webp" : ""}/u/${username}/avatar/${imgSize}`;
 
         return (
             <span className={cls} style={{backgroundImage: `url(${imageSrc})`}}/>
