7f37d1b4922ad8d862e5a9e94dd0b1629e0c7479
Fahad <ghazanfar89@gmail.com>
2021-06-12 14:00:27 +0500

    Remove feeds and implement global logic

--- src/common/components/entry-index-menu/index.tsx
+++ src/common/components/entry-index-menu/index.tsx
@@ -13,12 +13,17 @@ import ListStyleToggle from "../list-style-toggle";
 import {_t} from "../../i18n";
 
 import _c from "../../util/fix-class-names"
+import { Form } from "react-bootstrap";
 
 interface Props {
     history: History;
     global: Global;
     activeUser: ActiveUser | null;
     toggleListStyle: () => void;
+}
+
+interface States {
+    isGlobal: boolean;
 }
 
 export const isMyPage = (global: Global, activeUser: ActiveUser | null) => {
@@ -34,10 +39,34 @@ export const isActiveUser = ( activeUser: ActiveUser | null) => {
     return activeUser !== null;
 }
 
-export class EntryIndexMenu extends Component<Props> {
+export class EntryIndexMenu extends Component<Props, States> {
+    constructor(props:Props){
+        super(props)
+        this.state={ isGlobal: !this.props.history.location.pathname.includes('/my') };
+        this.onChange = this.onChange.bind(this)
+    }
+
+    onChange() {
+        const { history } = this.props;
+        this.setState({ isGlobal: !this.state.isGlobal });
+        if(history.location.pathname.includes('/my')){
+            history.push(history.location.pathname.replace('/my', ''))
+        } else {
+            history.push(history.location.pathname + '/my')
+        }
+    }
+
+    componentDidUpdate(prevProps:Props){
+        const { history, activeUser } = this.props;
+        if(history.location.pathname.includes('/my') && !isActiveUser(activeUser)){
+            history.push(history.location.pathname.replace('/my', ''))
+        }
+    }
+
     render() {
-        const {activeUser, global} = this.props;
-        const {filter, tag} = global;
+        const { activeUser, global } = this.props;
+        const { isGlobal } = this.state;
+        const { filter, tag } = global;
 
         const isMy = isMyPage(global, activeUser);
         const isActive = isActiveUser(activeUser);
@@ -53,8 +82,8 @@ export class EntryIndexMenu extends Component<Props> {
                 ...[EntryFilter.trending, EntryFilter.hot, EntryFilter.created].map((x) => {
                     return {
                         label: _t(`entry-filter.filter-${x}`),
-                        href: (tag && tag !== "my" && filter !== "feed") ? `/${x}/${tag}` : `/${x}`,
-                        active: !isMy && filter === x,
+                        href: isActive ? isGlobal ? `/${x}` : `/${x}/my` : `/${x}`,
+                        active: filter === x || filter === x + '/my',
                         id: x
                     };
                 }),
@@ -69,11 +98,6 @@ export class EntryIndexMenu extends Component<Props> {
                             <li className="nav-item">
                                 <Link to={`/@${activeUser?.username}/feed`} className={_c(`nav-link link-my ${filter === "feed" ? "active" : ""}`)}>
                                     {_t("entry-filter.filter-feed-friends")}
-                                </Link>
-                            </li>
-                            <li className="nav-item">
-                                <Link to={`/${EntryFilter.created}/my`} className={_c(`nav-link link-my ${tag === "my" ? "active" : ""}`)}>
-                                    {_t("entry-filter.filter-feed-subscriptions")}
                                 </Link>
                             </li>
                         </ul>
@@ -93,6 +117,17 @@ export class EntryIndexMenu extends Component<Props> {
                         </ul>
                     </div>
                 </div>
+                {isActive && 
+                    <Form.Check
+                        id="check-isGlobal"
+                        type="checkbox"
+                        label="Global"
+                        name="isGlobal"
+                        className="d-flex align-items-center ml-5 border-left pl-5"
+                        checked={isGlobal}
+                        onChange={this.onChange}
+                    />
+                }
             </div>
 
             <ListStyleToggle global={this.props.global} toggleListStyle={this.props.toggleListStyle}/>
