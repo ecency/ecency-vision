c91d346e5482b8ad2d483d31b43684e4170717ff
Fahad <ghazanfar89@gmail.com>
2021-07-07 21:50:08 +0500

    FOUC resolved

--- razzle.config.js
+++ razzle.config.js
@@ -2,8 +2,25 @@
 
 module.exports = {
   plugins: ['typescript', 'scss'],
-  modify: (config, { target, dev }) => {
-    config.devtool = dev ? 'source-map' : false;
-    return config;
+  options: {
+    buildType: 'spa'
   },
+  modifyWebpackConfig({
+    env: {
+      target, // the target 'node' or 'web'
+      dev, // is this a development build? true or false
+    },
+    webpackConfig, // the created webpack config
+    webpackObject, // the imported webpack node module
+    options: {
+      pluginOptions, // the options passed to the plugin ({ name:'pluginname', options: { key: 'value'}})
+      razzleOptions, // the modified options passed to Razzle in the `options` key in `razzle.config.js` (options: { key: 'value'})
+      webpackOptions, // the modified options that was used to configure webpack/ webpack loaders and plugins
+    },
+    paths, // the modified paths that will be used by Razzle.
+  }) {
+    // Do some stuff to webpackConfig
+    webpackConfig.devtool = dev ? 'source-map' : false;
+    return webpackConfig;
+  }
 };
