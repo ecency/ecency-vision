38e977a4c7479edb8a4b711b8440fa50221b0452
feruz <fmiboy@gmail.com>
2021-07-12 11:16:13 +0300

    app uri

--- src/server/handlers/fallback.tsx
+++ src/server/handlers/fallback.tsx
@@ -29,7 +29,7 @@ export const healthCheck = async (req: express.Request, res: express.Response) =
     })
 };
 
-export const appURI = async (req: express.Request, res: express.Response) => {
+export const iosURI = async (req: express.Request, res: express.Response) => {
     res.send({
         "applinks":{
             "apps":[],
@@ -50,4 +50,20 @@ export const appURI = async (req: express.Request, res: express.Response) => {
             ]
         }
     })
+};
+
+export const androidURI = async (req: express.Request, res: express.Response) => {
+    res.send(
+        [{
+            "relation": ["delegate_permission/common.handle_all_urls"],
+            "target": {
+              "namespace": "android_app",
+              "package_name": "app.esteem.mobile.android",
+              "sha256_cert_fingerprints":
+                [
+                    "4F:3F:42:16:01:72:6C:0A:BF:F1:49:EE:BE:8D:70:29:20:F4:03:F4:3F:9D:63:81:60:B6:56:34:AD:AB:0A:B2"
+                ]
+            }
+        }]
+    )
 };
