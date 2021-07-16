4b778834c8c41f2a44bd3ee05ac8d1213322a916
Fahad <ghazanfar89@gmail.com>
2021-07-16 12:26:48 +0500

    Rebase with development

--- src/desktop/configs/webpack.config.base.js
+++ src/desktop/configs/webpack.config.base.js
@@ -20,14 +20,7 @@ export default {
             cacheDirectory: true,
           },
         },
-      },
-      {
-        test: /\.(?:png|jpg|svg)$/,
-        loader: 'url-loader',
-        query: {
-        // Inline images smaller than 10kb as data URIs        limit: 10000
       }
-    }
     ],
   },
 
