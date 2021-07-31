f81bb715d908c816713f2d1285d6c52ae1615f3c
Fahad <ghazanfar89@gmail.com>
2021-07-31 22:59:01 +0500

    Fix toggle theme on every page change

--- src/common/components/theme/index.ts
+++ src/common/components/theme/index.ts
@@ -8,18 +8,13 @@ interface Props {
 export default (props: Props) => {
     useEffect(() => {
         if (['day', 'night'].includes(props.global.theme)) {
-            let body: any = document.getElementsByTagName('body');
-            if (!body) return;
-            body = body[0];
-            if(body.classList.contains("theme-day")){
-                body.classList.remove("theme-day")
-                body.classList.add("theme-night")
-            }
-            else {
-                body.classList.remove("theme-night")
-                body.classList.add("theme-day")
-            }
+       if(!props.global.isMobile) {
+            const body = document.querySelector('body');
+        if (!body) return;
+
+        body.className = `theme-${props.global.theme}`;
         }
+    }
 
     }, [props.global.theme]);
 
