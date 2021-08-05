339fe73e30417868b4c4454b803758c36f987e32
feruz <fmiboy@gmail.com>
2021-08-05 15:45:34 +0300

    fix wrong json metadata

--- src/common/components/entry-list-item/index.tsx
+++ src/common/components/entry-list-item/index.tsx
@@ -184,7 +184,7 @@ export default class EntryListItem extends Component<Props, State> {
                 </picture>
             );
         }
-        const nsfw = entry.json_metadata.tags && entry.json_metadata.tags.includes("nsfw");
+        const nsfw = entry.json_metadata.tags && Array.isArray(entry.json_metadata.tags) && entry.json_metadata.tags.includes("nsfw");
 
         const cls = `entry-list-item ${promoted ? "promoted-item" : ""}`;
 
