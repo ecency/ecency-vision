b5b4dd6700065e033bade0a2ece52f46f8469668
Benjamin <onclebobs@gmail.com>
2021-08-30 15:10:17 +0200

    Redesign the Hive Engine token list

--- src/common/components/wallet-hive-engine/index.tsx
+++ src/common/components/wallet-hive-engine/index.tsx
@@ -6,11 +6,13 @@ import { DynamicProps } from "../../store/dynamic-props/types";
 
 import BaseComponent from "../base";
 import LinearProgress from "../linear-progress";
+import { OverlayTrigger, Tooltip } from "react-bootstrap";
 import WalletMenu from "../wallet-menu";
 
 import { getHiveEngineTokenBalances } from "../../api/hive-engine";
 import HiveEngineToken from "../../helper/hive-engine-wallet";
 import { proxifyImageSrc } from "@ecency/render-helper";
+import { informationVariantSvg } from "../../img/svg";
 
 import { _t } from "../../i18n";
 
@@ -64,53 +66,6 @@ export class WalletHiveEngine extends BaseComponent<Props, State> {
       return null;
     }
 
-    const table = (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>{_t("wallet-engine.token")}</th>
-            <th>{_t("wallet-engine.balance")}</th>
-            <th>{_t("wallet-engine.staked")}</th>
-          </tr>
-        </thead>
-        <tbody>
-          {tokens.map((b, i) => {
-            const imageSrc = proxifyImageSrc(
-              b.icon,
-              0,
-              0,
-              global?.canUseWebp ? "webp" : "match"
-            );
-            const fallbackImage = require("../../img/noimage.svg");
-
-            return (
-              <tr key={i}>
-                <td>
-                  <img
-                    alt={b.symbol}
-                    src={imageSrc}
-                    className="item-image"
-                    onError={(e: React.SyntheticEvent) => {
-                      const target = e.target as HTMLImageElement;
-                      target.src = fallbackImage;
-                    }}
-                  />
-                  {b.name} ({b.symbol})
-                </td>
-                <td>
-                  {b.balanced()}
-                </td>
-                <td>
-                  {b.staked()}
-                  {b.hasDelegations() && " " + b.delegations()}
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-
     return (
       <div className="wallet-hive-engine">
         <div className="wallet-main">
@@ -124,15 +79,78 @@ export class WalletHiveEngine extends BaseComponent<Props, State> {
               </div>
             </div>
 
-            {loading ? (
-              <div className="dialog-placeholder">
-                <LinearProgress />
-              </div>
-            ) : tokens.length === 0 ? (
-              <div className="no-results">{_t("wallet-engine.no-results")}</div>
-            ) : (
-              <div className="table-responsive">{table}</div>
-            )}
+            <div className="entry-list">
+              {loading ? (
+                <div className="dialog-placeholder">
+                  <LinearProgress />
+                </div>
+              ) : tokens.length === 0 ? (
+                <div className="no-results">
+                  {_t("wallet-engine.no-results")}
+                </div>
+              ) : (
+                <div className="entry-list-body">
+                  {tokens.map((b, i) => {
+                    const imageSrc = proxifyImageSrc(
+                      b.icon,
+                      0,
+                      0,
+                      global?.canUseWebp ? "webp" : "match"
+                    );
+                    const fallbackImage = require("../../img/noimage.svg");
+
+                    return (
+                      <div className="entry-list-item" key={i}>
+                        <div className="entry-header d-flex align-items-center ">
+                          <img
+                            alt={b.symbol}
+                            src={imageSrc}
+                            className="item-image"
+                            onError={(e: React.SyntheticEvent) => {
+                              const target = e.target as HTMLImageElement;
+                              target.src = fallbackImage;
+                            }}
+                          />
+                          {b.symbol}
+                        </div>
+                        <div className="entry-body ml-auto mr-auto d-flex align-items-center ">
+                          {/* <span className="item-conversion">$9.19/</span> */}
+                          <span className="item-balance">
+                            {b.balanced()} {b.symbol}
+                          </span>
+                        </div>
+
+                        <div className="d-flex align-items-center ml-auto ml-md-2">
+                          <OverlayTrigger
+                            delay={{ show: 0, hide: 500 }}
+                            key={"bottom"}
+                            placement={"bottom"}
+                            overlay={
+                              <Tooltip id={`tooltip-${b.symbol}`}>
+                                <div className="tooltip-inner">
+                                  <div className="profile-info-tooltip-content">
+                                    <p>{_t("wallet-engine.token")}: {b.name}</p>
+                                    <p>{_t("wallet-engine.balance")}: {b.balanced()} {b.symbol}</p>
+                                    <p>{_t("wallet-engine.staked")}: {b.staked()}</p>
+                                    {b.hasDelegations() && `<p>${b.delegations()}</p>`}
+                                  </div>
+                                </div>
+                              </Tooltip>
+                            }
+                          >
+                            <div className="d-flex align-items-center">
+                              <span className="info-icon mr-0 mr-md-2">
+                                {informationVariantSvg}
+                              </span>
+                            </div>
+                          </OverlayTrigger>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
           </div>
           <WalletMenu
             global={global}
