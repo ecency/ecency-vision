72362f720f4d1b07cce65d84dcbbdbbfd877924e
Muhammad Ahmad <muhammadahmad57@gmail.com>
2021-10-11 21:29:44 +0700

    hide select layout mobile, active selected

--- src/common/components/list-style-toggle/index.tsx
+++ src/common/components/list-style-toggle/index.tsx
@@ -12,78 +12,64 @@ import { _t } from "../../i18n";
 import _c from "../../util/fix-class-names";
 
 import {
-  viewModuleSvg,
-  gridViewSvg,
-  listSvg,
-  menuDownSvg,
+    viewModuleSvg,
+    gridViewSvg,
+    listSvg,
+    menuDownSvg,
 } from "../../img/svg";
 
 interface Props {
-  global: Global;
-  toggleListStyle: (view: string) => void;
+    global: Global;
+    toggleListStyle: (view: string | null) => void;
 }
 
 export default class ListStyleToggle extends Component<Props> {
-  shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
-    return !isEqual(this.props.global.listStyle, nextProps.global.listStyle);
-  }
+    shouldComponentUpdate(nextProps: Readonly<Props>): boolean {
+        return !isEqual(
+            this.props.global.listStyle,
+            nextProps.global.listStyle
+        );
+    }
 
-  changeStyle = (view: string) => {
-    const { toggleListStyle } = this.props;
+    changeStyle = (view: string) => {
+        const { toggleListStyle } = this.props;
 
-    toggleListStyle(view);
-  };
-
-  render() {
-    const { global } = this.props;
-    const { listStyle } = global;
-
-    const dropDownItems = [
-      {
-        label: <span className="gridMenu">{gridViewSvg} Card</span>,
-        onClick: () => this.changeStyle("grid"),
-      },
-      {
-        label: <span className="gridMenu">{listSvg} Classic</span>,
-        onClick: () => this.changeStyle("row"),
-      },
-    ];
-
-    const dropDownConfig = {
-      history: history,
-      label: (
-        <span className='view-feed'>
-          <span className='view-layout'>{viewModuleSvg}</span>{" "}
-          <span className="menu-down-icon" >
-            {menuDownSvg}
-          </span>
-        </span>
-      ),
-      items: dropDownItems,
-      //   preElem: preDropDownElem,
+        toggleListStyle(view);
     };
 
-    return (
-      <DropDown {...dropDownConfig} float="right" header="" />
+    render() {
+        const { global } = this.props;
+        const { listStyle } = global;
 
-      // <Tooltip content={_t("list-style.title")}>
-      // <DropdownButton
-      //   title={
-      //     <span className="btn-view">
-      //       <i className="bi bi-list" /> {viewModuleSvg}
-      //     </span>
-      //   }
-      //   className="feed-view"
-      //   key="end"
-      // >
-      //   <Dropdown.Item eventKey="grid" onClick={() => this.changeStyle("grid")}>
-      //     {gridViewSvg} Card
-      //   </Dropdown.Item>
-      //   <Dropdown.Divider />
-      //   <Dropdown.Item eventKey="row" onClick={() => this.changeStyle("row")}>
-      //     {listSvg} Classic
-      //   </Dropdown.Item>
-      // </DropdownButton>
-    );
-  }
+        const dropDownItems = [
+            {
+                label: <span className="gridMenu">{gridViewSvg} Card</span>,
+                active: global.listStyle === "grid",
+                onClick: () => this.changeStyle("grid"),
+            },
+            {
+                label: <span className="gridMenu">{listSvg} Classic</span>,
+                active: global.listStyle === "row",
+                onClick: () => this.changeStyle("row"),
+            },
+        ];
+
+        const dropDownConfig = {
+            history: history,
+            label: (
+                <span className="view-feed">
+                    <span className="view-layout">{viewModuleSvg}</span>{" "}
+                    <span className="menu-down-icon">{menuDownSvg}</span>
+                </span>
+            ),
+            items: dropDownItems,
+            //   preElem: preDropDownElem,
+        };
+
+        return (
+            <div className="viewLayouts">
+                <DropDown {...dropDownConfig} float="right" header="" />
+            </div>
+        );
+    }
 }
