d98053b6dd7235bd41555a2383ac4b15683e11e0
Fahad <ghazanfar89@gmail.com>
2021-11-05 18:52:36 +0500

    Add internationalization till trade history

--- src/common/components/chart-stats/index.tsx
+++ src/common/components/chart-stats/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { MarketStatistics } from '../../api/hive';
+import { _t } from '../../i18n';
 import { Skeleton } from '../skeleton';
 
 interface Props {
@@ -16,31 +17,31 @@ export const ChartStats = ({loading, data}: Props) =>{
                         <Skeleton className="skeleton-loading" />
                     </div> : <div className="d-flex flex-wrap justify-content-center">
                                 <small className="d-inline-flex border mr-5">
-                                    <div className="p-2 font-weight-bold border-right">Last Price</div>
+                                    <div className="p-2 font-weight-bold border-right">{_t("market.last-price")}</div>
                                     <div className="p-2">${data ? parseFloat(data!.latest!).toFixed(6) : null} (<span className="text-success">+0.00%</span>)</div>
                                 </small>
 
 
                                 <small className="d-inline-flex border">
-                                    <div className="p-2 font-weight-bold border-right">24h volume</div>
+                                    <div className="p-2 font-weight-bold border-right">{_t("market.volume")}</div>
                                     <div className="p-2">${data? parseFloat(data!.hbd_volume)!.toFixed(2):null}</div>
                                 </small>
 
 
                                 <small className="d-inline-flex border mx-5">
-                                    <div className="p-2 font-weight-bold border-right">Bid</div>
+                                    <div className="p-2 font-weight-bold border-right">{_t("market.bid")}</div>
                                     <div className="p-2">${data? parseFloat(data!.highest_bid)!.toFixed(6):null}</div>
                                 </small>
 
 
                                 <small className="d-inline-flex border">
-                                    <div className="p-2 font-weight-bold border-right">Ask</div>
+                                    <div className="p-2 font-weight-bold border-right">{_t("market.last-price")}</div>
                                     <div className="p-2">${data? parseFloat(data!.lowest_ask)!.toFixed(6):null}</div>
                                 </small>
 
 
                                 <small className="d-inline-flex border ml-5">
-                                    <div className="p-2 font-weight-bold border-right" >Spread</div>
+                                    <div className="p-2 font-weight-bold border-right" >{_t("market.spread")}</div>
                                     <div className="p-2">{data? ((200 * (parseFloat(data.lowest_ask) - parseFloat(data.highest_bid))) / (parseFloat(data.highest_bid) + parseFloat(data.lowest_ask))).toFixed(3) : null}%</div>
                                 </small>
                             </div>
--- src/common/components/hive-barter/index.tsx
+++ src/common/components/hive-barter/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {Button, Form, InputGroup} from 'react-bootstrap';
+import { _t } from '../../i18n';
 import { Skeleton } from '../skeleton';
 
 interface Props {
@@ -12,14 +13,14 @@ interface Props {
 export const HiveBarter = ({type, available, peakValue, loading}: Props) => {
     return loading ? <Skeleton className="loading-hive"/> : <div className="border p-3 rounded">
         <div className="d-flex justify-content-between align-items-center">
-            <h3 className="mb-0">{type === 1 ? 'Buy' : "Sell"} Hive</h3>
+            <h3 className="mb-0">{type === 1 ? _t("market.buy") : _t("market.sell")} {_t("wallet.hive")}</h3>
             <div>
                 <small className="d-flex">
-                    <div className="mr-1 text-primary">Available:</div>
+                    <div className="mr-1 text-primary">{_t("market.available")}:</div>
                     <div>{available}</div>
                 </small>
                 <small className="d-flex">
-                    <div className="mr-1 text-primary">{type === 1 ? 'Lowest ask' : "Highest bid"}:</div>
+                    <div className="mr-1 text-primary">{type === 1 ? _t("market.lowest-ask") : _t("market.highest-bid")}:</div>
                     <div>{peakValue.toFixed(6)}</div>
                 </small>
             </div>
@@ -27,36 +28,36 @@ export const HiveBarter = ({type, available, peakValue, loading}: Props) => {
         <hr />
         <Form>
             <Form.Group>
-                <Form.Label>Price</Form.Label>
+                <Form.Label>{_t("market.price")}</Form.Label>
                 <InputGroup hasValidation>
                     <Form.Control
                         value={peakValue.toFixed(6)}
                         placeholder="0.0"
                     />
-                    <InputGroup.Text className="rounded-left">HBD/HIVE</InputGroup.Text>
+                    <InputGroup.Text className="rounded-left">{_t("market.hbd")}/{_t("wallet.hive")}</InputGroup.Text>
                 </InputGroup>
             </Form.Group>
 
             <Form.Group>
-                <Form.Label>Amount</Form.Label>
+                <Form.Label>{_t("market.amount")}</Form.Label>
                 <InputGroup hasValidation>
                     <Form.Control
                     placeholder="0.0"
                     />
-                    <InputGroup.Text className="rounded-left">HIVE</InputGroup.Text>
+                    <InputGroup.Text className="rounded-left">{_t("wallet.hive")}</InputGroup.Text>
                 </InputGroup>
             </Form.Group>
 
             <Form.Group className="mb-4">
-                <Form.Label>Total</Form.Label>
+                <Form.Label>{_t("market.total")}</Form.Label>
                 <InputGroup hasValidation>
                     <Form.Control
                     placeholder="0.0"
                     />
-                    <InputGroup.Text className="rounded-left">HBD($)</InputGroup.Text>
+                    <InputGroup.Text className="rounded-left">{_t("market.hbd")}($)</InputGroup.Text>
                 </InputGroup>
             </Form.Group>
-            <Button block>{type === 1 ? 'Buy' : "Sell"}</Button>
+            <Button block>{type === 1 ? _t("market.buy") : _t("market.sell")}</Button>
         </Form>
     </div>
 }
