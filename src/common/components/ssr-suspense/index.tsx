4ea3e8b42ac0f9fe97f181dc3c0a18e97423ea50
Fahad <ghazanfar89@gmail.com>
2021-12-03 21:03:43 +0400

    checkpoint

---
+++ src/common/components/ssr-suspense/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const SSRSuspense =
+  typeof window !== "undefined"
+    ? React.Suspense
+    : ({ children, ...props }:any) => children;
+
+export default SSRSuspense;
