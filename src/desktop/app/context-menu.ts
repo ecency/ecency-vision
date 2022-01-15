c2eed3d0385b1dde1d6b3b8b918fb020f1adb9a5
Fahad <ghazanfar89@gmail.com>
2022-01-15 16:07:07 +0400

    checkpoint

--- src/desktop/app/context-menu.ts
+++ src/desktop/app/context-menu.ts
@@ -1,7 +1,8 @@
-import {remote, shell, clipboard} from "electron";
+import { shell, clipboard, Renderer} from "electron";
 
 import {_t} from "../../common/i18n";
 
+const { remote } = Renderer;
 const {Menu, MenuItem} = remote;
 
 const isAnyTextSelected = () => window.getSelection() && window.getSelection()!.toString() !== '';
