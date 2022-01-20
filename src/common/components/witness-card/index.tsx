42c70953919a76bb338385d7dbd08171e5d45637
Fahad <ghazanfar89@gmail.com>
2022-01-20 12:08:43 +0400

    checkpoint

---
+++ src/common/components/witness-card/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import witnessVoteBtn from "../witness-vote-btn";
+
+export const WitnessCard = () => {
+  return (
+    <div className="p-3 mb-3 border rounded">
+      <div>
+            <div><b>Rank: 1</b></div>
+            
+          <div>
+              {witnessVoteBtn({
+                ...this.props,
+                voted: witnessVotes.includes(row.name),
+                witness: row.name,
+                onSuccess: (approve) => {
+                        if (approve) {
+                            this.addWitness(row.name);
+                        } else {
+                            this.deleteWitness(row.name);
+                        }
+                    }
+                })}
+            </div>
+            </div>
+      <div className="d-flex align-items-center">
+        <div>
+            <img src="https://images.ecency.com/webp/u/blocktrades/avatar/medium" alt="" className="rounded-circle avatar"/>
+        </div>
+        <div>Blockchain</div>
+      </div>
+      <div><b>Miss: 1231</b> window-icon</div>
+      <div><b>Fee: 1231</b></div>
+      <div><b>Feed: 1231</b></div>
+      <div><b>version: 1231</b></div>
+    </div>
+  );
+};
--- src/common/pages/witnesses.tsx
+++ src/common/pages/witnesses.tsx
@@ -32,6 +32,7 @@ import {linkSvg, openInNewSvg} from "../img/svg";
 import {pageMapDispatchToProps, pageMapStateToProps, PageProps} from "./common";
 import moment from "moment";
 import {FullAccount} from "../store/accounts/types";
+import { WitnessCard } from "../components/witness-card";
 
 interface WitnessTransformed {
     rank: number;
@@ -187,7 +188,7 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
         const {witnesses, loading, witnessVotes, proxy} = this.state;
         const extraWitnesses = witnessVotes.filter(w => !witnesses.find(y => y.name === w));
 
-        const table = <table className="table">
+        const table = <><table className="table d-none d-md-block">
             <thead>
             <tr>
                 <th className="col-rank">
@@ -279,7 +280,13 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
                 </tr>
             })}
             </tbody>
-        </table>;
+        </table>
+        <div className="d-md-none">
+        {witnesses.map((row, i) => {
+                return <WitnessCard />
+            })}
+        </div>
+        </>;
 
         const header = <div className="page-header mt-5">
             <div className="header-title">
