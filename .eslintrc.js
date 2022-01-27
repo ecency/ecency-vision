54ee6b28909287d640fe949eae30ec8df8e180e7
Fahad <ghazanfar89@gmail.com>
2022-01-27 21:53:55 +0400

    Add linting files

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+    env: {
+      browser: true,
+      es2021: true
+    },
+    extends: [
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:@typescript-eslint/recommended",
+      "plugin:prettier/recommended"
+    ],
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+      ecmaFeatures: {
+        jsx: true
+      },
+      ecmaVersion: "latest",
+      sourceType: "module"
+    },
+    plugins: ["react", "@typescript-eslint"],
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "react/display-name": "off",
+      "prefer-const": "off",
+      "@typescript-eslint/no-explicit-any": "off",
+      "no-undef": "off"
+    }
+  };
