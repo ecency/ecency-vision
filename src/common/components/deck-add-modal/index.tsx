36ee3369bb729c3431952506c3f9552aa8631ec2
Fahad <ghazanfar89@gmail.com>
2022-02-10 11:48:44 +0400

    support data for column

--- src/common/components/deck-add-modal/index.tsx
+++ src/common/components/deck-add-modal/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
-import { Button, Form, Modal, ModalBody } from "react-bootstrap";
+import { Button, Form, InputGroup, Modal, ModalBody } from "react-bootstrap";
 import ModalHeader from "react-bootstrap/esm/ModalHeader";
+import { lookupAccounts } from "../../api/hive";
+import { formatError } from "../../api/operations";
+import { _t } from "../../i18n";
 import {
   arrowLeftSvg,
   arrowRightSvg,
-  closeSvg,
   hot,
   magnify,
   newSvg,
@@ -15,6 +17,9 @@ import {
   tags,
   wallet,
 } from "../../img/svg";
+import { error } from "../feedback";
+import SuggestionList from "../suggestion-list";
+import userAvatar from "../user-avatar";
 
 const OptionWithIcon = ({ title, icon, onOptionClick }: any) => (
   <div
@@ -22,7 +27,7 @@ const OptionWithIcon = ({ title, icon, onOptionClick }: any) => (
     onClick={() => onOptionClick(title)}
   >
     <div>{icon}</div>
-    <div className="mt-2">{title}</div>
+    <div className="mt-2 text-center">{title}</div>
   </div>
 );
 
@@ -32,7 +37,7 @@ const options = [
     icon: person,
   },
   {
-    title: "Trending",
+    title: "Trending topics",
     icon: hot,
   },
   {
@@ -70,6 +75,7 @@ const options = [
 ];
 
 const contentTypes = [
+  { code: "", name: "Select content type" },
   { code: "blogs", name: "Blogs" },
   { code: "posts", name: "Posts" },
   { code: "comments", name: "Comments" },
@@ -77,16 +83,100 @@ const contentTypes = [
 ];
 
 const AddColumn = ({ setSelectedValue }: any) => {
+  const [to, setTo] = useState("");
+  const [contentType, setContentType] = useState("");
+  const [toSelected, setToSelected] = useState("");
+  const [toData, setToData] = useState<any>([]);
+  const [toDataLoading, setToDataLoading] = useState(false);
+  let _timer: any = null;
+
+  const toChanged = (e: any) => {
+    let toValue = e.target.value;
+    setToDataLoading(true);
+    setTo(toValue);
+    if (_timer) {
+      clearTimeout(_timer);
+    }
+
+    if (toValue === "") {
+      setTo("");
+      setToDataLoading(false);
+      return;
+    }
+
+    _timer = setTimeout(() => {
+      return lookupAccounts(toValue, 5)
+        .then((resp) => {
+          if (resp) {
+            setToData(resp);
+          }
+        })
+        .catch((err) => {
+          error(formatError(err));
+        })
+        .finally(() => {
+          setToDataLoading(false);
+        });
+    }, 500);
+  };
+  const suggestionProps = {
+    renderer: (i: string) => {
+      return (
+        <>
+          {userAvatar({ username: i, size: "medium", global: {} as any })}{" "}
+          <span style={{ marginLeft: "4px" }}>{i}</span>
+        </>
+      );
+    },
+    onSelect: (selectedText: string) => {
+      setTo(selectedText);
+      setToSelected(selectedText);
+    },
+  };
+
   return (
     <div className="d-flex flex-column align-items-center mt-5">
       <Form.Group>
         <Form.Label>Username</Form.Label>
-        <Form.Control />
+
+        <SuggestionList items={toData} {...suggestionProps}>
+          <InputGroup>
+            <InputGroup.Prepend>
+              <InputGroup.Text>
+                {toDataLoading ? (
+                  <div
+                    className="spinner-border text-primary spinner-border-sm"
+                    role="status"
+                  >
+                    <span className="sr-only">Loading...</span>
+                  </div>
+                ) : (
+                  "@"
+                )}
+              </InputGroup.Text>
+            </InputGroup.Prepend>
+            <Form.Control
+              type="text"
+              autoFocus={true}
+              placeholder={_t("transfer.to-placeholder")}
+              value={to}
+              onChange={toChanged}
+            //   className={toError ? "is-invalid" : ""}
+            />
+          </InputGroup>
+        </SuggestionList>
       </Form.Group>
 
       <Form.Group className="w-100">
         <Form.Label>Type of content</Form.Label>
-        <Form.Control type="text" as="select">
+        <Form.Control
+          type="text"
+          as="select"
+          onChange={(e) => {
+            setContentType(e.target.value);
+          }}
+          value={contentType}
+        >
           {contentTypes.map((x: any) => (
             <option key={x.code} value={x.code}>
               {x.name}
@@ -94,9 +184,12 @@ const AddColumn = ({ setSelectedValue }: any) => {
           ))}
         </Form.Control>
       </Form.Group>
-      <Button className="align-self-start mb-5">Add</Button>
+      <Button className="align-self-start mb-5" disabled={contentType==="" || toSelected.length ===0}>Add</Button>
 
-      <div className="mt-5 d-flex align-items-center back-icon pointer w-100" onClick={()=>setSelectedValue(null)}>
+      <div
+        className="mt-5 d-flex align-items-center back-icon pointer w-100"
+        onClick={() => setSelectedValue(null)}
+      >
         <div className="mr-2">{arrowLeftSvg}</div>
         <div>Go back</div>
       </div>
@@ -115,7 +208,7 @@ export const DeckAddModal = ({ open, onClose }: any) => {
         <div className="flex-grow-1">
           {selectedOption ? (
             <div className="d-flex align-items-center">
-              <div className="header-icon mr-2">
+              <div className="header-icon mr-2 d-flex">
                 {options.find((item) => item.title === selectedOption)?.icon}
               </div>
               <div>
@@ -131,7 +224,10 @@ export const DeckAddModal = ({ open, onClose }: any) => {
       </ModalHeader>
       <ModalBody className="d-flex justify-content-center">
         {selectedOption ? (
-          <AddColumn selectedValue={selectedOption} setSelectedValue={setSelectedOption}/>
+          <AddColumn
+            selectedValue={selectedOption}
+            setSelectedValue={setSelectedOption}
+          />
         ) : (
           <div className="d-flex w-100 flex-wrap">
             {options.map((option) => (
