fc413e693f23a4fcb6d3a0bc85000f278d269491
Fahad <ghazanfar89@gmail.com>
2022-02-21 14:20:51 +0400

    Resolve git issue

---
+++ src/common/components/deck-view/deck-options.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { closeSvg, deleteForeverSvg, refreshSvg } from "../../img/svg";
+
+interface DeckOptionsProps {
+  options: null | string;
+  setOptions: (option: null) => void;
+}
+
+export const DeckOptions = ({ setOptions, options }: DeckOptionsProps) => {
+  return (
+    <div className="deck mr-3 rounded-top deck-options p-3">
+      <div className="d-flex justify-content-end">
+        <div
+          className="deck-options-icon pointer"
+          onClick={() => setOptions(null)}
+        >
+          {closeSvg}
+        </div>
+      </div>
+
+      <div className="p-3 mt-3 d-flex align-items-center deck-option rounded">
+        <div className="deck-options-icon mr-2">{refreshSvg}</div>
+        <div>Reload</div>
+      </div>
+
+      <div className="p-3 mt-3 d-flex align-items-center deck-option rounded">
+        <div className="deck-options-icon mr-2">{deleteForeverSvg}</div>
+        <div>Remove</div>
+      </div>
+    </div>
+  );
+};
