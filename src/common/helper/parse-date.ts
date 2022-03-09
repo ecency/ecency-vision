ef9c033883fd3a2e9b76ec72c787eb369bbe5de3
feruz <fmiboy@gmail.com>
2022-03-09 11:53:08 +0200

    parse date expand and reuse

--- src/common/helper/parse-date.ts
+++ src/common/helper/parse-date.ts
@@ -1,1 +1,21 @@
-export default (d: string): Date => new Date(`${d}.000Z`);
+import moment from "moment";
+
+export const dateToRelative = (d: string): string => {
+  const dm = moment(new Date(`${d}.000Z`))
+  const dd = dm.fromNow(true);
+  return dd.replace(' seconds','s').replace(' minutes','m').replace(' hours','h').replace('an hour','1h').replace(' days','d').replace('a day','1d').replace(' months','M').replace('a month','1M').replace(' years','y').replace('a year','1y');
+}
+
+export const dateToFullRelative = (d: string): string => {
+  const dm = moment(new Date(`${d}.000Z`))
+  return dm.fromNow();
+}
+
+export const dateToFormatted = (d: string): string => {
+  const dm = moment(new Date(`${d}.000Z`));
+  return dm.format("LLLL");
+}
+
+const parseDate = (d: string): Date => new Date(`${d}.000Z`);
+
+export default parseDate;
