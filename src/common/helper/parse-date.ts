d4c8c79fc5f3cdec04584b0c99656bc9788f7ae4
feruz <fmiboy@gmail.com>
2022-03-09 12:33:51 +0200

    checkpoint

--- src/common/helper/parse-date.ts
+++ src/common/helper/parse-date.ts
@@ -4,7 +4,7 @@ export const dateToRelative = (d: string): string => {
   const isTimeZoned = d.includes('.')||d.includes('+') ? d :`${d}.000Z`;
   const dm = moment(new Date(isTimeZoned));
   const dd = dm.fromNow(true);
-  return dd.replace(' seconds','s').replace(' minutes','m').replace(' hours','h').replace('an hour','1h').replace(' days','d').replace('a day','1d').replace(' months','M').replace('a month','1M').replace(' years','y').replace('a year','1y');
+  return dd.replace('a few seconds','~1s').replace(' seconds','s').replace(' minutes','m').replace('a minute','1m').replace(' hours','h').replace('an hour','1h').replace(' days','d').replace('a day','1d').replace(' months','M').replace('a month','1M').replace(' years','y').replace('a year','1y');
 }
 
 export const dateToFullRelative = (d: string): string => {
