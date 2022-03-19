9bc296e4647dbe878beaa917d576b6eaa7ceacc2
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-03-19 13:08:38 +0200

    Added a spk fallback to setupConfig

--- src/setup.ts
+++ src/setup.ts
@@ -1,8 +1,8 @@
 console.log(process.env.RAZZLE_HIVE_ID);
 
 export const setupConfig = {
-  communityId: process.env.RAZZLE_HIVE_ID ?? "",
-  selectedTheme: process.env.RAZZLE_THEME ?? "",
+  communityId: process.env.RAZZLE_HIVE_ID ?? "hive-112019",
+  selectedTheme: process.env.RAZZLE_THEME ?? "sky",
   tags: process.env.RAZZLE_TAGS?.split(",") ?? ["3speak", "spk"],
   navBarImg: "",
 };
