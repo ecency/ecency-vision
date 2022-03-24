ee69f8b3bee123eedb76b6c1b04a84b9fc8a8e27
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-03-24 20:18:34 +0200

    Changed community number

--- src/setup.ts
+++ src/setup.ts
@@ -1,7 +1,7 @@
 console.log(process.env.RAZZLE_HIVE_ID);
 
 export const setupConfig = {
-  communityId: process.env.RAZZLE_HIVE_ID ?? "hive-112019",
+  communityId: process.env.RAZZLE_HIVE_ID ?? "hive-176364",
   selectedTheme: process.env.RAZZLE_THEME ?? "sky",
   tags: process.env.RAZZLE_TAGS?.split(",") ?? ["3speak", "spk"],
   navBarImg: "",
