9615718d75ad2f0ae9a609cc2253acfd2c80119d
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-03-31 22:06:02 +0300

    Minor changes to action:

--- src/setup.ts
+++ src/setup.ts
@@ -1,6 +1,6 @@
 export const setupConfig = {
-  communityId: process.env.RAZZLE_HIVE_ID ?? "hive-112019",
-  selectedTheme: process.env.RAZZLE_THEME ?? "sky",
-  tags: process.env.RAZZLE_TAGS?.split(",") ?? ["3speak", "spk"],
+  communityId: process.env.RAZZLE_HIVE_ID ?? "",
+  selectedTheme: process.env.RAZZLE_THEME ?? "",
+  tags: process.env.RAZZLE_TAGS?.split(",") ?? [""],
   navBarImg: "",
 };
