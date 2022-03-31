3b51fea41c893e2266fc71eebb65f0ae77ccbc5e
PijusSerapinasMipsas <pijus.s@mipsas.com>
2022-03-31 21:55:33 +0300

    removed the fallbacks

--- src/setup.ts
+++ src/setup.ts
@@ -1,8 +1,6 @@
-console.log(process.env.RAZZLE_HIVE_ID);
-
 export const setupConfig = {
-  communityId: process.env.RAZZLE_HIVE_ID ?? "hive-176364",
-  selectedTheme: process.env.RAZZLE_THEME ?? "sky",
-  tags: process.env.RAZZLE_TAGS?.split(",") ?? ["3speak", "spk"],
+  communityId: process.env.RAZZLE_HIVE_ID,
+  selectedTheme: process.env.RAZZLE_THEME,
+  tags: process.env.RAZZLE_TAGS?.split(","),
   navBarImg: "",
 };
