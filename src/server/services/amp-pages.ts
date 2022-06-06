b47a2f6b472e0f9e80b59c20faab44447139b6db
Ildar Timerbaev <dkildar@gmail.com>
2022-06-06 10:57:04 +0300

    https://github.com/ecency/ecency-vision/issues/782 Increased render helper amp version

--- src/server/services/amp-pages.ts
+++ src/server/services/amp-pages.ts
@@ -27,13 +27,15 @@ export async function getAsAMP(
 
   const renderResult = await renderAmp(request, preloadedState);
 
-  let ampResult = await htmlToAMP(renderResult, false, true);
+  let ampResult = await htmlToAMP(renderResult, false, true, false);
   ampResult = ampResult.replace(/\n/gm, '');
 
-  const styleBlockIndex = ampResult.search('</style>') + 8;
-  const pageStyles = fs.readFileSync(`build/public${assets['pages-amp-entry-amp-page'].css[0]}`).toString();
+  if (assets['pages-amp-entry-amp-page'].css) {
+    const styleBlockIndex = ampResult.search('</style>') + 8;
+    const pageStyles = fs.readFileSync(`build/public${assets['pages-amp-entry-amp-page'].css[0]}`).toString();
 
-  ampResult = [ampResult.slice(0, styleBlockIndex), `<style amp-custom>${pageStyles}</style>`, ampResult.slice(styleBlockIndex)].join('');
+    ampResult = [ampResult.slice(0, styleBlockIndex), `<style amp-custom>${pageStyles}</style>`, ampResult.slice(styleBlockIndex)].join('');
+  }
 
   const modifiedClasses = 'theme-day';
   ampResult = ampResult.replace('<body>', `<body class="${modifiedClasses}">`);
