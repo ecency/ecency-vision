d5554d4c63a6984620d5718d6bed789f836e4e05
Adesojisouljay <adesojiisrael@gmail.com>
2022-06-27 04:01:33 +0100

    cleanup component

--- src/common/pages/referral.tsx
+++ src/common/pages/referral.tsx
@@ -2,8 +2,6 @@ import React from "react";
 
 import {connect} from "react-redux";
 
-import {pathToRegexp} from "path-to-regexp";
-
 import BaseComponent from "../components/base";
 import Meta from "../components/meta";
 import Feedback from "../components/feedback";
@@ -14,15 +12,8 @@ import NavBarElectron from "../../desktop/app/components/navbar";
 import LinearProgress from "../components/linear-progress";
 import ProfileLink from "../components/profile-link";
 import UserAvatar from "../components/user-avatar";
-import EntryLink, {PartialEntry} from "../components/entry-link";
-import WitnessVoteBtn from "../components/witness-vote-btn";
-import WitnessesExtra from "../components/witnesses-extra"
-import WitnessesProxy from "../components/witnesses-proxy"
-import WitnessesActiveProxy from "../components/witnesses-active-proxy";
+import EntryLink from "../components/entry-link";
 
-import routes from "../../common/routes";
-
-import {getAccount, getAccounts, getWitnessesByVote, Witness} from "../api/hive";
 
 import {_t} from "../i18n";
 import {Tsx} from "../i18n/helper";
@@ -30,84 +21,18 @@ import {Tsx} from "../i18n/helper";
 import {linkSvg, openInNewSvg} from "../img/svg";
 
 import {pageMapDispatchToProps, pageMapStateToProps, PageProps} from "./common";
-import {FullAccount} from "../store/accounts/types";
-import { WitnessCard } from "../components/witness-card";
-import { dateToRelative } from '../helper/parse-date';
 
-interface WitnessTransformed {
-    rank: number;
-    name: string;
-    miss: number;
-    fee: string;
-    feed: string;
-    blockSize: number;
-    acAvail: number;
-    acBudget: number;
-    version: string;
-    url: string;
-    parsedUrl?: PartialEntry;
-    signingKey?:string
-    priceAge:string
-    witnessBy?:string
-}
 
-const transform = (list: Witness[]): WitnessTransformed[] => {
-    return list.map((x, i) => {
-        const rank = i + 1;
-
-        const {props} = x;
-
-        const {total_missed: miss, url} = x;
-        const fee = props.account_creation_fee;
-        const feed = x.hbd_exchange_rate.base;
-        const {maximum_block_size: blockSize} = props;
-        const {available_witness_account_subsidies: acAvail} = x;
-        const {account_subsidy_budget: acBudget} = props;
-        const {running_version: version} = x;
-        const {signing_key:signingKey} = x
-        const {last_hbd_exchange_update:priceAge} = x;
-
-        let parsedUrl;
-        const oUrl = new URL(url, 'https://ecency.com');
-        const ex = pathToRegexp(routes.ENTRY).exec(oUrl.pathname);
-
-        if (ex) {
-            parsedUrl = {
-                category: ex[1],
-                author: ex[2].replace("@", ""),
-                permlink: ex[3]
-            }
-        }
-
-        return {
-            rank,
-            name: x.owner,
-            miss,
-            fee,
-            feed,
-            blockSize,
-            acAvail: Math.round(acAvail / 10000),
-            acBudget,
-            version,
-            url,
-            parsedUrl,
-            signingKey,
-            priceAge,
-        };
-    });
-}
 
 interface State {
-    witnesses: WitnessTransformed[];
-    witnessVotes: string[];
+    referrals: [];
     proxy: string | null;
     loading: boolean;
 }
 
-class WitnessesPage extends BaseComponent<PageProps, State> {
+class ReferralPage extends BaseComponent<PageProps, State> {
     state: State = {
-        witnesses: [],
-        witnessVotes: [],
+        referrals: [],
         proxy: null,
         loading: true
     }
@@ -129,49 +54,17 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
         this.stateSet({loading: true});
 
         const {activeUser} = this.props;
-        if (activeUser) {
-            const resp = await getAccount(activeUser.username);
-            const {witness_votes: witnessVotes, proxy} = resp;
-            this.stateSet({witnessVotes: witnessVotes || [], proxy: proxy || null});
-        } else {
-            this.stateSet({witnessVotes: [], proxy: null});
-        }
+        
 
-        const witnesses = await getWitnessesByVote();
-        await this.getWitness(transform(witnesses));
+        const referrals:[] = []
+        await this.getReferrals(referrals);
     }
 
-    addWitness = (name: string) => {
-        const {witnessVotes} = this.state;
-        const newVotes = [...witnessVotes, name]
-        this.stateSet({witnessVotes: newVotes});
-    }
-
-    deleteWitness = (name: string) => {
-        const {witnessVotes} = this.state;
-        const newVotes = witnessVotes.filter(x => x !== name)
-        this.stateSet({witnessVotes: newVotes});
-    }
-
-    getWitness = async (witnessArray:WitnessTransformed[])=>{
-        const witnessUserNamesArray:string[] = witnessArray.map((item: WitnessTransformed)=> {
-            return item.name
-        })
+    getReferrals = async (referralArray:[])=>{
         try {
-            const accounts:FullAccount[] = await getAccounts(witnessUserNamesArray);
-            const byWitnessState:WitnessTransformed[] = witnessArray.map((item: WitnessTransformed, index: number) => {
-                try {
-                    const parsedArray = JSON.parse(accounts[index].posting_json_metadata?accounts[index].posting_json_metadata:'');
-                    return {
-                        ...item,
-                        witnessBy:parsedArray.profile.witness_owner ? parsedArray.profile.witness_owner: undefined
-                    }
-                }
-                catch (e) {
-                    return item
-                }
-            })
-            this.stateSet({witnesses:byWitnessState, loading: false});
+           // Fetch referrals
+           console.log('Getting Referrals')
+            
         } catch (error) {
             console.log('Something went wrong: ',error)
         }
@@ -185,8 +78,7 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
         };
 
         const {global, activeUser} = this.props;
-        const {witnesses, loading, witnessVotes, proxy} = this.state;
-        const extraWitnesses = witnessVotes.filter(w => !witnesses.find(y => y.name === w));
+        const {referrals, loading, proxy} = this.state;
 
         const table = <><table className="table d-none d-sm-block">
             <thead>
@@ -215,21 +107,21 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
             </tr>
             </thead>
             <tbody>
-            {witnesses.map((row, i) => {
-                return <tr key={row.rank}>
+            {referrals.map((row, i) => {
+                return <tr key={i}>
                     <td>
                         <div className="witness-rank">
-                            <span className="rank-number">{row.rank}</span>
+                            <span className="rank-number">{'row.rank'}</span>
                             $$
                         </div>
                     </td>
                     <td>
                         {ProfileLink({
                             ...this.props,
-                            username: row.name,
+                            username: 'adesojisouljay',
                             children: <span className="witness-card notranslate"> {UserAvatar({
                                 ...this.props,
-                                username: row.name,
+                                username: 'adesojisouljay',
                                 size: "medium"
                             })}
                                 <div className={'witness-ctn'}>
@@ -252,7 +144,7 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
                             }
 
                             return (
-                                <a target="_external" href={row.url} className="witness-link">{openInNewSvg}</a>
+                                <a target="_external" href={'http/localhost:3000/referrals'} className="witness-link">{openInNewSvg}</a>
                             );
                         })()}
                     </td>
@@ -278,11 +170,6 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
                 {_t('referral.page-title')}
             </div>
             <Tsx k="referral.page-description-long"><div className="header-description" /></Tsx>
-            {activeUser && (
-                <Tsx k="referral.remaining" args={{n: 30 - witnessVotes.length, max: 30}}>
-                    <div className="remaining"/>
-                </Tsx>
-            )}
         </div>;
         let containerClasses = global.isElectron ? " mt-0 pt-6" : "";
 
@@ -320,4 +207,4 @@ class WitnessesPage extends BaseComponent<PageProps, State> {
 }
 
 
-export default connect(pageMapStateToProps, pageMapDispatchToProps)(WitnessesPage);
+export default connect(pageMapStateToProps, pageMapDispatchToProps)(ReferralPage);
