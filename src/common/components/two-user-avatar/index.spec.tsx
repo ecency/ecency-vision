0c5b2d711d98de0bbf1df06698bfc2f1572f9669
feruz <fmiboy@gmail.com>
2022-08-01 13:24:44 +0300

    update tests

--- src/common/components/two-user-avatar/index.spec.tsx
+++ src/common/components/two-user-avatar/index.spec.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import UserAvatar from "./index";
+import TwoUserAvatar from "./index";
 import TestRenderer from "react-test-renderer";
 
 import {globalInstance} from "../../helper/test-helper";
@@ -11,32 +11,32 @@ jest.mock("../../constants/defaults.json", () => ({
 
 describe("UserAvatar", () => {
     it("(1) Should render small size", () => {
-        const props = {global: globalInstance, username: "good-karma", size: "small"};
-        const renderer = TestRenderer.create(<UserAvatar {...props} />);
+        const props = {global: globalInstance, from: "good-karma", to: "demo", size: "small"};
+        const renderer = TestRenderer.create(<TwoUserAvatar {...props} />);
         expect(renderer.toJSON()).toMatchSnapshot();
     });
 
     it("(2) Should render normal size", () => {
-        const props = {global: globalInstance, username: "good-karma", size: "normal"};
-        const renderer = TestRenderer.create(<UserAvatar {...props} />);
+        const props = {global: globalInstance, from: "good-karma", to: "demo", size: "normal"};
+        const renderer = TestRenderer.create(<TwoUserAvatar {...props} />);
         expect(renderer.toJSON()).toMatchSnapshot();
     });
 
     it("(3) Should render medium size", () => {
-        const props = {global: globalInstance, username: "good-karma", size: "medium"};
-        const renderer = TestRenderer.create(<UserAvatar {...props} />);
+        const props = {global: globalInstance, from: "good-karma", to: "demo", size: "medium"};
+        const renderer = TestRenderer.create(<TwoUserAvatar {...props} />);
         expect(renderer.toJSON()).toMatchSnapshot();
     });
 
     it("(4) Should render large size", () => {
-        const props = {global: globalInstance, username: "good-karma", size: "large"};
-        const renderer = TestRenderer.create(<UserAvatar {...props} />);
+        const props = {global: globalInstance, from: "good-karma", to: "demo", size: "large"};
+        const renderer = TestRenderer.create(<TwoUserAvatar {...props} />);
         expect(renderer.toJSON()).toMatchSnapshot();
     });
 
     it("(5) Should render xlarge size", () => {
-        const props = {global: globalInstance, username: "good-karma", size: "xLarge"};
-        const renderer = TestRenderer.create(<UserAvatar {...props} />);
+        const props = {global: globalInstance, from: "good-karma", to: "demo", size: "xLarge"};
+        const renderer = TestRenderer.create(<TwoUserAvatar {...props} />);
         expect(renderer.toJSON()).toMatchSnapshot();
     });
 });
