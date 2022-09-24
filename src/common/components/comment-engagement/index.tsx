3ae71a57eee79f79f0fd89ceae5ec77dcdb13643
feruzm <fmiboy@gmail.com>
2022-09-24 19:38:51 +0300

    auto focus instead of scroll

--- src/common/components/comment-engagement/index.tsx
+++ src/common/components/comment-engagement/index.tsx
@@ -1,23 +1,24 @@
-import React from 'react'
+import React from "react";
 import { commentSvg } from "../../img/svg";
 import { Button } from "react-bootstrap";
 import { _t } from "../../i18n";
 
-function CommentEngagement() {   
-
-   const scrollToCommentInput = () => {
-      const inputSection:  Element | null = document.querySelector('.entry-footer');
-      inputSection?.scrollIntoView({behavior: "smooth"});
-   } 
-
-    return ( <div className="comment-engagement">
-        <div className="icon">{commentSvg}</div>
-         <div className="label">{_t("discussion.no-conversation")}</div>
-          <Button id="scroll-to-input" onClick={scrollToCommentInput}> 
-            {_t("discussion.start-conversation")}
-          </Button>
-          
-       </div>)
+function CommentEngagement() {
+  const scrollToCommentInput = () => {
+    // const inputSection:  Element | null = document.querySelector('.entry-footer');
+    // inputSection?.scrollIntoView({behavior: "smooth"});
+    const inputS: HTMLElement | null = document.querySelector(".the-editor");
+    inputS?.focus();
+  };
+  return (
+    <div className="comment-engagement">
+      <div className="icon">{commentSvg}</div>
+      <div className="label">{_t("discussion.no-conversation")}</div>
+      <Button id="scroll-to-input" onClick={scrollToCommentInput}>
+        {_t("discussion.start-conversation")}
+      </Button>
+    </div>
+  );
 }
 
 export default CommentEngagement;
