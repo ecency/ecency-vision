f1aaec474e6b080ddc3509b7b26ce57b606e1ba9
feruzm <fmiboy@gmail.com>
2022-10-05 16:26:01 +0500

    apr with hours

--- src/common/components/wallet-hive/index.tsx
+++ src/common/components/wallet-hive/index.tsx
@@ -48,7 +48,7 @@ import parseAsset from "../../helper/parse-asset";
 import { _t } from "../../i18n";
 
 import { plusCircle } from "../../img/svg";
-import { dayDiff, dateToFullRelative } from "../../helper/parse-date";
+import { dayDiff, dateToFullRelative, hourDiff } from "../../helper/parse-date";
 
 interface Props {
   history: History;
@@ -340,10 +340,16 @@ export class WalletHive extends BaseComponent<Props, State> {
         ? account.savings_hbd_seconds_last_update
         : account.savings_hbd_last_interest_payment
     );
+    const lastIPaymentDiffH = hourDiff(
+      account.savings_hbd_last_interest_payment == "1970-01-01T00:00:00"
+        ? account.savings_hbd_seconds_last_update
+        : account.savings_hbd_last_interest_payment
+    );
     const interestAmount =
       (Number(hbd) / 100) * (w.savingBalanceHbd / (12 * 30)) * lastIPaymentDiff;
     const estimatedInterest = formattedNumber(interestAmount, { suffix: "$" });
     const remainingDays = 30 - lastIPaymentDiff;
+    const remainingHours = lastIPaymentDiffH;
 
     const totalHP = formattedNumber(vestsToHp(w.vestingShares, hivePerMVests), { suffix: "HP" });
     const totalDelegated = formattedNumber(vestsToHp(w.vestingSharesDelegated, hivePerMVests), {
@@ -740,8 +746,10 @@ export class WalletHive extends BaseComponent<Props, State> {
                         className={`claim-btn ${remainingDays >= 0 ? "disabled" : ""}`}
                         onClick={this.toggleClaimInterest}
                       >
-                        {remainingDays >= 0
-                          ? _t("wallet.hive-dollars-apr-when", { value: remainingDays })
+                        {remainingDays > 0
+                          ? _t("wallet.hive-dollars-apr-day", { value: remainingDays })
+                          : remainingDays == 0
+                          ? _t("wallet.hive-dollars-apr-hour", { value: remainingHours })
                           : _t("wallet.hive-dollars-apr-now")}{" "}
                         {plusCircle}
                       </a>
