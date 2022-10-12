695ac9f16c2fe630e91ac7a06561e7724a67ad14
Adesojisouljay <adesojiisrael@gmail.com>
2022-10-12 13:30:39 +0100

    reblog persistence

--- src/common/pages/entry-index.tsx
+++ src/common/pages/entry-index.tsx
@@ -38,11 +38,18 @@ class EntryIndexPage extends Component<Props, State> {
     fetchEntries(global.filter, global.tag, false);
     fetchTrendingTags();
     this.loadEntries();
+
+    const filterReblog: any = window.localStorage.getItem("my_reblog")
+      this.setState({noReblog: JSON.parse(filterReblog)})
+    
   }
 
   componentDidUpdate(prevProps: Readonly<PageProps>, prevState: Readonly<State>): void {
     const { global, fetchEntries, activeUser } = this.props;
     const { global: pGlobal, activeUser: pActiveUser, entries: pEntries } = prevProps;
+
+    window.localStorage.setItem("my_reblog", JSON.stringify(this.state.noReblog))
+   
     if (prevState.noReblog !== this.state.noReblog) {
       if (this.state.noReblog === true) {
         this.loadEntries();
