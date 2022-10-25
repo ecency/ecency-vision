59ae840da86080948fee990e6d8e4ca964c69047
ildar.timerbaev <dkildar@gmail.com>
2022-10-25 11:31:15 +0300

    Added unauthorized status

--- src/common/pages/market/swap-mode.tsx
+++ src/common/pages/market/swap-mode.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { MarketSwapForm } from "../../components/market-swap-form";
-import { Col, Row } from "react-bootstrap";
+import { Button, Col, Row } from "react-bootstrap";
 import { PageProps } from "../common";
+import { _t } from "../../i18n";
+import { Link } from "react-router-dom";
 
 export const SwapMode = (props: PageProps) => {
   return (
@@ -15,6 +17,26 @@ export const SwapMode = (props: PageProps) => {
           signingKey={props.signingKey}
           setSigningKey={props.setSigningKey}
         />
+        {!props.activeUser ? (
+          <div className="auth-required d-flex justify-content-center align-items-center flex-column">
+            <div className="font-weight-bold mb-3">{_t("market.auth-required-title")}</div>
+            <div className="mb-3">{_t("market.auth-required-desc")}</div>
+            <div className="d-flex">
+              <Button
+                variant="outline-primary"
+                className="mr-2"
+                onClick={() => props.toggleUIProp("login")}
+              >
+                {_t("g.login")}
+              </Button>
+              <Link to="/signup">
+                <Button variant="primary">{_t("g.signup")}</Button>
+              </Link>
+            </div>
+          </div>
+        ) : (
+          <></>
+        )}
       </Col>
     </Row>
   );
