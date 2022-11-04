953cc43479a64a74c8026a233177e5bf99415e39
Adesojisouljay <adesojiisrael@gmail.com>
2022-11-04 19:27:10 +0100

    fixed typo

--- src/common/components/sort-profile-communities/index.spec.tsx
+++ src/common/components/sort-profile-communities/index.spec.tsx
@@ -3,16 +3,16 @@ import { SortCommunities } from "./index";
 import renderer from "react-test-renderer";
 import { _t } from "../../i18n";
 
-describe("profile community sorting component", () => {
+describe("profile communities sorting component", () => {
     const component = renderer.create(<SortCommunities />);
 
-  it("Should sort communities in ascending and descending order", () => {
+  it("Should sort communities in ascending order", () => {
     const ascendingLabel = component.root.findByProps({ id: "ascending" });
     ascendingLabel.props.onClick();
     expect(ascendingLabel).toBeFalsy;
   });
 
-  it("Should sort communities in descending and descending order", () => {
+  it("Should sort communities in descending order", () => {
     const descendingLabel = component.root.findByProps({ id: "descending" });
     descendingLabel.props.onClick();
     expect(descendingLabel).toBeFalsy;
