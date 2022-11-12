faa7bebc6944abf0d71478a603d7d447fb34ef1d
feruzm <fmiboy@gmail.com>
2022-11-12 10:13:14 +0530

    update and fix tests

--- src/common/components/sort-profile-communities/index.spec.tsx
+++ src/common/components/sort-profile-communities/index.spec.tsx
@@ -3,23 +3,13 @@ import { SortCommunities } from "./index";
 import renderer from "react-test-renderer";
 import { _t } from "../../i18n";
 
-describe("profile communities sorting component", () => {
-    const component = renderer.create(<SortCommunities />);
+const props = {
+  sortCommunitiesInAsc: () => {},
+  sortCommunitiesInDsc: () => {}
+};
 
-  it("Should sort communities in ascending order", () => {
-    const ascendingLabel = component.root.findByProps({ id: "ascending" });
-    ascendingLabel.props.onClick();
-    expect(ascendingLabel).toBeFalsy;
-  });
+const component = renderer.create(<SortCommunities {...props} />);
 
-  it("Should sort communities in descending order", () => {
-    const descendingLabel = component.root.findByProps({ id: "descending" });
-    descendingLabel.props.onClick();
-    expect(descendingLabel).toBeFalsy;
-  });
-  
-  it("matches snapshot", () => {
-    const main = component.toJSON()
-    expect(main).toMatchSnapshot()
-  })
+it("(1) Default render", () => {
+  expect(component.toJSON()).toMatchSnapshot();
 });
