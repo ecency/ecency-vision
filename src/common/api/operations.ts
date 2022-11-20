92241190bec9212ffb5de1df3992a1a1318e9710
ildar.timerbaev <dkildar@gmail.com>
2022-11-20 21:46:14 +0300

    Fixed swap form signing by HS

--- src/common/api/operations.ts
+++ src/common/api/operations.ts
@@ -540,7 +540,7 @@ export const limitOrderCreateHot = (
   ];
 
   const params: Parameters = {
-    callback: `https://ecency.com/market`
+    callback: `https://ecency.com/market${idPrefix === OrderIdPrefix.SWAP ? "#swap" : ""}`
   };
   return hs.sendOperation(op, params, () => {});
 };
