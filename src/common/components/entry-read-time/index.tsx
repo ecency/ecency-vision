38edd5d5a16031f50ccb18c5c7e24ef97e786d18
Adesojisouljay <adesojiisrael@gmail.com>
2022-11-30 09:19:48 +0100

    improve languages word count

--- src/common/components/entry-read-time/index.tsx
+++ src/common/components/entry-read-time/index.tsx
@@ -11,16 +11,47 @@ export const ReadTime = (props: any) => {
   const [wordCount, setWordCount] = useState(0);
 
   useEffect(() => {
+    // countWords(entry.body)
+    // console.log(entry.body)
     calculateExtras();
   }, [entry]);
 
   const calculateExtras = async () => {
-    const wordsWithoutSpace: any = entry?.body.trim()?.split(/\s+/);
-    const totalCount: number = wordsWithoutSpace?.length;
+    // const wordsWithoutSpace: any = entry?.body.trim()?.split(/\s+/);
+    const entryCount = countWords(entry.body)
+    // const totalCount: number = wordsWithoutSpace?.length;
     const wordPerMinuite: number = 225;
-    setWordCount(totalCount);
-    setReadTime(Math.ceil(totalCount / wordPerMinuite));
+    setWordCount(entryCount);
+    setReadTime(Math.ceil(entryCount / wordPerMinuite));
   };
+
+  const countWords = (entry: string) =>{
+    const entryCategory1 = new RegExp('[\u3000-\u4DFF]','g');
+    const entryCategory2 = new RegExp('[\u4E00-\u9FFF]','g');
+    const entryCategory3 = new RegExp('[\u0E00-\u0E7F]','g');
+    entry = entry.replace(entryCategory1,' {PNK} ');
+    entry = entry.replace(entryCategory2,' {CJK} ');
+    entry = entry.replace(entryCategory3,' {THI} ');
+    entry = entry.replace(/(\(|\)|\*|\||\+|\”|\’|_|;|:|,|\.|\?)/ig," ") ;
+    entry = entry.replace(/\s+/ig," ");
+    let splitEntry = entry.trim()?.split(/\s+/);
+    let count: any = 0;
+    let counter1 = 0;
+    // let counter2 = 0;
+    let counter3 = 0;
+    for (let i=0;i<splitEntry.length;i++){
+        if (splitEntry[i]=='{PNK}'){
+              counter1++;
+        }else if(splitEntry[i]=='{THI}'){
+              counter3++;
+        }else if (splitEntry[i].length>0){
+              count++;
+        }
+    }
+    count += Math.ceil(counter1/3) + Math.ceil(counter3/4);
+    console.log(count)
+    return count;
+}
 
   return toolTip ? (
     <div className="post-info">
