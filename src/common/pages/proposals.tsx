a73ff73058c70520d7a261b1571a4a3e8680de07
ildar.timerbaev <dkildar@gmail.com>
2022-12-09 10:58:55 +0300

    Fixed date parsing common function

--- src/common/pages/proposals.tsx
+++ src/common/pages/proposals.tsx
@@ -425,7 +425,7 @@ class ProposalDetailPage extends BaseComponent<DetailProps, DetailState> {
       url: `/proposals/${proposal.id}`,
       canonical: `/proposals/${proposal.id}`,
       published: parseDate(entry.created).toISOString(),
-      modified: moment(entry.updated).isValid() ? moment(entry.updated).toISOString() : "",
+      modified: parseDate(entry.updated).toISOString(),
       image: catchPostImage(entry.body, 600, 500, global.canUseWebp ? "webp" : "match")
     };
     let containerClasses = global.isElectron
