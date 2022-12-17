aa9e1aab954323eacd42b192b27219587a72a234
ildar.timerbaev <dkildar@gmail.com>
2022-12-17 12:25:16 +0300

    Updated tests

--- src/common/components/announcement/index.spec.tsx
+++ src/common/components/announcement/index.spec.tsx
@@ -1,8 +1,13 @@
 import React from "react";
 import Announcement from "./index";
 import renderer from "react-test-renderer";
+import { BrowserRouter } from "react-router-dom";
 
 it("(1) Default render", () => {
-  const component = renderer.create(<Announcement />);
+  const component = renderer.create(
+    <BrowserRouter>
+      <Announcement />
+    </BrowserRouter>
+  );
   expect(component.toJSON()).toMatchSnapshot();
 });
