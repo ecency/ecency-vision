d821aa7289d9163627065ca84ed7b6a2c7d300a0
Talha Saeed <talha.saeed@rolustech.net>
2023-01-13 19:04:57 +0500

    Draft Improvements

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -455,6 +455,23 @@ export const pencilOutlineSvg = (
   </svg>
 );
 
+export const cloneOutlineSvg = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="16"
+    height="16"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2.5"
+    strokeLinecap="square"
+    strokeLinejoin="round"
+  >
+    <rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect>
+    <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path>
+  </svg>
+);
+
 export const pencilOutlinedSvg = (
   <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
     <path
