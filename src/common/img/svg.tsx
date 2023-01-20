7492530fdcf73c84367cce5405aab66bdca8b152
Talha Saeed <talha.saeed@rolustech.net>
2023-01-20 12:13:55 +0500

    svg.tsx syntax issue resolved

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -1756,8 +1756,8 @@ export const rawContentSvg = (
     strokeLinecap="round"
     strokeLinejoin="round"
   >
-    <polyline points="16 18 22 12 16 6"></polyline>
-    <polyline points="8 6 2 12 8 18"></polyline>
+    <polyline points="16 18 22 12 16 6" />
+    <polyline points="8 6 2 12 8 18" />
   </svg>
 );
 export const allStakeSvg = (
