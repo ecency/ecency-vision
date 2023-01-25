24665baaeef47741d3bccd95e9eb644959886e71
Adesojisouljay <adesojiisrael@gmail.com>
2023-01-25 18:55:02 +0100

    Rc modal UI

--- src/common/components/rc-info/index.tsx
+++ src/common/components/rc-info/index.tsx
@@ -8,6 +8,8 @@ import { rcFormatter } from '../../util/formatted-number';
 import { ConfirmDelete } from '../rc-delegations-list';
 
 export const ResourceCreditsInfo = (props: any) => {
+  const radius = 70;
+  const dasharray = 440;
 
     const [showRcInfo, setShowRcInfo] = useState(false);
     const [delegated, setDelegated] = useState();
@@ -20,6 +22,8 @@ export const ResourceCreditsInfo = (props: any) => {
     const [amountFromList, setAmountFromList]: any = useState('')
     const [showConfirmDelete, setShowConfirmDelete] = useState(false);
     const [delegateeData, setDelegateeData] = useState("")
+    const [usedOffset, setUsedOffset] = useState<any>(null);
+    const [unUsedOffset, setUnUsedOffset] = useState<any>(null);
 
     const { rcPercent, account, activeUser } = props;
 
@@ -35,7 +39,9 @@ export const ResourceCreditsInfo = (props: any) => {
             const formatIncoming = rcFormatter(inComing);
             setReceivedDelegation(formatIncoming);
           })
+          getCircleOffsets();
     }, []);
+
 
     const showModal = () => {
         setShowRcInfo(true);
@@ -78,6 +84,14 @@ export const ResourceCreditsInfo = (props: any) => {
   const hideConfirmDelete = () => {
     setShowConfirmDelete(false)
   }
+
+  const getCircleOffsets = () => {
+    const usedOffset = rcPercent / 100 * dasharray;
+    const unUsedOffset = (100 - rcPercent) / 100 * dasharray;
+    setUsedOffset(usedOffset);
+    setUnUsedOffset(unUsedOffset);    
+  }
+
   return (
       <div>
 
@@ -131,29 +145,67 @@ export const ResourceCreditsInfo = (props: any) => {
       </Modal.Header>
       <Modal.Body>
         <div className="container">
-          <div className="rc-info">
-            <p className="h4">{_t("rc-info.resource-credit")}</p>
-            <p>{rcFormatter(resourceCredit)}</p>          
+          <div className="percent">            
+            <div className="circle">            
+              <div className="outer-circle progress">
+                <div className="inner-circle">
+                  <span>
+                  {rcFormatter(resourceCredit)}                
+                  </span>            
+                </div>
+              </div>
+
+              <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px" >    
+                <circle className="container-circle" cx="80" cy="80" r={radius} strokeLinecap="round" style={{fill: "none", strokeWidth: "20px", strokeDashoffset:"0", strokeDasharray: dasharray, stroke: "#357ce6"}} />
+                <circle className="available-circle" cx="80" cy="80" r={radius} strokeLinecap="round" style={{fill: "none", strokeWidth: "20px", strokeDashoffset:unUsedOffset, strokeDasharray:"0", stroke: "none"}} />
+                <circle className="used-circle" cx="80" cy="80" r={radius} strokeLinecap="round" style={{fill: "none", strokeWidth: "20px", strokeDashoffset:usedOffset, strokeDasharray:dasharray, stroke: "gray"}} />
+              </svg>
+            </div>
+
+            <div className="percentage-info">
+              <div className="unused">
+                <div className="unused-box">
+                </div>
+                <span>
+                  {`${_t("rc-info.rc-available")}: ${rcPercent}%`}
+                </span>
+              </div>
+              <div className="used">
+              <div className="used-box">
+                </div>
+                <span>
+                  {`${_t("rc-info.rc-used")}: ${100 - rcPercent}%`}
+                </span>
+              </div>
+            </div>
           </div>
 
-          <div className="rc-info">
-            <p className="h4">{_t("rc-info.rc-available")}</p>
-            <p>{`${rcPercent}%`}</p>          
-          </div>
+          <div className="rc-details">
+              <div className="delegations">
+                <span className="outgoing" onClick={showOutGoingList}>
+                {`${_t("rc-info.delegated")}: ${delegated}`}
+                </span>
+                <span className="incoming" onClick={showIncomingList}>
+                {`${_t("rc-info.received-delegations")}: ${receivedDelegation}`}
+                </span>
+              </div>
 
-          <div className="rc-info">
-            <p className="h4">{_t("rc-info.rc-used")}</p>
-            <p>{`${(100 - rcPercent).toFixed(2)}%`}</p>          
-          </div>
+              <div className="line-break">
+                <hr/>
+              </div>
 
-          <div onClick={showOutGoingList} className="cursor-pointer rc-info">
-            <p className="h4">{_t("rc-info.delegated")}</p>
-            <p>{delegated}</p>          
-          </div>
+              <div className="extra-details">
+                <p>{_t("rc-info.extra-details-heading")}</p>
+                <div className="extras">
+                  <ul>
+                    <li>{_t("rc-info.extra-details-post")}</li>
+                    <li>{_t("rc-info.extra-details-comment")}</li>
+                    <li>{_t("rc-info.extra-details-transfer")}</li>                 
+                    <li>{_t("rc-info.extra-details-upvote")}</li>                 
+                  </ul>
+                </div>
+              </div>
 
-          <div onClick={showIncomingList} className="cursor-pointer rc-info">
-            <p className="h4">{_t("rc-info.received-delegations")}</p>
-            <p>{receivedDelegation}</p>            
           </div>
         </div>
        
