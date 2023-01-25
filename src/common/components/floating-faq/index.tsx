d0935f91e1be8d33d0651efda63dd23d6eeca699
Talha Saeed <talha.saeed@rolustech.net>
2023-01-25 11:26:37 +0500

    Feedback resolved

--- src/common/components/floating-faq/index.tsx
+++ src/common/components/floating-faq/index.tsx
@@ -17,6 +17,8 @@ import { _t } from "../../i18n";
 const FloatingFAQ = () => {
   const [show, setShow] = useState(false);
   const [expandedHelp, setExpandedHelp] = useState(true);
+  const [expandedClass, setExpandedClass] = useState("section d-flex flex-column");
+  const [contactClass, setContactClass] = useState("section d-flex flex-column");
   const [expandedContact, setExpandedContact] = useState(false);
   const [searchText, setSearchText] = useState("");
   const [faqKeys, setFaqKeys] = useState<string[]>([]);
@@ -32,7 +34,7 @@ const FloatingFAQ = () => {
   useEffect(() => {
     let searchResult: string[] = [];
     faqKeys.map((x) => {
-      let isSearchValid = _t(`static.faq.${x}-body`)
+      const isSearchValid = _t(`static.faq.${x}-body`)
         .toLocaleLowerCase()
         .includes(searchText.toLocaleLowerCase());
       if (isSearchValid) {
@@ -46,18 +48,20 @@ const FloatingFAQ = () => {
   }, [searchText]);
 
   useEffect(() => {
-    let faqKeys = [...faqKeysGeneral];
+    const faqKeys = [...faqKeysGeneral];
     setFaqKeys(faqKeys);
     setDefaultFaqKeys(faqKeys.slice(0, 4));
     setDatatoShow(faqKeys.slice(0, 4));
   }, []);
 
-  let cls_help = expandedHelp
-    ? "section d-flex flex-column border-bottom"
-    : "section d-flex flex-column";
-  let cls_contact = expandedContact
-    ? "section d-flex flex-column border-bottom"
-    : "section d-flex flex-column";
+  useEffect(() => {
+    if (expandedHelp) {
+      setExpandedClass("section d-flex flex-column border-bottom");
+    }
+    if (expandedContact) {
+      setContactClass("section d-flex flex-column border-bottom");
+    }
+  }, [expandedHelp, expandedContact]);
 
   return (
     <>
@@ -86,7 +90,7 @@ const FloatingFAQ = () => {
           <div className="faq-content-list">
             <div className="faq-content-list-item">
               <Accordion defaultActiveKey="0">
-                <div className={cls_help}>
+                <div className={expandedClass}>
                   <div className="d-flex justify-content-between align-items-center section-card position-relative">
                     <div className="d-flex align-items-center">
                       <div className="d-flex align-items-center ml-3">
@@ -144,7 +148,7 @@ const FloatingFAQ = () => {
             </div>
             <div className="faq-content-list-item contact-us">
               <Accordion>
-                <div className={cls_contact}>
+                <div className={contactClass}>
                   <div className="d-flex justify-content-between align-items-center section-card position-relative">
                     <div className="d-flex align-items-center">
                       <div className="d-flex align-items-center ml-3">
