d820f524020e155532d8dc34ab03805b4f5be462
ildar.timerbaev <dkildar@gmail.com>
2023-02-10 21:41:21 +0300

    Updated username validation in sign up page

--- src/common/pages/sign-up.tsx
+++ src/common/pages/sign-up.tsx
@@ -89,17 +89,19 @@ export const SignUp = (props: PageProps) => {
     if (!username && !usernameTouched) {
       return;
     }
-    if (username.length < 3) {
-      setUsernameError(_t("sign-up.username-min-length-error"));
-    } else if (username.length > 16) {
-      setUsernameError(_t("sign-up.username-max-length-error"));
-    } else if (!/^[\x00-\x7F]*$/.test(username[0])) {
-      setUsernameError(_t("sign-up.username-no-ascii-first-letter-error"));
-    } else if (!/^([a-zA-Z0-9]|-|\.)+$/.test(username)) {
-      setUsernameError(_t("sign-up.username-contains-symbols-error"));
-    } else if (username.includes("--")) {
-      setUsernameError(_t("sign-up.username-contains-double-hyphens"));
-    }
+    username.split(".").some((item) => {
+      if (item.length < 3) {
+        setUsernameError(_t("sign-up.username-min-length-error"));
+      } else if (item.length > 16) {
+        setUsernameError(_t("sign-up.username-max-length-error"));
+      } else if (!/^[\x00-\x7F]*$/.test(item[0])) {
+        setUsernameError(_t("sign-up.username-no-ascii-first-letter-error"));
+      } else if (!/^([a-zA-Z0-9]|-|\.)+$/.test(item)) {
+        setUsernameError(_t("sign-up.username-contains-symbols-error"));
+      } else if (item.includes("--")) {
+        setUsernameError(_t("sign-up.username-contains-double-hyphens"));
+      }
+    });
   }, [username, usernameTouched]);
 
   const regularRegister = async () => {
