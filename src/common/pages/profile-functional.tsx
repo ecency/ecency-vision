f74e4f8f670dcd9bce22619290fa6d2c98f02047
feruz <fmiboy@gmail.com>
2023-02-22 18:59:48 +0200

    fix few undefined objects

--- src/common/pages/profile-functional.tsx
+++ src/common/pages/profile-functional.tsx
@@ -359,7 +359,7 @@ export const Profile = (props: Props) => {
       return {};
     }
 
-    return account.__loaded
+    return account?.__loaded
       ? {
           title: `${ncount}${account.profile?.name || account.name}'s ${
             section ? (section === "engine" ? "tokens" : `${section}`) : ""
@@ -434,7 +434,7 @@ export const Profile = (props: Props) => {
       >
         <div className="profile-side">{ProfileCard({ ...props, account, section })}</div>
         <span itemScope={true} itemType="http://schema.org/Person">
-          {account.__loaded && (
+          {account?.__loaded && (
             <meta itemProp="name" content={account.profile?.name || account.name} />
           )}
         </span>
