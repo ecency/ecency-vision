a94076e137c71c10bc06fff85d6ab17945584eca
feruz <fmiboy@gmail.com>
2023-02-23 23:03:49 +0200

    catch type remove

--- src/common/components/editor-toolbar/index.tsx
+++ src/common/components/editor-toolbar/index.tsx
@@ -368,7 +368,7 @@ export class EditorToolbar extends Component<Props> {
       } else {
         error(_t("editor-toolbar.image-error-cache"));
       }
-    } catch (e: unknown) {
+    } catch (e) {
       if (axios.isAxiosError(e) && e.response?.status === 413) {
         error(_t("editor-toolbar.image-error-size"));
       } else {
