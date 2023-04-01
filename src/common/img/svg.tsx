f2b82f2ce3a84014fc2c766248b29d68cd835770
Talha Saeed <talha.saeed@rolustech.net>
2023-04-01 15:55:54 +0500

    Test Updated

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -1829,6 +1829,24 @@ export const sellStakeSvg = (
   </svg>
 );
 
+export const KebabMenu = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="20"
+    height="20"
+    viewBox="0 0 24 24"
+    fill="none"
+    stroke="currentColor"
+    strokeWidth="2"
+    strokeLinecap="square"
+    strokeLinejoin="round"
+  >
+    <circle cx="12" cy="12" r="1" />
+    <circle cx="12" cy="5" r="1" />
+    <circle cx="12" cy="19" r="1" />
+  </svg>
+);
+
 export const kebabMenuHorizontalSvg = (
   <svg
     xmlns="http://www.w3.org/2000/svg"
