4523a24c4228c20157a899e2f6a3648065ab6253
Talha Saeed <talha.saeed@rolustech.net>
2023-04-14 10:29:47 +0500

    Svg issue fixed

--- src/common/img/svg.tsx
+++ src/common/img/svg.tsx
@@ -1929,8 +1929,9 @@ export const kebabMenuHorizontalSvg = (
     strokeLinecap="square"
     strokeLinejoin="round"
   >
-    <rect x="9" y="9" width="13" height="13" rx="2" ry="2" />
-    <path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1" />
+    <circle cx="12" cy="12" r="1" />
+    <circle cx="19" cy="12" r="1" />
+    <circle cx="5" cy="12" r="1" />
   </svg>
 );
 
