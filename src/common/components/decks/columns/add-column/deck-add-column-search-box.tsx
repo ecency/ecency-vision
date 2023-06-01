893ecb79afc170b2a92611cf61ba583aa6a778f6
ildar.timerbaev <dkildar@gmail.com>
2023-06-01 12:23:30 +0300

    Deck 2.1: Allow to search username with active user

--- src/common/components/decks/columns/add-column/deck-add-column-search-box.tsx
+++ src/common/components/decks/columns/add-column/deck-add-column-search-box.tsx
@@ -43,7 +43,7 @@ export const DeckAddColumnSearchBox = ({
         return;
       }
 
-      if (usernameInput === "" || activeUser?.username === usernameInput) {
+      if (usernameInput === "") {
         setIsUsernameDataLoading(false);
         return;
       }
