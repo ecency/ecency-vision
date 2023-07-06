d7dd68e0039cc192adf4d273ea2ff69c2d7d7a7d
Adesojisouljay <adesojiisrael@gmail.com>
2023-07-06 21:33:44 +0100

    require memo on exchange account

---
+++ src/common/constants/exchanges.ts
@@ -0,0 +1,11 @@
+// Should be updated with more exchange names
+const exchangeAccounts = [
+    "deepcrypto8", 
+    "gateiodeposit", 
+    "probithive", 
+    "mxchive", 
+    "huobi-pro", 
+    "coinexdeposit"
+]
+
+export default exchangeAccounts;
