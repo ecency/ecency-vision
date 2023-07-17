ce424eb22297774d8e6a71a4ddf2e344b40a2a50
ildar.timerbaev <dkildar@gmail.com>
2023-07-17 21:27:34 +0300

    Swap form: updated tests

--- src/common/components/market-swap-form/market-info.spec.tsx
+++ src/common/components/market-swap-form/market-info.spec.tsx
@@ -2,6 +2,8 @@ import { MarketInfo, Props } from "./market-info";
 import { MarketAsset } from "./market-pair";
 import React from "react";
 import { withStore } from "../../tests/with-store";
+import { queryClient } from "../../core";
+import { QueryClientProvider } from "@tanstack/react-query";
 
 it("should render market rate", function () {
   const props: Props = {
@@ -11,6 +13,10 @@ it("should render market rate", function () {
     toAsset: MarketAsset.HIVE,
     usdFromMarketRate: 12
   };
-  const component = withStore(<MarketInfo {...props} />);
+  const component = withStore(
+    <QueryClientProvider client={queryClient}>
+      <MarketInfo {...props} />
+    </QueryClientProvider>
+  );
   expect(component.toJSON()).toMatchSnapshot();
 });
