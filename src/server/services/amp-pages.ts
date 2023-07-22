828fc2530a71fc99f3112f2e85f2645ed7e28856
feruz <fmiboy@gmail.com>
2023-07-22 17:43:05 +0300

    test loadbalancing

--- src/server/services/amp-pages.ts
+++ src/server/services/amp-pages.ts
@@ -18,7 +18,7 @@ export async function getAsAMP(
 ): Promise<string> {
   const client = createClient({
     socket: {
-      host: "cache",
+      host: "redis",
       port: 6379
     },
     password: config.redisPass
