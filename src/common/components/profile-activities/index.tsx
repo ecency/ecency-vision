04bf2381a1a78d1f4532c2af80eabed8d8d12dad
Adesojisouljay <adesojiisrael@gmail.com>
2023-08-07 20:04:31 +0100

    sort activities

--- src/common/components/profile-activities/index.tsx
+++ src/common/components/profile-activities/index.tsx
@@ -6,12 +6,16 @@ import { Link } from "react-router-dom"
 import LinearProgress from "../linear-progress"
 import { Account } from "../../store/accounts/types"
 import { Button } from "react-bootstrap"
+import ProfileLink from "../profile-link";
+import UserAvatar from "../user-avatar";
+import activeUser from "../../store/active-user"
+import { Transaction } from "@hiveio/dhive"
+import { DynamicProps } from "../../store/dynamic-props/types"
 
-interface Props{
-  account: Account
-}
+// interface Props{
+// }
 
-export const ProfileActivites = (props: Props) => {
+export const ProfileActivites = (props: any) => {
 
   const { account } = props;
 
@@ -19,9 +23,10 @@ export const ProfileActivites = (props: Props) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-  getActivities();
+    console.log(props)
+    getActivities();
   },[])
-
+  
   const getActivities = async () => {
     setLoading(true)
     try {
@@ -41,8 +46,8 @@ export const ProfileActivites = (props: Props) => {
           data,
         };
       });
-      console.log(accountHistory.reverse())
-      setActivities(accountHistory.reverse())
+      console.log(accountHistory)
+      setActivities(accountHistory)
       setLoading(false)
       return accountHistory;
       
@@ -51,75 +56,295 @@ export const ProfileActivites = (props: Props) => {
     }
   }
 
+  const handleCustomJson = (a: any): any => {
+    let jsonData;
+      try {
+        if(a?.data?.json){
+          jsonData = JSON.parse(a?.data?.json)
+          console.log(jsonData)
+        };              
+      } catch (error) {
+        console.log(error)
+      }
+      return jsonData
+  }
+
   return (
-    <div className="profile-activities">
+    <>
       {loading && <LinearProgress/>}
         <div className="activities-container">
-          {activities?.map((a: any) => {return (
-            <>
-              {a.type === "comment" ? <div className="activities">
+          <div className="activities-page-info">
+            <span>All activities related to <Link to={`/@${account?.name}`}>@{account?.name}'s</Link> account</span>
+          </div>
+          {activities?.map((a: any, i: number) => {
+           const jsonData = handleCustomJson(a)
+            return (
+            <div key={i}>
+              {a.type === "comment" && a.data.author === account?.name && a.data.parent_author !== "" ? <div className="activities">
                 <div className="activities-header">
                   <h4>Comment</h4>
                 </div>
                 <div className="activities-info-wrapper">
                   <div className="activities-details">
-                    <span>Authour: @{a.data.author}</span>
-                    <span>Body: {a.data.body.substring(0, 100)}</span>
-                    <span>Permlink: {a.data.permlink}</span>
-                    <span className="mr-1">Post Link: 
-                      <Link to={`/@${a.data.author}/${a.data.permlink}`}>{`/@${a.data.author}/${a.data.permlink}`}</Link>
+                    <div className="auhtor-wrapper">
+                    <span>Author:</span>                   
+                      <span>
+                        {ProfileLink({
+                              ...props,
+                              username: a.data.author,
+                              children: <UserAvatar username={a.data.author} size="small" />
+                            })}
+                      </span>
+                      <span className="item-info">
+                        {ProfileLink({
+                          ...props,
+                          username: a.data.author,
+                          children: <span className="item-name notranslate">{a.data.author}</span>
+                        })}
+                      </span>
+                    </div>
+                    <span>
+                      <a href="#">@{a.data.author}</a>
+                      <span> commented on </span>
+                      <Link to={`/${a.data.parent_permlink}/@${a.data.author}/${a.data.permlink}`}>
+                        {a.data.parent_permlink} 
+                      </Link>
+                      <span> by </span>
+                      <a href="#" className="ml-1">@{a.data.parent_author === "" ? a.data.author : a.data.parent_author}</a>
                     </span>
                     <span>Time: {dateToFullRelative(a.timestamp)}</span>
                   </div>
                 </div>
               </div> 
-              : a.type === "effective_comment_vote" ? <div className="activities">
+              : a.type === "comment" && a.data.parent_author === account?.name && a.data.author !== account?.name ? <div className="activities">
                 <div className="activities-header">
-                  <h4>Effective comment vote</h4>
+                  <h4>Reply</h4>
                 </div>
                 <div className="activities-info-wrapper">
                   <div className="activities-details">
-                    <span>Authour: @{a.data.author}</span>
-                    <span>Pending payout: {a.data.pending_payout}</span>
-                    <span>Post Link: {a.data.permlink}</span>
-                    <span>Voter: {a.data.voter}</span>
+                    <div className="auhtor-wrapper">
+                    <span>Author:</span>                   
+                      <span>
+                        {ProfileLink({
+                              ...props,
+                              username: a.data.author,
+                              children: <UserAvatar username={a.data.author} size="small" />
+                            })}
+                      </span>
+                      <span className="item-info">
+                        {ProfileLink({
+                          ...props,
+                          username: a.data.author,
+                          children: <span className="item-name notranslate">{a.data.author}</span>
+                        })}
+                      </span>
+                    </div>
+                    <span>
+                      <a href="#">@{a.data.author}</a>
+                      <span> replied to </span>
+                      <Link to={`/${a.data.parent_permlink}/@${a.data.author}/${a.data.permlink}`}>
+                        {a.data.parent_permlink} 
+                      </Link>
+                      <span> by </span>
+                      <a href="#" className="ml-1">@{a.data.parent_author === "" ? a.data.author : a.data.parent_author}</a>
+                    </span>
                     <span>Time: {dateToFullRelative(a.timestamp)}</span>
-                  </div>                  
+                  </div>
                 </div>
-            </div> : a.type === "vote" ? <div className="activities">
+              </div> 
+             : a.type === "vote" && a.data.voter === account?.name ? <div className="activities">
               <div className="activities-header">
                 <h4>Vote</h4>
               </div>
               <div className="activities-info-wrapper">
                 <div className="activities-details">
-                  <span>Authour: @{a.data.author}</span>
-                  <span>Post permlink: {a.data.permlink}</span>
-                  <span>Voter: {a.data.voter}</span>
-                  <span>Weight: {a.data.weight / 100}%</span>
+                    <div className="auhtor-wrapper">
+                      <span>Author:</span>                      
+                      <span> 
+                        {ProfileLink({
+                              ...props,
+                              username: a.data.author,
+                              children: <UserAvatar username={a.data.author} size="small" />
+                            })}
+                      </span>
+                      <span className="item-info">
+                        {ProfileLink({
+                          ...props,
+                          username: a.data.author,
+                          children: <span className="item-name notranslate">{a.data.author}</span>
+                        })}
+                      </span>
+                    </div>
+                    <span>
+                      <a href="#">@{a.data.voter}</a>
+                      <span> voted on </span>
+                      <Link to={`/@${a.data.author}/${a.data.permlink}`}>
+                        {a.data.permlink} 
+                      </Link>
+                      <span> by </span>
+                      <a href="#" className="ml-1">@{a.data.author}</a>
+                    </span>
                   <span>Time: {dateToFullRelative(a.timestamp)}</span>
                 </div>
               </div>
-            </div> : a.type === "curation_reward" ? <div className="activities">
+            </div> 
+             : a.type === "proposal_pay" ? <div className="activities">
               <div className="activities-header">
-                <h4>Curation reward</h4>
+                <h4>Proposal pay</h4>
               </div>
               <div className="activities-info-wrapper">
                 <div className="activities-details">
-                  <span>Authour: @{a.data.comment_author}</span>
-                  <span>Post permlink: {a.data.comment_permlink}</span>
-                  <span>Curator: {a.data.curator}</span>
-                  <span>Reward: {a.data.reward}</span>
+                    <span>
+                      <a href="#">@{a.data.receiver}</a>
+                      <span> received {a.data.payment} from </span>
+                      <a href="#" className="ml-1">@{a.data.payer}</a>
+                    </span>
                   <span>Time: {dateToFullRelative(a.timestamp)}</span>
                 </div>
               </div>
             </div>
-              : null}
-            </>
+            // Crashes page will check later
+             : (a.data.id === "follow" && jsonData[1]!?.what.includes("blog")) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Follow</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{jsonData[1]?.follower}</a>
+                      <span> started following </span>
+                      <a href="#" className="ml-1">@{jsonData[1].following}</a>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : (a.data.id === "follow" && !jsonData[1].what.includes("blog")) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Unfollow</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{jsonData[1].follower}</a>
+                      <span> unfollowed </span>
+                      <a href="#" className="ml-1">@{jsonData[1].following}</a>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : a.data.id === "community" && jsonData?.includes("subscribe") ? <div className="activities">
+              <div className="activities-header">
+                <h4>Subscribe</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{account?.name}</a>
+                      <span> Subscribed to community</span>
+                      <Link to={`/created/${jsonData[1]?.community}`} className="ml-1">{jsonData[1]?.community}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : a.data.id === "community" && jsonData?.includes("unsubscribe") ? <div className="activities">
+              <div className="activities-header">
+                <h4>Unsubscribe</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{account?.name}</a>
+                      <span> unsubscribed from community</span>
+                      <Link to={`/created/${jsonData[1].community}`} className="ml-1">{jsonData[1].community}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : (a.type === "account_witness_vote" && a.data.approve) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Witness Vote</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{a.data.account}</a>
+                      <span> voted witness </span>
+                      <Link to={`/@${a.data.witness}`} className="ml-1">@{a.data.witness}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : (a.type === "account_witness_vote" && !a.data.approve) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Unapprove witness Vote</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{a.data.account}</a>
+                      <span> unvoted witness </span>
+                      <Link to={`/@${a.data.witness}`} className="ml-1">@{a.data.witness}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : (a.type === "update_proposal_votes" && a.data.approve) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Approve proposal</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{a.data.voter}</a>
+                      <span> approved </span>
+                      <Link to={`/proposals/${a.data.proposal_ids}`} className="ml-1">proposal#{a.data.proposal_ids}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : (a.type === "update_proposal_votes" && !a.data.approve) ? <div className="activities">
+              <div className="activities-header">
+                <h4>Unapprove proposal</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{a.data.voter}</a>
+                      <span> unapproved </span>
+                      <Link to={`/proposals/${a.data.proposal_ids}`} className="ml-1">proposal#{a.data.proposal_ids}</Link>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+             : a.type === "account_update2" ? <div className="activities">
+              <div className="activities-header">
+                <h4>Account update</h4>
+              </div>
+              <div className="activities-info-wrapper">
+                <div className="activities-details">
+                    <span>
+                      <a href="#">@{a.data.account}</a>
+                      <span> updated their account</span>
+                    </span>
+                  <span>Time: {dateToFullRelative(a.timestamp)}</span>
+                </div>
+              </div>
+            </div> 
+            : null}
+            </div>
           )})}
         </div>
-        <div className="justify-self-center mt-3">
+        {!loading && <div className="justify-self-center mt-3">
           <Button className="w-100">Load more</Button>
-        </div>
-    </div>
+        </div>}
+    </>
   )
 }
