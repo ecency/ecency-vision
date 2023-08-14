20a20c8b6a8a0277f990cf62ce83cd1295f1e983
ildar.timerbaev <dkildar@gmail.com>
2023-08-15 00:32:17 +0300

    Submit page 2.0: added transformer of old draft to new one

--- src/common/pages/submit/hooks/advanced-manager.ts
+++ src/common/pages/submit/hooks/advanced-manager.ts
@@ -2,8 +2,16 @@ import useLocalStorage from "react-use/lib/useLocalStorage";
 import { PREFIX } from "../../../util/local-storage";
 import { BeneficiaryRoute, RewardType } from "../../../api/operations";
 import { useState } from "react";
+import { Advanced } from "../types";
+import useMount from "react-use/lib/useMount";
+import { useThreeSpeakManager } from "./three-speak-manager";
 
 export function useAdvancedManager() {
+  // @deprecated
+  const [localAdvanced, setLocalAdvanced, removeLocalAdvanced] = useLocalStorage<Advanced>(
+    PREFIX + "local_advanced"
+  );
+
   const [advanced, setAdvanced] = useState(false);
   const [reward, setReward] = useLocalStorage<RewardType>(PREFIX + "_sa_r", "default");
   const [beneficiaries, setBeneficiaries] = useLocalStorage<BeneficiaryRoute[]>(
@@ -13,6 +21,26 @@ export function useAdvancedManager() {
   const [description, setDescription] = useLocalStorage<string | null>(PREFIX + "_sa_d", null);
   const [schedule, setSchedule] = useLocalStorage<string | null>(PREFIX + "_sa_s", null);
   const [reblogSwitch, setReblogSwitch] = useLocalStorage(PREFIX + "_sa_rb", false);
+
+  const threeSpeakManager = useThreeSpeakManager();
+
+  useMount(() => {
+    if (localAdvanced) {
+      setReward(localAdvanced.reward);
+      setBeneficiaries(localAdvanced.beneficiaries);
+      setSchedule(localAdvanced.schedule);
+      setReblogSwitch(localAdvanced.reblogSwitch);
+      setDescription(localAdvanced.description);
+      threeSpeakManager.setIs3Speak(localAdvanced.isThreespeak);
+      threeSpeakManager.setSpeakAuthor(localAdvanced.speakAuthor);
+      threeSpeakManager.setSpeakPermlink(localAdvanced.speakPermlink);
+      threeSpeakManager.setVideoId(localAdvanced.videoId);
+      threeSpeakManager.setIsNsfw(localAdvanced.isNsfw);
+      threeSpeakManager.setVideoMetadata(localAdvanced.videoMetadata);
+
+      removeLocalAdvanced();
+    }
+  });
 
   return {
     advanced,
