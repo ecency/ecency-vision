7b32020adc8e0a693ac362330119532002291016
ildar.timerbaev <dkildar@gmail.com>
2023-09-20 21:48:13 +0300

    Added attached videos collapsing when its unfocused

--- src/common/pages/submit/submit-video-attachments.tsx
+++ src/common/pages/submit/submit-video-attachments.tsx
@@ -1,5 +1,5 @@
 import { useThreeSpeakManager } from "./hooks";
-import React, { useMemo } from "react";
+import React, { useMemo, useState } from "react";
 import { proxifyImageSrc } from "@ecency/render-helper";
 import { useMappedStore } from "../../store/use-mapped-store";
 import { closeSvg } from "../../img/svg";
@@ -10,10 +10,15 @@ export function SubmitVideoAttachments() {
   const { global } = useMappedStore();
   const { videos, remove, hasMultipleUnpublishedVideo } = useThreeSpeakManager();
 
+  const [collapsed, setCollapsed] = useState(true);
   const videoList = useMemo(() => [...Object.values(videos)], [videos]);
 
   return (
-    <div className="submit-video-attachments m-3">
+    <div
+      className="submit-video-attachments m-3"
+      onMouseEnter={() => setCollapsed(false)}
+      onMouseLeave={() => setCollapsed(true)}
+    >
       {videoList.length > 0 ? <p className="text-muted">Attached 3Speak videos</p> : <></>}
       {hasMultipleUnpublishedVideo && (
         <Alert variant="danger">{_t("submit.should-be-only-one-unpublished")}</Alert>
@@ -24,6 +29,9 @@ export function SubmitVideoAttachments() {
             className="attachment-item position-relative"
             key={item._id}
             style={{
+              height: collapsed ? "4rem" : "9rem",
+              minWidth: collapsed ? "5.5rem" : "12rem",
+              maxWidth: collapsed ? "5.5rem" : "12rem",
               backgroundImage: `url("${proxifyImageSrc(
                 item.thumbUrl,
                 512,
@@ -32,11 +40,17 @@ export function SubmitVideoAttachments() {
               )}")`
             }}
           >
-            <div className="type">3speak</div>
-            <div className="remove" onClick={() => remove(item._id)}>
-              {closeSvg}
-            </div>
-            <div className="title text-truncate">{item.title}</div>
+            {collapsed ? (
+              <></>
+            ) : (
+              <>
+                <div className="type">3speak</div>
+                <div className="remove" onClick={() => remove(item._id)}>
+                  {closeSvg}
+                </div>
+                <div className="title text-truncate">{item.title}</div>
+              </>
+            )}
           </div>
         ))}
       </div>
