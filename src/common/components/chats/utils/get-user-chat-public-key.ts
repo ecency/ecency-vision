18140d64e828b49d718b3fe109c50db806b7bf8f
Talha Saeed <talha.saeed@rolustech.net>
2023-10-03 11:47:31 +0500

    Remove commented code

--- src/common/components/chats/utils/get-user-chat-public-key.ts
+++ src/common/components/chats/utils/get-user-chat-public-key.ts
@@ -1,16 +1,5 @@
 import { getAccountFull } from "../../../api/hive";
 
-// export const getUserChatPublicKey = async (username: string) => {
-//   const response = await getAccountFull(username);
-//   console.log("response", response);
-//   const { posting_json_metadata } = response;
-//   const profile = JSON.parse(posting_json_metadata!).profile;
-//   console.log("profile", profile);
-//   const { nsKey } = profile || {};
-//   console.log("nsKey", nsKey);
-//   return nsKey;
-// };
-
 export const getUserChatPublicKey = async (username: string) => {
   const response = await getAccountFull(username);
   if (response && response.posting_json_metadata) {
