382d4520c167066089f2570d3b9181ae485ae151
ildar.timerbaev <dkildar@gmail.com>
2023-11-20 21:10:56 +0600

    Chats: show only PIN in manage chat key

--- src/common/features/chats/components/manage-chat-key.tsx
+++ src/common/features/chats/components/manage-chat-key.tsx
@@ -3,36 +3,25 @@ import { _t } from "../../../i18n";
 import { InputGroupCopyClipboard } from "@ui/input";
 import qrcode from "qrcode";
 import { classNameObject } from "../../../helper/class-name-object";
-import { useKeysQuery } from "../queries/keys-query";
-import { useGetAccountFullQuery } from "../../../api/queries";
 import { useMappedStore } from "../../../store/use-mapped-store";
-import { getUserChatPrivateKey } from "../utils";
+import { PREFIX } from "../../../util/local-storage";
 
 export default function ManageChatKey() {
   const { activeUser } = useMappedStore();
-  const { privateKey, publicKey } = useKeysQuery();
 
-  const { data: fullAccount } = useGetAccountFullQuery(activeUser?.username);
+  const pin = useMemo(() => localStorage.getItem(PREFIX + "_nostr_pr_" + activeUser?.username), []);
   const qrImgRef = useRef<HTMLImageElement | null>(null);
   const [isQrShow, setIsQrShow] = useState(false);
 
-  const { iv } = useMemo(
-    () => (fullAccount ? getUserChatPrivateKey(fullAccount) : { iv: undefined }),
-    [fullAccount]
-  );
-
   useEffect(() => {
-    if (privateKey) {
+    if (pin) {
       compileQR();
     }
-  }, [privateKey]);
+  }, [pin]);
 
   const compileQR = async () => {
     if (qrImgRef.current) {
-      qrImgRef.current.src = await qrcode.toDataURL(
-        `Private key: ${privateKey}. Public key: ${publicKey}. Initial vector value: ${iv}`,
-        { width: 300 }
-      );
+      qrImgRef.current.src = await qrcode.toDataURL(pin!!, { width: 300 });
       setIsQrShow(true);
     }
   };
@@ -40,12 +29,8 @@ export default function ManageChatKey() {
   return (
     <div className="flex gap-4 flex-col w-full">
       <div className="text-sm text-gray-600">{_t("chat.chat-priv-key")}</div>
-      <div className="text-sm">{_t("chat.public-key")}</div>
-      <InputGroupCopyClipboard value={publicKey ?? ""} />
-      <div className="text-sm">{_t("chat.private-key")}</div>
-      <InputGroupCopyClipboard value={privateKey ?? ""} />
-      <div className="text-sm">{_t("chat.import.iv")}</div>
-      <InputGroupCopyClipboard value={iv ?? ""} />
+      <div className="text-sm">PIN</div>
+      <InputGroupCopyClipboard value={pin ?? ""} />
       <img
         ref={qrImgRef}
         className={classNameObject({
