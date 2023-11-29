4e75b7d43c54b62e3b04573398f9acfe85844ea6
ildar.timerbaev <dkildar@gmail.com>
2023-11-29 23:52:42 +0600

    Chats: require the PIN when opening credentials management

--- src/common/features/chats/components/manage-chat-key.tsx
+++ src/common/features/chats/components/manage-chat-key.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useMemo, useRef, useState } from "react";
 import { _t } from "../../../i18n";
-import { InputGroupCopyClipboard } from "@ui/input";
+import { CodeInput, InputGroupCopyClipboard } from "@ui/input";
 import qrcode from "qrcode";
 import { classNameObject } from "../../../helper/class-name-object";
 import { useMappedStore } from "../../../store/use-mapped-store";
@@ -12,6 +12,9 @@ export default function ManageChatKey() {
   const { activeUser } = useMappedStore();
 
   const qrImgRef = useRef<HTMLImageElement | null>(null);
+
+  const [isUnlocked, setIsUnlocked] = useState(false);
+  const [validationPin, setValidationPin] = useState("");
   const [isQrShow, setIsQrShow] = useState(false);
 
   const { publicKey, privateKey, iv } = useKeysQuery();
@@ -30,6 +33,12 @@ export default function ManageChatKey() {
   );
 
   useEffect(() => {
+    if (validationPin === pin) {
+      setIsUnlocked(true);
+    }
+  }, [validationPin, pin]);
+
+  useEffect(() => {
     if (ecencyKey) {
       compileQR();
     }
@@ -44,32 +53,46 @@ export default function ManageChatKey() {
 
   return (
     <div className="flex gap-4 flex-col w-full">
-      <div className="text-sm text-gray-600">{_t("chat.chat-priv-key")}</div>
-      <div className="text-sm">PIN</div>
-      <InputGroupCopyClipboard value={pin ?? ""} />
-      <div className="text-sm mt-4">Ecency key</div>
-      <InputGroupCopyClipboard value={ecencyKey ?? ""} />
-      <img
-        ref={qrImgRef}
-        className={classNameObject({
-          "self-center border rounded-xl border-[--border-color]": true,
-          hidden: !isQrShow,
-          block: isQrShow
-        })}
-      />
-      <Button
-        className="mx-auto"
-        onClick={() => {
-          const blob = new Blob([ecencyKey], { type: "text/plain" });
-          const a = document.createElement("a");
-          a.href = window.URL.createObjectURL(new File([blob], "ecency-key.txt"));
-          a.download = "ecency-key.txt";
-          a.click();
-          document.removeChild(a);
-        }}
-      >
-        {_t("g.download")}
-      </Button>
+      {isUnlocked ? (
+        <>
+          <div className="text-sm text-gray-600">{_t("chat.chat-priv-key")}</div>
+          <div className="text-sm">PIN</div>
+          <InputGroupCopyClipboard value={pin ?? ""} />
+          <div className="text-sm mt-4">Ecency key</div>
+          <InputGroupCopyClipboard value={ecencyKey ?? ""} />
+          <img
+            ref={qrImgRef}
+            className={classNameObject({
+              "self-center border rounded-xl border-[--border-color]": true,
+              hidden: !isQrShow,
+              block: isQrShow
+            })}
+          />
+          <Button
+            className="mx-auto"
+            onClick={() => {
+              const blob = new Blob([ecencyKey], { type: "text/plain" });
+              const a = document.createElement("a");
+              a.href = window.URL.createObjectURL(new File([blob], "ecency-key.txt"));
+              a.download = "ecency-key.txt";
+              a.click();
+              document.removeChild(a);
+            }}
+          >
+            {_t("g.download")}
+          </Button>
+        </>
+      ) : (
+        <>
+          <div className="text-center text-gray-600 dark:text-gray-400">
+            {_t("chat.unlock-the-section")}
+          </div>
+          <CodeInput value={validationPin} setValue={setValidationPin} codeSize={8} />
+          {validationPin !== pin && validationPin.length === pin?.length && (
+            <div className="text-red text-sm">{_t("chat.welcome.pin-failed")}</div>
+          )}
+        </>
+      )}
     </div>
   );
 }
