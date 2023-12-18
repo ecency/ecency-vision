cc96b56379f9afd1de43e8d0a82070aedb362a47
ildar.timerbaev <dkildar@gmail.com>
2023-12-18 18:52:53 +0600

    Chats: fixed showing dates in messages list

--- src/common/features/chats/utils/group-messages.ts
+++ src/common/features/chats/utils/group-messages.ts
@@ -8,8 +8,12 @@ export function groupMessages(messages: Message[]) {
 
     // If difference less than 5 minutes then we group them together
     const differenceInDates = item.created - messages[currentIndex - 1].created;
-    if (differenceInDates * 1000 < 120000) {
-      acc[acc.length - 1][1].push(item);
+    const groupItems = acc[acc.length - 1][1];
+    if (
+      differenceInDates * 1000 < 120000 &&
+      groupItems[groupItems.length - 1].creator === item.creator
+    ) {
+      groupItems.push(item);
       return acc;
     }
 
