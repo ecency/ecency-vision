916d3df7c56515a493ade4c61b5364f49c4a381b
ildar.timerbaev <dkildar@gmail.com>
2024-01-30 22:21:10 +0600

    Chats: re-worked chat button in community page

--- src/common/features/chats/components/join-community-chat-btn.tsx
+++ src/common/features/chats/components/join-community-chat-btn.tsx
@@ -1,19 +1,12 @@
-import React, { useContext, useMemo } from "react";
+import React, { useMemo } from "react";
 import { History } from "history";
 import { Community } from "../../../store/communities";
 import { _t } from "../../../i18n";
 import { Spinner } from "@ui/spinner";
 import { Button } from "@ui/button";
-import {
-  ChatContext,
-  useAddCommunityChannel,
-  useChannelsQuery,
-  useCommunityChannelQuery,
-  useCreateCommunityChat,
-  useLeaveCommunityChannel,
-  useLeftCommunityChannelsQuery
-} from "@ecency/ns-query";
+import { useCommunityChannelQuery, useCreateCommunityChat } from "@ecency/ns-query";
 import { useMappedStore } from "../../../store/use-mapped-store";
+import { messageSendSvg } from "../../../img/svg";
 
 interface Props {
   history: History;
@@ -21,70 +14,30 @@ interface Props {
 }
 
 export default function JoinCommunityChatBtn(props: Props) {
-  const { hasUserJoinedChat } = useContext(ChatContext);
   const { data: currentChannel, isLoading: isCurrentChannelLoading } = useCommunityChannelQuery(
     props.community
   );
   const { activeUser } = useMappedStore();
-  const { data: channels } = useChannelsQuery();
-  const { data: leftChannelsIds, isLoading: isChannelsIdsLoading } =
-    useLeftCommunityChannelsQuery();
 
-  const { mutateAsync: addCommunityChannel, isLoading: isAddCommunityChannelLoading } =
-    useAddCommunityChannel(currentChannel);
   const { mutateAsync: createCommunityChat, isLoading: isCreateCommunityChatLoading } =
     useCreateCommunityChat(props.community);
-  const { mutateAsync: leaveCommunityChannel, isLoading: isLeavingCommunityChannelLoading } =
-    useLeaveCommunityChannel(currentChannel);
-
-  const isGlobalLoading = useMemo(
-    () => isChannelsIdsLoading || isCurrentChannelLoading,
-    [isChannelsIdsLoading, isCurrentChannelLoading]
-  );
   const isCommunityChannelCreated = useMemo(() => !!currentChannel, [currentChannel]);
-  const isCommunityChannelJoined = useMemo(
-    () =>
-      channels?.some(
-        (item) =>
-          item.communityName === props.community.name &&
-          !leftChannelsIds?.includes(currentChannel?.id!)
-      ),
-    [channels, leftChannelsIds]
-  );
 
   // For now only Ecency official account able to start community channels
   const isAbleToCreateChannel = useMemo(() => activeUser?.username === "ecency", [activeUser]);
 
-  const join = async () => {
-    if (!hasUserJoinedChat) {
-      return;
-    }
-    await addCommunityChannel();
-  };
-
-  return isGlobalLoading ? (
+  return isCurrentChannelLoading ? (
     <></>
   ) : (
     <>
-      {isCommunityChannelJoined && (
+      {isCommunityChannelCreated && (
         <Button
           size="sm"
-          appearance="secondary"
-          disabled={isLeavingCommunityChannelLoading}
-          icon={isLeavingCommunityChannelLoading && <Spinner className="w-3.5 h-3.5" />}
-          onClick={() => leaveCommunityChannel()}
-        >
-          {_t("chat.chat-joined")}
-        </Button>
-      )}
-      {!isCommunityChannelJoined && isCommunityChannelCreated && (
-        <Button
-          size="sm"
-          disabled={isAddCommunityChannelLoading}
           to={`/chats/${props.community.name}/channel`}
           iconPlacement="left"
+          icon={messageSendSvg}
         >
-          {_t("chat.join-community-chat")}
+          {_t("chat.view-community-channel")}
         </Button>
       )}
       {!isCommunityChannelCreated && isAbleToCreateChannel && (
@@ -95,7 +48,7 @@ export default function JoinCommunityChatBtn(props: Props) {
           icon={isCreateCommunityChatLoading && <Spinner />}
           iconPlacement="left"
         >
-          {_t("chat.start-community-chat")}
+          {_t("chat.start-community-channel")}
         </Button>
       )}
     </>
