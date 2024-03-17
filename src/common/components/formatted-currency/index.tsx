0c975f77070071069b401e43a58b5e50d65836dc
leprechaun <leprechaun@steemfiles.com>
2024-03-17 10:25:52 -0300

    * use micro, milli, satoshis so the amount of Bitcoin is displayed with good precision.

--- src/common/components/formatted-currency/index.tsx
+++ src/common/components/formatted-currency/index.tsx
@@ -10,6 +10,8 @@ interface Props {
   fixAt: number;
 }
 
+const prefixes = ["", "m", "µ", "n"];
+
 export default class FormattedCurrency extends Component<Props> {
   public static defaultProps: Partial<Props> = {
     fixAt: 2
@@ -21,6 +23,36 @@ export default class FormattedCurrency extends Component<Props> {
 
     const valInCurrency = value * currencyRate;
 
-    return <>{formattedNumber(valInCurrency, { fractionDigits: fixAt, prefix: currencySymbol })}</>;
+    let multiplier = 1.0;
+    let prefix = "";
+    if (currencySymbol === "฿" && valInCurrency > 0) {
+      let decimal_places = 0;
+      let currency_string: string;
+      for (; decimal_places < 9; decimal_places += 3, multiplier *= 1000) {
+        const truncated_amount = Math.trunc(valInCurrency * Math.pow(10, decimal_places + fixAt));
+        if (isNaN(truncated_amount) || truncated_amount > 100) break;
+      }
+      if (decimal_places === 9) {
+        if (isNaN(multiplier * valInCurrency) || multiplier * valInCurrency === 0) {
+          decimal_places = 0;
+          multiplier = 1.0;
+        } else {
+          return formattedNumber(valInCurrency * Math.pow(10, 8), {
+            fractionDigits: 0,
+            suffix: "sats"
+          });
+        }
+      }
+      prefix = prefixes[decimal_places / 3];
+    }
+
+    return (
+      <>
+        {formattedNumber(valInCurrency * multiplier, {
+          fractionDigits: fixAt,
+          prefix: prefix + currencySymbol
+        })}
+      </>
+    );
   }
 }
