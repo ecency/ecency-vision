9a823625d9c167378caa2714b9873125a9ef2559
ildar.timerbaev <dkildar@gmail.com>
2024-07-30 21:13:20 +0300

    Show chat widget

--- src/app/chat-provider.tsx
+++ src/app/chat-provider.tsx
@@ -4,6 +4,7 @@ import { ChatContextProvider } from "@ecency/ns-query";
 import { useGlobalStore } from "@/core/global-store";
 import { getAccountFullQuery } from "@/api/queries";
 import { getAccessToken } from "@/utils";
+import { ChatPopUp } from "@/app/chats/_components/chat-popup";
 
 export function ChatProvider(props: PropsWithChildren) {
   const activeUser = useGlobalStore((state) => state.activeUser);
@@ -23,6 +24,7 @@ export function ChatProvider(props: PropsWithChildren) {
       ecencyAccessToken={accessToken}
     >
       {props.children}
+      <ChatPopUp />
     </ChatContextProvider>
   );
 }
